4103b36612edf9d95f8114e9635bb7d08f3d6038
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-11-02 15:09:32 +0100

    start digest

---
+++ src/digest.rs
@@ -0,0 +1,98 @@
+#[derive(Debug)]
+pub enum Error {
+    InvalidStateFinished,
+    ModeUnsupportedForStreaming,
+}
+
+/// The Digest Algorithm.
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(u32)]
+pub enum Algorithm {
+    Sha1 = 1,
+    Sha224 = 2,
+    Sha256 = 3,
+    Sha384 = 4,
+    Sha512 = 5,
+    Blake2s = 6,
+    Blake2b = 7,
+    Sha3_224 = 8,
+    Sha3_256 = 9,
+    Sha3_384 = 10,
+    Sha3_512 = 11,
+}
+
+impl From<u32> for Algorithm {
+    fn from(v: u32) -> Algorithm {
+        match v {
+            1 => Algorithm::Sha1,
+            2 => Algorithm::Sha224,
+            3 => Algorithm::Sha256,
+            4 => Algorithm::Sha384,
+            5 => Algorithm::Sha512,
+            6 => Algorithm::Blake2s,
+            7 => Algorithm::Blake2b,
+            8 => Algorithm::Sha3_224,
+            9 => Algorithm::Sha3_256,
+            10 => Algorithm::Sha3_384,
+            11 => Algorithm::Sha3_512,
+            _ => panic!("Unknown Digest mode {}", v),
+        }
+    }
+}
+
+impl From<Algorithm> for u32 {
+    fn from(v: Algorithm) -> u32 {
+        match v {
+            Algorithm::Sha1 => 1,
+            Algorithm::Sha224 => 2,
+            Algorithm::Sha256 => 3,
+            Algorithm::Sha384 => 4,
+            Algorithm::Sha512 => 5,
+            Algorithm::Blake2s => 6,
+            Algorithm::Blake2b => 7,
+            Algorithm::Sha3_224 => 8,
+            Algorithm::Sha3_256 => 9,
+            Algorithm::Sha3_384 => 10,
+            Algorithm::Sha3_512 => 11,
+        }
+    }
+}
+
+/// Returns the output size of a digest.
+pub const fn digest_size(mode: Algorithm) -> usize {
+    match mode {
+        Algorithm::Sha1 => 20,
+        Algorithm::Sha224 => 28,
+        Algorithm::Sha256 => 32,
+        Algorithm::Sha384 => 48,
+        Algorithm::Sha512 => 64,
+        Algorithm::Blake2s => 32,
+        Algorithm::Blake2b => 64,
+        Algorithm::Sha3_224 => 28,
+        Algorithm::Sha3_256 => 32,
+        Algorithm::Sha3_384 => 48,
+        Algorithm::Sha3_512 => 64,
+    }
+}
+
+impl Algorithm {
+    pub fn size(self) -> usize {
+        digest_size(self)
+    }
+}
+
+pub fn hash<const LEN:usize>(alg: Algorithm, _payload: &[u8]) -> [u8; LEN] {
+    match alg {
+        Algorithm::Sha1 => todo!(),
+        Algorithm::Sha224 => todo!(),
+        Algorithm::Sha256 => todo!(),
+        Algorithm::Sha384 => todo!(),
+        Algorithm::Sha512 => todo!(),
+        Algorithm::Blake2s => todo!(),
+        Algorithm::Blake2b => todo!(),
+        Algorithm::Sha3_224 => todo!(),
+        Algorithm::Sha3_256 => todo!(),
+        Algorithm::Sha3_384 => todo!(),
+        Algorithm::Sha3_512 => todo!(),
+    }
+}
