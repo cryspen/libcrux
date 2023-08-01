c12a30266b6631776b1959646a7a5f8deb94e0ca
xvzcf <xvzcf@users.noreply.github.com>
2023-08-01 13:05:02 -0400

    Moved cfg guard from jasmin submodules to jasmin module.

--- src/jasmin.rs
+++ src/jasmin.rs
@@ -1,16 +1,8 @@
 pub mod chacha20;
 pub mod poly1305;
-
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod sha2;
-
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod sha3;
-
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod x25519;
-
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod kyber_derand;
 
 #[cfg(test)]
