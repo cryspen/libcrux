232e59d0f395208948baa014939ed7a53e52b40e
xvzcf <xvzcf@users.noreply.github.com>
2023-08-01 10:21:39 -0400

    Testing.

--- src/jasmin.rs
+++ src/jasmin.rs
@@ -1,6 +1,14 @@
 pub mod chacha20;
 pub mod poly1305;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod sha2;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod sha3;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod x25519;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub mod kyber_derand;
