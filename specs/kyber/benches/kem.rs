8e34fa1c0d27af0e65550c90fd751b488a301940
xvzcf <xvzcf@users.noreply.github.com>
2023-08-02 11:55:47 -0400

    Added jasmin derandomized ref implementation to benchmarking.

--- specs/kyber/benches/kem.rs
+++ specs/kyber/benches/kem.rs
@@ -2,6 +2,8 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
 use pqcrypto::kem::kyber768;
 
+use libcrux::kem::Algorithm;
+
 use rand;
 
 pub fn randombytes<const N: usize>() -> [u8; N] {
@@ -33,6 +35,13 @@ pub fn comparisons_key_generation(c: &mut Criterion) {
         b.iter(|| {
             let (_public_key, _secret_key) = kyber768::keypair();
         })
+    });
+
+    group.bench_function("jasmin reference implementation", |b| {
+        b.iter(
+            || {
+                let _key_pair = libcrux::kem::key_gen(Algorithm::Kyber768, &mut rand::rngs::OsRng).unwrap();
+            })
     });
 }
 
@@ -67,6 +76,20 @@ pub fn comparisons_encapsulation(c: &mut Criterion) {
             },
             |public_key| {
                 let (_shared_secret, _ciphertext) = kyber768::encapsulate(&public_key);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("jasmin reference implementation", |b| {
+        b.iter_batched(
+            || {
+                let (_sk, pk) = libcrux::kem::key_gen(Algorithm::Kyber768, &mut rand::rngs::OsRng).unwrap();
+                pk
+            },
+            |pk| {
+                let (_shared_secret, _ciphertext) =
+                    libcrux::kem::encapsulate(Algorithm::Kyber768, &pk, &mut rand::rngs::OsRng).unwrap();
             },
             BatchSize::SmallInput,
         )
@@ -106,6 +129,22 @@ pub fn comparisons_decapsulation(c: &mut Criterion) {
             },
             |(ciphertext, secret_key)| {
                 let _shared_secret = kyber768::decapsulate(&ciphertext, &secret_key);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("jasmin reference implementation", |b| {
+        b.iter_batched(
+            || {
+                let (secret_key, public_key) = libcrux::kem::key_gen(Algorithm::Kyber768, &mut rand::rngs::OsRng).unwrap();
+                let (_shared_secret, ciphertext) =
+                    libcrux::kem::encapsulate(Algorithm::Kyber768, &public_key, &mut rand::rngs::OsRng).unwrap();
+
+                (secret_key, ciphertext)
+            },
+            |(secret_key, ciphertext)| {
+                let _shared_secret = libcrux::kem::decapsulate(Algorithm::Kyber768, &ciphertext, &secret_key).unwrap();
             },
             BatchSize::SmallInput,
         )
