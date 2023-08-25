dd916076f4e38a133c6ecdcf481b9e07fada00a6
Maamoun TK <maamoun.tk@gmail.com>
2023-08-25 16:10:51 +0300

    Unify conditional model for cpu feature functions in platform package

--- sys/platform/src/lib.rs
+++ sys/platform/src/lib.rs
@@ -18,45 +18,41 @@ mod linux_arm;
 #[cfg(test)]
 mod test;
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+// TODO: Check for z14 or z15
 pub fn simd128_support() -> bool {
-    cpu_id::supported(Feature::sse2)
-        && cpu_id::supported(Feature::sse3)
-        && cpu_id::supported(Feature::sse4_1)
-        && cpu_id::supported(Feature::avx)
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use macos_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        cpu_id::supported(Feature::sse2)
+            && cpu_id::supported(Feature::sse3)
+            && cpu_id::supported(Feature::sse4_1)
+            && cpu_id::supported(Feature::avx)
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use linux_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(not(any(all(target_arch = "aarch64", 
+        any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86", target_arch = "x86_64")))]
+    {
+        false
+    }
 }
 
-#[cfg(all(target_arch = "aarch64", target_os = "linux"))]
-pub fn simd128_support() -> bool {
-    use linux_arm::*;
-    adv_simd()
-}
-
-#[cfg(all(target_arch = "aarch64", target_os = "macos"))]
-pub fn simd128_support() -> bool {
-    use macos_arm::*;
-    adv_simd()
-}
-
-#[cfg(all(target_arch = "aarch64", not(any(target_os = "linux", target_os = "macos"))))]
-pub fn simd128_support() -> bool {
-    false
-}
-
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
-pub fn simd128_support() -> bool {
-    // XXX: Check for z14 or z15
-    false
-}
-
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub fn simd256_support() -> bool {
-    cpu_id::supported(Feature::avx2)
-}
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    return cpu_id::supported(Feature::avx2);
 
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-pub fn simd256_support() -> bool {
-    // XXX: Check for z14 or z15
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
     false
 }
 
@@ -68,13 +64,11 @@ pub fn x25519_support() -> bool {
     false
 }
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub fn bmi2_adx_support() -> bool {
-    cpu_id::supported(Feature::bmi2) && cpu_id::supported(Feature::adx)
-}
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    return cpu_id::supported(Feature::bmi2) && cpu_id::supported(Feature::adx);
 
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-pub fn bmi2_adx_support() -> bool {
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
     false
 }
 
@@ -96,6 +90,13 @@ pub fn pmull_support() -> bool {
         use crate::linux_arm::*;
         pmull()
     }
+
+    #[cfg(not(any(all(target_arch = "aarch64", 
+        any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86", target_arch = "x86_64")))]
+    {
+        false
+    }
 }
 
 /// Check whether advanced SIMD features are supported
@@ -112,7 +113,8 @@ pub fn adv_simd_support() -> bool {
         adv_simd()
     }
 
-    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[cfg(not(all(target_arch = "aarch64", 
+        any(target_os = "linux", target_os = "macos"))))]
     {
         false
     }
@@ -140,6 +142,13 @@ pub fn aes_ni_support() -> bool {
             && cpu_id::supported(Feature::pclmulqdq)
             && cpu_id::supported(Feature::movbe)
     }
+
+    #[cfg(not(any(all(target_arch = "aarch64", 
+        any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86", target_arch = "x86_64")))]
+    {
+        false
+    }
 }
 
 /// Check whether SHA256 is supported
@@ -160,45 +169,11 @@ pub fn sha256_support() -> bool {
     {
         cpu_id::supported(Feature::sha)
     }
-}
 
-#[cfg(not(any(
-    target_arch = "x86",
-    target_arch = "x86_64",
-    target_arch = "aarch64",
-    all(
-        target_arch = "aarch64",
-        not(any(target_os = "macos", target_os = "linux"))
-    )
-)))]
-mod other {
-    /// Check whether p(cl)mull is supported
-    pub fn pmull_support() -> bool {
-        false
-    }
-
-    /// Check whether advanced SIMD features are supported
-    pub fn adv_simd_support() -> bool {
-        false
-    }
-
-    /// Check whether AES is supported
-    pub fn aes_ni_support() -> bool {
-        false
-    }
-
-    /// Check whether SHA256 is supported
-    pub fn sha256_support() -> bool {
+    #[cfg(not(any(all(target_arch = "aarch64", 
+        any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86", target_arch = "x86_64")))]
+    {
         false
     }
 }
-#[cfg(not(any(
-    target_arch = "x86",
-    target_arch = "x86_64",
-    target_arch = "aarch64",
-    all(
-        target_arch = "aarch64",
-        not(any(target_os = "macos", target_os = "linux"))
-    )
-)))]
-pub use other::*;
