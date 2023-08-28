416c56f85ba8d8a9f7b7fc1d2b0a1f560bdc7035
mamonet <66893036+mamonet@users.noreply.github.com>
2023-08-28 12:35:31 +0300

    Use cc to build libjade and add platform detection crate (#51)

---
+++ sys/platform/src/lib.rs
@@ -0,0 +1,190 @@
+//! High-level functions to detect available CPU features
+//! at runtime on supported processor architectures and
+//! operation systems
+
+#![no_std]
+
+// Use std for tests
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod x86;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use x86::{self as cpu_id, Feature};
+
+#[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+mod linux_arm;
+#[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+mod macos_arm;
+
+#[cfg(test)]
+mod test;
+
+// TODO: Check for z14 or z15
+pub fn simd128_support() -> bool {
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use macos_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        cpu_id::supported(Feature::sse2)
+            && cpu_id::supported(Feature::sse3)
+            && cpu_id::supported(Feature::sse4_1)
+            && cpu_id::supported(Feature::avx)
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use linux_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(not(any(
+        all(target_arch = "aarch64", any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86",
+        target_arch = "x86_64"
+    )))]
+    {
+        false
+    }
+}
+
+pub fn simd256_support() -> bool {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    return cpu_id::supported(Feature::avx2);
+
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    false
+}
+
+pub fn x25519_support() -> bool {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    return cpu_id::supported(Feature::bmi2) && cpu_id::supported(Feature::adx);
+
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    false
+}
+
+pub fn bmi2_adx_support() -> bool {
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    return cpu_id::supported(Feature::bmi2) && cpu_id::supported(Feature::adx);
+
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+    false
+}
+
+/// Check whether p(cl)mull is supported
+pub fn pmull_support() -> bool {
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use crate::macos_arm::*;
+        pmull()
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        cpu_id::supported(Feature::pclmulqdq)
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use crate::linux_arm::*;
+        pmull()
+    }
+
+    #[cfg(not(any(
+        all(target_arch = "aarch64", any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86",
+        target_arch = "x86_64"
+    )))]
+    {
+        false
+    }
+}
+
+/// Check whether advanced SIMD features are supported
+pub fn adv_simd_support() -> bool {
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use macos_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use linux_arm::*;
+        adv_simd()
+    }
+
+    #[cfg(not(all(target_arch = "aarch64", any(target_os = "linux", target_os = "macos"))))]
+    {
+        false
+    }
+}
+
+/// Check whether AES is supported
+pub fn aes_ni_support() -> bool {
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use crate::macos_arm::*;
+        aes()
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use crate::linux_arm::*;
+        aes()
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        cpu_id::supported(Feature::avx)
+            && cpu_id::supported(Feature::sse)
+            && cpu_id::supported(Feature::aes)
+            && cpu_id::supported(Feature::pclmulqdq)
+            && cpu_id::supported(Feature::movbe)
+    }
+
+    #[cfg(not(any(
+        all(target_arch = "aarch64", any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86",
+        target_arch = "x86_64"
+    )))]
+    {
+        false
+    }
+}
+
+/// Check whether SHA256 is supported
+pub fn sha256_support() -> bool {
+    #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
+    {
+        use crate::macos_arm::*;
+        sha256()
+    }
+
+    #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
+    {
+        use crate::linux_arm::*;
+        sha256()
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    {
+        cpu_id::supported(Feature::sha)
+    }
+
+    #[cfg(not(any(
+        all(target_arch = "aarch64", any(target_os = "linux", target_os = "macos")),
+        target_arch = "x86",
+        target_arch = "x86_64"
+    )))]
+    {
+        false
+    }
+}
---
+++ sys/platform/src/linux_arm.rs
@@ -0,0 +1,59 @@
+//! Obtain particular CPU features for AArch64 on Linux
+
+use libc::{getauxval, AT_HWCAP, HWCAP_ASIMD, HWCAP_AES, HWCAP_PMULL, HWCAP_SHA2};
+
+#[inline(always)]
+fn auxval() {
+    let val = unsafe { getauxval(AT_HWCAP) };
+    let val_asimd = val & HWCAP_ASIMD != 0;
+    unsafe { ADV_SIMD = val_asimd };
+    let val_aes = val & HWCAP_AES != 0;
+    unsafe { AES = val_aes };
+    let val_pmull = val & HWCAP_PMULL != 0;
+    unsafe { PMULL = val_pmull };
+    let val_sha256 = val & HWCAP_SHA2 != 0;
+    unsafe { SHA256 = val_sha256 };
+}
+
+static mut ADV_SIMD: bool = false;
+static mut AES: bool = false;
+static mut PMULL: bool = false;
+static mut SHA256: bool = false;
+
+#[inline(always)]
+pub(super) fn aes() -> bool {
+    init();
+    unsafe { AES }
+}
+
+#[inline(always)]
+pub(super) fn adv_simd() -> bool {
+    init();
+    unsafe { ADV_SIMD }
+}
+
+#[inline(always)]
+pub(super) fn pmull() -> bool {
+    init();
+    unsafe { PMULL }
+}
+
+#[inline(always)]
+pub(super) fn sha256() -> bool {
+    init();
+    unsafe { SHA256 }
+}
+
+static mut INITIALIZED: bool = false;
+
+/// Initialize CPU detection.
+#[inline(always)]
+pub(super) fn init() {
+    if unsafe { INITIALIZED } {
+        return;
+    }
+    auxval();
+    unsafe {
+        INITIALIZED = true;
+    }
+}
