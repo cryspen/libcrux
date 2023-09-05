dfb2c544a073ad5e30765f32796b5d28a2889266
Maamoun TK <maamoun.tk@gmail.com>
2023-09-05 10:09:49 +0300

    Add empty header to pass Hacl_Curve25519_64.c build

---
+++ sys/hacl/c/include/msvc/config.h
@@ -0,0 +1,4 @@
+// This is empty header that represents Global HACL configuration
+// file with no variables defined to pass Hacl_Curve25519_64.c build.
+// Configuration of platform and features are done during hacl package
+// build in sys/hacl/build.rs
