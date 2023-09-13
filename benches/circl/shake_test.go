60b3fd1e81b3ded7f7ff39f3ec0d382080dae3ae
xvzcf <xvzcf@users.noreply.github.com>
2023-09-13 17:22:57 -0400

    Ran go fmt.

--- benches/circl/shake_test.go
+++ benches/circl/shake_test.go
@@ -5,40 +5,40 @@ import (
 	"testing"
 )
 
-const bytesToOutput = 10000;
+const bytesToOutput = 10000
 
 func BenchmarkSHAKE128(b *testing.B) {
-    input := make([]byte, 0, 34);
-    output := make([]byte, bytesToOutput);
+	input := make([]byte, 0, 34)
+	output := make([]byte, bytesToOutput)
 
-    xof := xof.SHAKE128.New()
-    _, err := xof.Write([]byte(input))
-    if err != nil {
-        b.Fatal(err)
-    }
+	xof := xof.SHAKE128.New()
+	_, err := xof.Write([]byte(input))
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	for i := 0; i < b.N; i++ {
-        n, err := xof.Read(output)
-        if n != bytesToOutput || err != nil {
-            b.Fatal()
-        }
+		n, err := xof.Read(output)
+		if n != bytesToOutput || err != nil {
+			b.Fatal()
+		}
 	}
 }
 
 func BenchmarkSHAKE256(b *testing.B) {
-    input := make([]byte, 0, 34);
-    output := make([]byte, bytesToOutput)
+	input := make([]byte, 0, 34)
+	output := make([]byte, bytesToOutput)
 
-    xof := xof.SHAKE256.New()
-    _, err := xof.Write([]byte(input))
-    if err != nil {
-        b.Fatal(err)
-    }
+	xof := xof.SHAKE256.New()
+	_, err := xof.Write([]byte(input))
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	for i := 0; i < b.N; i++ {
-        n, err := xof.Read(output)
-        if n != bytesToOutput || err != nil {
-            b.Fatal()
-        }
+		n, err := xof.Read(output)
+		if n != bytesToOutput || err != nil {
+			b.Fatal()
+		}
 	}
 }
