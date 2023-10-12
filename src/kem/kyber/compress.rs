ffef21cb1e364a27a0f30a1e06666ece63cadad6
Goutam Tamvada <xvzcf@users.noreply.github.com>
2023-10-12 05:55:50 -0400

    Reordered functions in Kyber reference so definitions come before calls. (#113)

--- src/kem/kyber/compress.rs
+++ src/kem/kyber/compress.rs
@@ -4,6 +4,15 @@ use super::{
     conversions::to_unsigned_representative,
 };
 
+fn compress_q<const COEFFICIENT_BITS: usize>(fe: u16) -> KyberFieldElement {
+    debug_assert!(COEFFICIENT_BITS <= BITS_PER_COEFFICIENT);
+
+    let mut compressed = (fe as u32) << (COEFFICIENT_BITS + 1);
+    compressed += FIELD_MODULUS as u32;
+    compressed /= (FIELD_MODULUS << 1) as u32;
+
+    (compressed & ((1u32 << COEFFICIENT_BITS) - 1)) as KyberFieldElement
+}
 pub fn compress<const COEFFICIENT_BITS: usize>(
     mut re: KyberPolynomialRingElement,
 ) -> KyberPolynomialRingElement {
@@ -11,27 +20,6 @@ pub fn compress<const COEFFICIENT_BITS: usize>(
         .coefficients
         .map(|coefficient| compress_q::<COEFFICIENT_BITS>(to_unsigned_representative(coefficient)));
     re
-}
-
-pub fn decompress<const COEFFICIENT_BITS: usize>(
-    mut re: KyberPolynomialRingElement,
-) -> KyberPolynomialRingElement {
-    re.coefficients = re
-        .coefficients
-        .map(|coefficient| decompress_q::<COEFFICIENT_BITS>(coefficient));
-    re
-}
-
-fn compress_q<const COEFFICIENT_BITS: usize>(fe: u16) -> KyberFieldElement {
-    debug_assert!(COEFFICIENT_BITS <= BITS_PER_COEFFICIENT);
-
-    let two_pow_bit_size = 1u32 << COEFFICIENT_BITS;
-
-    let mut compressed = (fe as u32) * (two_pow_bit_size << 1);
-    compressed += FIELD_MODULUS as u32;
-    compressed /= (FIELD_MODULUS << 1) as u32;
-
-    (compressed & (two_pow_bit_size - 1)) as KyberFieldElement
 }
 
 fn decompress_q<const COEFFICIENT_BITS: usize>(fe: KyberFieldElement) -> KyberFieldElement {
@@ -42,4 +30,12 @@ fn decompress_q<const COEFFICIENT_BITS: usize>(fe: KyberFieldElement) -> KyberFi
     decompressed >>= COEFFICIENT_BITS + 1;
 
     decompressed as KyberFieldElement
+}
+pub fn decompress<const COEFFICIENT_BITS: usize>(
+    mut re: KyberPolynomialRingElement,
+) -> KyberPolynomialRingElement {
+    re.coefficients = re
+        .coefficients
+        .map(|coefficient| decompress_q::<COEFFICIENT_BITS>(coefficient));
+    re
 }
