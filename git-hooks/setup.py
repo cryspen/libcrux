44bf6b5a6b66cc98947dc66122fc0ae789b614a7
xvzcf <xvzcf@users.noreply.github.com>
2023-10-24 13:27:47 -0400

    Use dashes instead of underscores in filenames for consistency.

--- git-hooks/setup.py
+++ git-hooks/setup.py
@@ -4,7 +4,7 @@ import os
 
 # This path is expressed relative to the .git/hooks directory.
 # See https://stackoverflow.com/questions/4592838/symbolic-link-to-a-hook-in-git#comment32655407_4594681
-source_directory = os.path.join("..", "..", "git_hooks")
+source_directory = os.path.join("..", "..", "git-hooks")
 
 target_directory = os.path.join(".git", "hooks")
 
