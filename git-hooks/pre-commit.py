8f6408e281a9d09713099688f7f7980402369230
xvzcf <xvzcf@users.noreply.github.com>
2023-10-24 13:38:44 -0400

    Refactor pre-commit hook and add support for f-star extraction.

--- git-hooks/pre-commit.py
+++ git-hooks/pre-commit.py
@@ -12,11 +12,20 @@ def shell(command, expect=0, cwd=None):
     if ret.returncode != expect:
         raise Exception("Error {}. Expected {}.".format(ret, expect))
 
-
 for item in repo.index.diff("HEAD"):
-    if Path(item.a_path).suffix == ".py":
-        shell(["black", "."])
-    elif Path(item.a_path).suffix == ".rs":
-        shell(["cargo", "fmt"])
+    path = Path(item.a_path)
+    if path.suffix == ".py":
+        format_python_files = True
+    elif path.suffix == ".rs":
+        format_rust_files = True
+        if 'kyber' in path.parts:
+            update_kyber_fstar_extraction = True
+
+if format_python_files == True:
+    shell(['black', '.'])
+if format_rust_files == True:
+    shell(['cargo', 'fmt'])
+if update_kyber_fstar_extraction == True:
+    shell(['./extract-to-fstar.py', '--kyber-reference'])
 
 repo.git.add(update=True)
