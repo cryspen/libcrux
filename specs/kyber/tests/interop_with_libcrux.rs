e4bed125f3e37dbff09ebebc99aad69947d8ee62
xvzcf <xvzcf@users.noreply.github.com>
2023-10-26 11:44:51 -0400

    Don't return early in libcrux-spec interop test.

--- specs/kyber/tests/interop_with_libcrux.rs
+++ specs/kyber/tests/interop_with_libcrux.rs
@@ -9,16 +9,16 @@ fn same_inputs_result_in_same_output() {
     let mut keygen_seed = [0u8; KYBER768_KEY_GENERATION_SEED_SIZE];
     OsRng.fill_bytes(&mut keygen_seed);
 
-    let libcrux_key_pair_result = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed);
-    let spec_key_pair_result = hacspec_kyber::generate_keypair(keygen_seed);
+    let (libcrux_key_pair, spec_key_pair) = loop {
+        let libcrux_key_pair_result = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed);
+        let spec_key_pair_result = hacspec_kyber::generate_keypair(keygen_seed);
 
-    if libcrux_key_pair_result.is_err() {
-        spec_key_pair_result.expect_err("If rejection sampling failed in libcrux, it should fail in the spec as well.");
-        return
-    }
-
-    let libcrux_key_pair = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed).unwrap();
-    let spec_key_pair = hacspec_kyber::generate_keypair(keygen_seed).unwrap();
+        if libcrux_key_pair_result.is_ok() {
+            break (libcrux_key_pair_result.unwrap(), spec_key_pair_result.unwrap())
+        } else {
+            spec_key_pair_result.expect_err("if rejection sampling failed in libcrux, it should've failed in the spec as well.");
+        }
+    };
 
     assert_eq!(libcrux_key_pair.pk(), spec_key_pair.pk());
     assert_eq!(libcrux_key_pair.sk(), spec_key_pair.sk());
