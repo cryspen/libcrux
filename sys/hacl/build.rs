9d2b34ef7dd5bbf222410b81c0f20aa82f5db67c
Franziskus Kiefer <franziskuskiefer@gmail.com>
2023-11-27 10:12:11 +0100

    update to hacl 95112e8dcb1ea3fe7d254290f8864b8f8e2fb6b2

--- sys/hacl/build.rs
+++ sys/hacl/build.rs
@@ -105,14 +105,11 @@ fn create_bindings(platform: &Platform, home_dir: &Path) {
         // Set include paths for HACL headers
         .clang_args(clang_args)
         // Allow function we want to have in
-        .allowlist_function("Hacl_Chacha20Poly1305.*")
+        .allowlist_function("Hacl_AEAD_Chacha20Poly1305_.*")
         .allowlist_function("Hacl_Curve25519.*")
         .allowlist_function("Hacl_Hash_SHA2.*")
-        .allowlist_function("Hacl_Streaming_SHA2.*")
-        .allowlist_function("Hacl_SHA3.*")
-        .allowlist_function("Hacl_Hash_SHA3.*")
+        .allowlist_function("Hacl_Hash_.*")
         .allowlist_function("Hacl_Blake2.*")
-        .allowlist_function("Hacl_Streaming_Keccak.*")
         .allowlist_function("Hacl_HMAC_DRBG.*")
         .allowlist_function("Hacl_Ed25519.*")
         .allowlist_function("Hacl_HKDF_.*")
@@ -123,20 +120,20 @@ fn create_bindings(platform: &Platform, home_dir: &Path) {
         .allowlist_function("Hacl_RSAPSS.*")
         .allowlist_function("hacl_free")
         .allowlist_type("Spec_.*")
-        .allowlist_type("Hacl_Streaming_SHA2.*")
         .allowlist_type("Hacl_HMAC_DRBG.*")
         .allowlist_type("Hacl_Streaming.*")
+        .allowlist_type("Hacl_Hash_.*")
         .allowlist_var("Spec_.*")
         .allowlist_var("Hacl_Streaming.*")
         // XXX: These functions use uint128 in the API, which is not FFI safe
-        .blocklist_function("Hacl_Blake2b_32_blake2b_update_multi")
-        .blocklist_function("Hacl_Blake2b_32_blake2b_update_last")
-        .blocklist_function("Hacl_Blake2b_256_blake2b_update_multi")
-        .blocklist_function("Hacl_Blake2b_256_blake2b_update_last")
+        .blocklist_function("Hacl_Hash_Blake2b_update_multi")
+        .blocklist_function("Hacl_Hash_Blake2b_update_last")
+        .blocklist_function("Hacl_Hash_Blake2b_Simd256_update_multi")
+        .blocklist_function("Hacl_Hash_Blake2b_Simd256_update_last")
         // Disable tests to avoid warnings and keep it portable
         .layout_tests(false)
         // Generate bindings
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .use_core()
         .generate()
         .expect("Unable to generate bindings");
@@ -201,7 +198,7 @@ fn build(platform: &Platform, home_path: &Path) {
         "hacl.c",
         "Hacl_NaCl.c",
         "Hacl_Salsa20.c",
-        "Hacl_Poly1305_32.c",
+        "Hacl_MAC_Poly1305.c",
         "Hacl_Curve25519_51.c",
         // "Hacl_Gf128_CT64.c",
         // "Hacl_AES_128_CTR32_BitSlice.c",
@@ -211,21 +208,20 @@ fn build(platform: &Platform, home_path: &Path) {
         "Hacl_HMAC_DRBG.c",
         "Hacl_HMAC.c",
         "Hacl_Hash_SHA2.c",
-        "Hacl_Hash_Blake2.c",
+        "Hacl_Hash_Blake2b.c",
+        "Hacl_Hash_Blake2s.c",
         "Lib_Memzero0.c",
         "Hacl_Ed25519.c",
         "Hacl_EC_Ed25519.c",
         "Hacl_Hash_Base.c",
-        "Hacl_Streaming_Blake2.c",
         "Hacl_Bignum256_32.c",
         "Hacl_Bignum.c",
         "Hacl_Bignum256.c",
         "Hacl_Bignum32.c",
         "Hacl_Bignum4096_32.c",
         "Hacl_GenericField32.c",
-        "Hacl_Chacha20Poly1305_32.c",
+        "Hacl_AEAD_Chacha20Poly1305.c",
         "Hacl_Chacha20.c",
-        "Hacl_Streaming_Poly1305_32.c",
         "Hacl_Chacha20_Vec32.c",
         "Hacl_P256.c",
         "Hacl_K256_ECDSA.c",
@@ -243,15 +239,13 @@ fn build(platform: &Platform, home_path: &Path) {
     // Platform detection
     if platform.simd128 {
         let files128 = svec![
-            "Hacl_Hash_Blake2s_128.c",
-            "Hacl_Streaming_Blake2s_128.c",
+            "Hacl_Hash_Blake2s_Simd128.c",
             "Hacl_Bignum4096.c",
             "Hacl_Bignum64.c",
             "Hacl_GenericField64.c",
-            "Hacl_Chacha20Poly1305_128.c",
-            "Hacl_Poly1305_128.c",
+            "Hacl_AEAD_Chacha20Poly1305_Simd128.c",
+            "Hacl_MAC_Poly1305_Simd128.c",
             "Hacl_Chacha20_Vec128.c",
-            "Hacl_Streaming_Poly1305_128.c",
             "Hacl_SHA2_Vec128.c",
             "Hacl_HKDF_Blake2s_128.c",
             "Hacl_HMAC_Blake2s_128.c",
@@ -272,12 +266,11 @@ fn build(platform: &Platform, home_path: &Path) {
     }
     if platform.simd256 {
         let files256 = svec![
-            "Hacl_Hash_Blake2b_256.c",
+            "Hacl_Hash_Blake2b_Simd256.c",
             "Hacl_Streaming_Blake2b_256.c",
-            "Hacl_Chacha20Poly1305_256.c",
-            "Hacl_Poly1305_256.c",
+            "Hacl_AEAD_Chacha20Poly1305_Simd256.c",
+            "Hacl_MAC_Poly1305_Simd256.c",
             "Hacl_Chacha20_Vec256.c",
-            "Hacl_Streaming_Poly1305_256.c",
             "Hacl_SHA2_Vec256.c",
             "Hacl_HKDF_Blake2b_256.c",
             "Hacl_HMAC_Blake2b_256.c",
--- sys/hacl/src/lib.rs
+++ sys/hacl/src/lib.rs
@@ -4,15 +4,15 @@
 
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
-#[cfg(not(target_arch = "wasm32"))]
+// #[cfg(not(target_arch = "wasm32"))]
 mod bindings;
-#[cfg(not(target_arch = "wasm32"))]
+// #[cfg(not(target_arch = "wasm32"))]
 pub use bindings::*;
 
-#[cfg(target_arch = "wasm32")]
-mod wasm32_bindings;
-#[cfg(target_arch = "wasm32")]
-pub use wasm32_bindings::*;
+// #[cfg(target_arch = "wasm32")]
+// mod wasm32_bindings;
+// #[cfg(target_arch = "wasm32")]
+// pub use wasm32_bindings::*;
 
 #[cfg(test)]
 mod test {
@@ -24,7 +24,7 @@ mod test {
         let mut digest = [0u8; 32];
         let msg = b"libcrux sha2 256 tests";
         unsafe {
-            Hacl_Streaming_SHA2_hash_256(msg.as_ptr() as _, msg.len() as u32, digest.as_mut_ptr());
+            Hacl_Hash_SHA2_hash_256(digest.as_mut_ptr(), msg.as_ptr() as _, msg.len() as u32);
         }
         let expected = "8683520e19e5b33db33c8fb90918c0c96fcdfd9a17c695ce0f0ea2eaa0c95956";
         assert_eq!(hex::encode(digest), expected);
