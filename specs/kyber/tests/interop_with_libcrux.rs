b8190b3f91844e79dbff872cee9950f350d956ec
xvzcf <xvzcf@users.noreply.github.com>
2023-12-07 12:38:39 -0500

    Fix Kyber specification tests.

--- specs/kyber/tests/interop_with_libcrux.rs
+++ specs/kyber/tests/interop_with_libcrux.rs
@@ -11,21 +11,8 @@ fn same_inputs_result_in_same_output() {
     let mut keygen_seed = [0u8; KYBER768_KEY_GENERATION_SEED_SIZE];
     OsRng.fill_bytes(&mut keygen_seed);
 
-    let (libcrux_key_pair, spec_key_pair) = loop {
-        let libcrux_key_pair_result = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed);
-        let spec_key_pair_result = hacspec_kyber::generate_keypair(keygen_seed);
-
-        if libcrux_key_pair_result.is_ok() {
-            break (
-                libcrux_key_pair_result.unwrap(),
-                spec_key_pair_result.unwrap(),
-            );
-        } else {
-            spec_key_pair_result.expect_err(
-                "if rejection sampling failed in libcrux, it should've failed in the spec as well.",
-            );
-        }
-    };
+    let spec_key_pair = hacspec_kyber::generate_keypair(keygen_seed).unwrap();
+    let libcrux_key_pair = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed);
 
     assert_eq!(libcrux_key_pair.pk(), spec_key_pair.pk());
     assert_eq!(libcrux_key_pair.sk(), spec_key_pair.sk());
@@ -33,23 +20,23 @@ fn same_inputs_result_in_same_output() {
     let mut message = [0u8; KYBER768_SHARED_SECRET_SIZE];
     OsRng.fill_bytes(&mut message);
 
-    let (libcrux_ct, libcrux_ss) =
-        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message).unwrap();
     let (spec_ct, spec_ss) = hacspec_kyber::encapsulate(*spec_key_pair.pk(), message).unwrap();
+    let (libcrux_ct, libcrux_ss) =
+        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message);
 
     assert_eq!(libcrux_ct.as_ref(), spec_ct);
     assert_eq!(libcrux_ss.as_ref(), spec_ss);
 
-    let (libcrux_ct, libcrux_ss) =
-        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message).unwrap();
     let (spec_ct, spec_ss) = hacspec_kyber::encapsulate(*spec_key_pair.pk(), message).unwrap();
+    let (libcrux_ct, libcrux_ss) =
+        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message);
 
     assert_eq!(libcrux_ct.as_ref(), spec_ct);
     assert_eq!(libcrux_ss.as_ref(), spec_ss);
 
+    let spec_ss = hacspec_kyber::decapsulate(spec_ct, *spec_key_pair.sk());
     let libcrux_ss =
         libcrux::kem::kyber768_decapsulate_derand(libcrux_key_pair.private_key(), &libcrux_ct);
-    let spec_ss = hacspec_kyber::decapsulate(spec_ct, *spec_key_pair.sk());
 
     assert_eq!(libcrux_ss, spec_ss);
 }
@@ -85,30 +72,30 @@ fn implicit_rejection_happens_the_same_way() {
     let mut keygen_seed = [0u8; KYBER768_KEY_GENERATION_SEED_SIZE];
     OsRng.fill_bytes(&mut keygen_seed);
 
-    let libcrux_key_pair = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed).unwrap();
     let spec_key_pair = hacspec_kyber::generate_keypair(keygen_seed).unwrap();
+    let libcrux_key_pair = libcrux::kem::kyber768_generate_keypair_derand(keygen_seed);
 
     let mut message = [0u8; KYBER768_SHARED_SECRET_SIZE];
     OsRng.fill_bytes(&mut message);
 
-    let (libcrux_ct, libcrux_ss) =
-        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message).unwrap();
     let (spec_ct, spec_ss) = hacspec_kyber::encapsulate(*spec_key_pair.pk(), message).unwrap();
+    let (libcrux_ct, libcrux_ss) =
+        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message);
 
     assert_eq!(libcrux_ct.as_ref(), spec_ct);
     assert_eq!(libcrux_ss.as_ref(), spec_ss);
 
-    let (libcrux_ct, _) =
-        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message).unwrap();
     let (spec_ct, _) = hacspec_kyber::encapsulate(*spec_key_pair.pk(), message).unwrap();
+    let (libcrux_ct, _) =
+        libcrux::kem::kyber768_encapsulate_derand(&libcrux_key_pair.pk().into(), message);
 
     let (modified_libcrux_ct, modified_spec_ct) = modify_ciphertext_pair(libcrux_ct, spec_ct);
 
+    let spec_ss = hacspec_kyber::decapsulate(modified_spec_ct, *spec_key_pair.sk());
     let libcrux_ss = libcrux::kem::kyber768_decapsulate_derand(
         libcrux_key_pair.private_key(),
         &modified_libcrux_ct,
     );
-    let spec_ss = hacspec_kyber::decapsulate(modified_spec_ct, *spec_key_pair.sk());
 
     assert_eq!(libcrux_ss, spec_ss);
 }
