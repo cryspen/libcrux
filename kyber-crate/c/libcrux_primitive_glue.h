b317f5fa1a4acb84564a4a432a75f9de06941fa6
Jonathan Protzenko <protz@microsoft.com>
2024-02-14 14:56:01 -0800

    Tweak some visibility rules to get rid of the sed regarding struct names

---
+++ kyber-crate/c/libcrux_primitive_glue.h
@@ -0,0 +1,12 @@
+/* Hand-written file */
+
+#define libcrux_digest_shake128x4(len, s1, s2, s3, s4, _ret_t) \
+  libcrux_digest_shake128x4_(len, s1, s2, s3, s4) \
+
+static inline
+K___uint8_t_840size_t__uint8_t_840size_t__uint8_t_840size_t__uint8_t_840size_t_
+libcrux_digest_shake128x4_(size_t len, Eurydice_slice s1, Eurydice_slice s2, Eurydice_slice s3, Eurydice_slice s4) {
+  K___uint8_t_840size_t__uint8_t_840size_t__uint8_t_840size_t__uint8_t_840size_t_ ret = { 0 };
+  // TODO
+  exit(0);
+}
