a98e242b036a38ad6d36df8f5c482329ce101b0d
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-03-13 20:56:35 +0100

    fix lib25519 build

--- benchmarks/benches/x25519.rs
+++ benchmarks/benches/x25519.rs
@@ -9,114 +9,114 @@ fn derive(c: &mut Criterion) {
     // Comparing libcrux performance for different payload sizes and other implementations.
     let mut group = c.benchmark_group("x25519/derive");
 
-    // group.bench_function("libcrux", |b| {
-    //     b.iter_batched(
-    //         || {
-    //             let sk1 = randombytes(32);
-    //             let pk1 = ecdh::secret_to_public(ecdh::Algorithm::X25519, &sk1).unwrap();
-    //             let sk2 = randombytes(32);
-    //             (pk1, sk2)
-    //         },
-    //         |(pk1, sk2)| {
-    //             let _zz = ecdh::derive(ecdh::Algorithm::X25519, &pk1, &sk2).unwrap();
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    group.bench_function("libcrux", |b| {
+        b.iter_batched(
+            || {
+                let sk1 = randombytes(32);
+                let pk1 = ecdh::secret_to_public(ecdh::Algorithm::X25519, &sk1).unwrap();
+                let sk2 = randombytes(32);
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let _zz = ecdh::derive(ecdh::Algorithm::X25519, &pk1, &sk2).unwrap();
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-    // group.bench_function("Ring", |b| {
-    //     use ring::{agreement, rand::SystemRandom};
+    group.bench_function("Ring", |b| {
+        use ring::{agreement, rand::SystemRandom};
 
-    //     b.iter_batched(
-    //         || {
-    //             let rng = SystemRandom::new();
-    //             let sk1 =
-    //                 agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
-    //             let pk1 = sk1.compute_public_key().unwrap();
-    //             let sk2 =
-    //                 agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
+        b.iter_batched(
+            || {
+                let rng = SystemRandom::new();
+                let sk1 =
+                    agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
+                let pk1 = sk1.compute_public_key().unwrap();
+                let sk2 =
+                    agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
 
-    //             (pk1, sk2)
-    //         },
-    //         |(pk1, sk2)| {
-    //             let _zz: Result<Vec<u8>, ring::error::Unspecified> = agreement::agree_ephemeral(
-    //                 sk2,
-    //                 &agreement::UnparsedPublicKey::new(&agreement::X25519, pk1),
-    //                 |k| Ok(k.to_vec()),
-    //             )
-    //             .unwrap();
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let _zz: Result<Vec<u8>, ring::error::Unspecified> = agreement::agree_ephemeral(
+                    sk2,
+                    &agreement::UnparsedPublicKey::new(&agreement::X25519, pk1),
+                    |k| Ok(k.to_vec()),
+                )
+                .unwrap();
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-    // #[cfg(all(not(windows), not(target_arch = "wasm32"), not(target_arch = "x86")))]
-    // group.bench_function("OpenSSL", |b| {
-    //     use openssl::derive::Deriver;
-    //     use openssl::pkey::{Id, PKey};
-    //     use openssl::pkey_ctx::PkeyCtx;
+    #[cfg(all(not(windows), not(target_arch = "wasm32"), not(target_arch = "x86")))]
+    group.bench_function("OpenSSL", |b| {
+        use openssl::derive::Deriver;
+        use openssl::pkey::{Id, PKey};
+        use openssl::pkey_ctx::PkeyCtx;
 
-    //     b.iter_batched(
-    //         || {
-    //             let mut ctx = PkeyCtx::new_id(Id::X25519).unwrap();
-    //             ctx.keygen_init().unwrap();
-    //             let sk1 = ctx.keygen().unwrap();
-    //             let pk1 = sk1.raw_public_key().unwrap();
+        b.iter_batched(
+            || {
+                let mut ctx = PkeyCtx::new_id(Id::X25519).unwrap();
+                ctx.keygen_init().unwrap();
+                let sk1 = ctx.keygen().unwrap();
+                let pk1 = sk1.raw_public_key().unwrap();
 
-    //             ctx.keygen_init().unwrap();
-    //             let sk2 = ctx.keygen().unwrap();
-    //             let pk1 = PKey::public_key_from_raw_bytes(&pk1, Id::X25519).unwrap();
-    //             (pk1, sk2)
-    //         },
-    //         |(pk1, sk2)| {
-    //             let mut deriver = Deriver::new(&sk2).unwrap();
-    //             deriver.set_peer(&pk1).unwrap();
-    //             let _zz = deriver.derive_to_vec().unwrap();
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+                ctx.keygen_init().unwrap();
+                let sk2 = ctx.keygen().unwrap();
+                let pk1 = PKey::public_key_from_raw_bytes(&pk1, Id::X25519).unwrap();
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let mut deriver = Deriver::new(&sk2).unwrap();
+                deriver.set_peer(&pk1).unwrap();
+                let _zz = deriver.derive_to_vec().unwrap();
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-    // group.bench_function("Dalek", |b| {
-    //     use rand_core::OsRng;
-    //     use x25519_dalek::{EphemeralSecret, PublicKey};
+    group.bench_function("Dalek", |b| {
+        use rand_core::OsRng;
+        use x25519_dalek::{EphemeralSecret, PublicKey};
 
-    //     b.iter_batched(
-    //         || {
-    //             let sk1 = EphemeralSecret::random_from_rng(OsRng);
-    //             let pk1 = PublicKey::from(&sk1);
-    //             let sk2 = EphemeralSecret::random_from_rng(OsRng);
-    //             (pk1, sk2)
-    //         },
-    //         |(pk1, sk2)| {
-    //             let _zz = sk2.diffie_hellman(&pk1);
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+        b.iter_batched(
+            || {
+                let sk1 = EphemeralSecret::random_from_rng(OsRng);
+                let pk1 = PublicKey::from(&sk1);
+                let sk2 = EphemeralSecret::random_from_rng(OsRng);
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let _zz = sk2.diffie_hellman(&pk1);
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
-    // group.bench_function("Dalek Ristretto", |b| {
-    //     use curve25519_dalek::ristretto::RistrettoPoint;
-    //     use curve25519_dalek::scalar::Scalar;
-    //     use rand_core::OsRng;
+    group.bench_function("Dalek Ristretto", |b| {
+        use curve25519_dalek::ristretto::RistrettoPoint;
+        use curve25519_dalek::scalar::Scalar;
+        use rand_core::OsRng;
 
-    //     b.iter_batched(
-    //         || {
-    //             let mut sk1_b = [0u8; 32];
-    //             OsRng.fill_bytes(&mut sk1_b);
-    //             let sk1 = Scalar::from_bytes_mod_order(sk1_b);
-    //             let pk1 = RistrettoPoint::mul_base(&sk1);
-    //             let mut sk2_b = [0u8; 32];
-    //             OsRng.fill_bytes(&mut sk2_b);
-    //             let sk2 = Scalar::from_bytes_mod_order(sk2_b);
-    //             (pk1, sk2)
-    //         },
-    //         |(pk1, sk2)| {
-    //             let _zz = pk1 * sk2;
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+        b.iter_batched(
+            || {
+                let mut sk1_b = [0u8; 32];
+                OsRng.fill_bytes(&mut sk1_b);
+                let sk1 = Scalar::from_bytes_mod_order(sk1_b);
+                let pk1 = RistrettoPoint::mul_base(&sk1);
+                let mut sk2_b = [0u8; 32];
+                OsRng.fill_bytes(&mut sk2_b);
+                let sk2 = Scalar::from_bytes_mod_order(sk2_b);
+                (pk1, sk2)
+            },
+            |(pk1, sk2)| {
+                let _zz = pk1 * sk2;
+            },
+            BatchSize::SmallInput,
+        )
+    });
 
     #[cfg(all(target_arch = "x86_64", target_os = "linux", crypto_lib25519))]
     group.bench_function("lib25519", |b| {
