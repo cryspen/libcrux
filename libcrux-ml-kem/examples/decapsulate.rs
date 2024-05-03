34878500e9bf355987b3199780bd1c95008de573
Karthik Bhargavan <karthik.bhargavan@gmail.com>
2024-05-03 10:55:11 +0200

    more examples

---
+++ libcrux-ml-kem/examples/decapsulate.rs
@@ -0,0 +1,16 @@
+use libcrux_ml_kem::{kyber768, ENCAPS_SEED_SIZE, KEY_GENERATION_SEED_SIZE};
+use rand::{rngs::OsRng, RngCore};
+
+fn main() {
+    let mut randomness = [0u8; KEY_GENERATION_SEED_SIZE];
+    OsRng.fill_bytes(&mut randomness);
+
+    let key_pair = kyber768::generate_key_pair(randomness);
+    let mut randomness = [0u8; ENCAPS_SEED_SIZE];
+    OsRng.fill_bytes(&mut randomness);
+    let (ct,ss) = kyber768::encapsulate(key_pair.public_key(), randomness);
+
+    for _ in 0..100_000 {
+    	let _ = kyber768::decapsulate(key_pair.private_key(), &ct);
+    }
+}
