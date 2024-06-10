3fe64a1da2531e2fb8bd0847f2097e23247bf825
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-06-10 16:12:13 +0200

    Formatting

--- src/hmac.rs
+++ src/hmac.rs
@@ -2,6 +2,6 @@
 //!
 //! This module implements HMAC on SHA 1 and SHA 2 (except for SHA 224).
 
-pub use libcrux_hmac::Algorithm;
-pub use libcrux_hmac::tag_size;
 pub use libcrux_hmac::hmac;
+pub use libcrux_hmac::tag_size;
+pub use libcrux_hmac::Algorithm;
