7d3fe6b875c431c6452800ded305a8e060c4f6ce
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-06-10 13:27:53 +0200

    Change visibility of `libcrux-ml-kem::MlKemKeyPair` fields to `pub`
    So they can be accessed in the `libcrux-kem` crate.

--- libcrux-ml-kem/src/types.rs
+++ libcrux-ml-kem/src/types.rs
@@ -137,8 +137,8 @@ mod index_impls {
 
 /// An ML-KEM key pair
 pub struct MlKemKeyPair<const PRIVATE_KEY_SIZE: usize, const PUBLIC_KEY_SIZE: usize> {
-    pub(crate) sk: MlKemPrivateKey<PRIVATE_KEY_SIZE>,
-    pub(crate) pk: MlKemPublicKey<PUBLIC_KEY_SIZE>,
+    pub sk: MlKemPrivateKey<PRIVATE_KEY_SIZE>,
+    pub pk: MlKemPublicKey<PUBLIC_KEY_SIZE>,
 }
 
 impl<const PRIVATE_KEY_SIZE: usize, const PUBLIC_KEY_SIZE: usize>
