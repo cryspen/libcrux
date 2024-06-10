51d838e3e0eba386be1ec5a3589ffe56a86259e7
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-06-10 11:28:44 +0200

    add avx2 code again

--- libcrux-ml-kem/c/libcrux_mlkem768_avx2.c
+++ libcrux-ml-kem/c/libcrux_mlkem768_avx2.c
@@ -5,42 +5,42 @@
   KaRaMeL version: 40e3a603
  */
 
-#include "libcrux_mlkem768_portable.h"
+#include "libcrux_mlkem768_avx2.h"
 
-void libcrux_ml_kem_mlkem768_portable_decapsulate(
+void libcrux_ml_kem_mlkem768_avx2_decapsulate(
     libcrux_ml_kem_types_MlKemPrivateKey____2400size_t *private_key,
     libcrux_ml_kem_mlkem768_MlKem768Ciphertext *ciphertext, uint8_t ret[32U]) {
   uint8_t ret0[32U];
-  libcrux_ml_kem_ind_cca_instantiations_portable_decapsulate___3size_t_2400size_t_1152size_t_1184size_t_1088size_t_1152size_t_960size_t_128size_t_10size_t_4size_t_320size_t_2size_t_128size_t_2size_t_128size_t_1120size_t(
+  libcrux_ml_kem_ind_cca_instantiations_avx2_decapsulate___3size_t_2400size_t_1152size_t_1184size_t_1088size_t_1152size_t_960size_t_128size_t_10size_t_4size_t_320size_t_2size_t_128size_t_2size_t_128size_t_1120size_t(
       private_key, ciphertext, ret0);
   memcpy(ret, ret0, (size_t)32U * sizeof(uint8_t));
 }
 
 K___libcrux_ml_kem_types_MlKemCiphertext___1088size_t___uint8_t_32size_t_
-libcrux_ml_kem_mlkem768_portable_encapsulate(
+libcrux_ml_kem_mlkem768_avx2_encapsulate(
     libcrux_ml_kem_types_MlKemPublicKey____1184size_t *public_key,
     uint8_t randomness[32U]) {
   libcrux_ml_kem_types_MlKemPublicKey____1184size_t *uu____0 = public_key;
   uint8_t uu____1[32U];
   memcpy(uu____1, randomness, (size_t)32U * sizeof(uint8_t));
-  return libcrux_ml_kem_ind_cca_instantiations_portable_encapsulate___3size_t_1088size_t_1184size_t_1152size_t_960size_t_128size_t_10size_t_4size_t_320size_t_2size_t_128size_t_2size_t_128size_t(
+  return libcrux_ml_kem_ind_cca_instantiations_avx2_encapsulate___3size_t_1088size_t_1184size_t_1152size_t_960size_t_128size_t_10size_t_4size_t_320size_t_2size_t_128size_t_2size_t_128size_t(
       uu____0, uu____1);
 }
 
 libcrux_ml_kem_mlkem768_MlKem768KeyPair
-libcrux_ml_kem_mlkem768_portable_generate_key_pair(uint8_t randomness[64U]) {
+libcrux_ml_kem_mlkem768_avx2_generate_key_pair(uint8_t randomness[64U]) {
   uint8_t uu____0[64U];
   memcpy(uu____0, randomness, (size_t)64U * sizeof(uint8_t));
-  return libcrux_ml_kem_ind_cca_instantiations_portable_generate_keypair___3size_t_1152size_t_2400size_t_1184size_t_1152size_t_2size_t_128size_t(
+  return libcrux_ml_kem_ind_cca_instantiations_avx2_generate_keypair___3size_t_1152size_t_2400size_t_1184size_t_1152size_t_2size_t_128size_t(
       uu____0);
 }
 
 core_option_Option__libcrux_ml_kem_types_MlKemPublicKey___1184size_t__
-libcrux_ml_kem_mlkem768_portable_validate_public_key(
+libcrux_ml_kem_mlkem768_avx2_validate_public_key(
     libcrux_ml_kem_types_MlKemPublicKey____1184size_t public_key) {
   core_option_Option__libcrux_ml_kem_types_MlKemPublicKey___1184size_t__
       uu____0;
-  if (libcrux_ml_kem_ind_cca_instantiations_portable_validate_public_key___3size_t_1152size_t_1184size_t(
+  if (libcrux_ml_kem_ind_cca_instantiations_avx2_validate_public_key___3size_t_1152size_t_1184size_t(
           public_key.value)) {
     uu____0 = ((
         core_option_Option__libcrux_ml_kem_types_MlKemPublicKey___1184size_t__){
--- libcrux-ml-kem/c/libcrux_mlkem768_avx2.h
+++ libcrux-ml-kem/c/libcrux_mlkem768_avx2.h
@@ -5,8 +5,8 @@
   KaRaMeL version: 40e3a603
  */
 
-#ifndef __libcrux_mlkem768_portable_H
-#define __libcrux_mlkem768_portable_H
+#ifndef __libcrux_mlkem768_avx2_H
+#define __libcrux_mlkem768_avx2_H
 
 #if defined(__cplusplus)
 extern "C" {
@@ -14,27 +14,27 @@ extern "C" {
 
 #include "eurydice_glue.h"
 #include "libcrux_core.h"
-#include "libcrux_mlkem512_portable.h"
+#include "libcrux_mlkem512_avx2.h"
 
-void libcrux_ml_kem_mlkem768_portable_decapsulate(
+void libcrux_ml_kem_mlkem768_avx2_decapsulate(
     libcrux_ml_kem_types_MlKemPrivateKey____2400size_t *private_key,
     libcrux_ml_kem_mlkem768_MlKem768Ciphertext *ciphertext, uint8_t ret[32U]);
 
 K___libcrux_ml_kem_types_MlKemCiphertext___1088size_t___uint8_t_32size_t_
-libcrux_ml_kem_mlkem768_portable_encapsulate(
+libcrux_ml_kem_mlkem768_avx2_encapsulate(
     libcrux_ml_kem_types_MlKemPublicKey____1184size_t *public_key,
     uint8_t randomness[32U]);
 
 libcrux_ml_kem_mlkem768_MlKem768KeyPair
-libcrux_ml_kem_mlkem768_portable_generate_key_pair(uint8_t randomness[64U]);
+libcrux_ml_kem_mlkem768_avx2_generate_key_pair(uint8_t randomness[64U]);
 
 core_option_Option__libcrux_ml_kem_types_MlKemPublicKey___1184size_t__
-libcrux_ml_kem_mlkem768_portable_validate_public_key(
+libcrux_ml_kem_mlkem768_avx2_validate_public_key(
     libcrux_ml_kem_types_MlKemPublicKey____1184size_t public_key);
 
 #if defined(__cplusplus)
 }
 #endif
 
-#define __libcrux_mlkem768_portable_H_DEFINED
+#define __libcrux_mlkem768_avx2_H_DEFINED
 #endif
