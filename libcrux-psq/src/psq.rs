7e36ee7c28430c718838a7dcb1988fd1096f4c16
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-06-12 15:30:44 +0200

    Option to use Curve25519, ML-KEM768 or Classic McEliece

--- libcrux-psq/src/psq.rs
+++ libcrux-psq/src/psq.rs
@@ -4,6 +4,7 @@
 //! protocol.
 
 use chrono::{DateTime, Duration, Utc};
+use classic_mceliece_rust::{decapsulate_boxed, encapsulate_boxed};
 use libcrux_hmac::hmac;
 use rand::{CryptoRng, Rng};
 
@@ -16,27 +17,172 @@ pub const MAC_LENGTH: usize = 32;
 type Psk = [u8; PSK_LENGTH];
 type Mac = [u8; MAC_LENGTH];
 
+#[derive(Debug)]
 pub enum Error {
+    InvalidPublicKey,
+    InvalidPrivateKey,
     GenerationError,
     DerivationError,
 }
 
+pub enum Algorithm {
+    X25519,
+    MlKem768,
+    ClassicMcEliece,
+}
+
+pub enum Ciphertext {
+    X25519(libcrux_kem::Ct),
+    MlKem768(libcrux_kem::Ct),
+    ClassicMcEliece(classic_mceliece_rust::Ciphertext),
+}
+
+pub enum PublicKey<'a> {
+    X25519(libcrux_kem::PublicKey),
+    MlKem768(libcrux_kem::PublicKey),
+    ClassicMcEliece(classic_mceliece_rust::PublicKey<'a>),
+}
+
+pub enum PrivateKey<'a> {
+    X25519(libcrux_kem::PrivateKey),
+    MlKem768(libcrux_kem::PrivateKey),
+    ClassicMcEliece(classic_mceliece_rust::SecretKey<'a>),
+}
+
+pub enum SharedSecret<'a> {
+    X25519(libcrux_kem::Ss),
+    MlKem768(libcrux_kem::Ss),
+    ClassicMcEliece(classic_mceliece_rust::SharedSecret<'a>),
+}
+
+impl SharedSecret<'_> {
+    fn encode(&self) -> Vec<u8> {
+        match self {
+            SharedSecret::X25519(ss) => ss.encode(),
+            SharedSecret::MlKem768(ss) => ss.encode(),
+            SharedSecret::ClassicMcEliece(ss) => ss.as_ref().to_owned(),
+        }
+    }
+}
+
+impl Ciphertext {
+    fn encode(&self) -> Vec<u8> {
+        match self {
+            Ciphertext::X25519(ct) => ct.encode(),
+            Ciphertext::MlKem768(ct) => ct.encode(),
+            Ciphertext::ClassicMcEliece(ct) => ct.as_ref().to_owned(),
+        }
+    }
+    fn decapsulate(&self, sk: &PrivateKey) -> Result<SharedSecret, Error> {
+        match self {
+            Ciphertext::X25519(ct) => {
+                if let PrivateKey::X25519(sk) = sk {
+                    let ss = ct.decapsulate(sk).unwrap();
+                    Ok(SharedSecret::X25519(ss))
+                } else {
+                    Err(Error::InvalidPrivateKey)
+                }
+            }
+            Ciphertext::MlKem768(ct) => {
+                if let PrivateKey::MlKem768(sk) = sk {
+                    let ss = ct.decapsulate(sk).unwrap();
+                    Ok(SharedSecret::MlKem768(ss))
+                } else {
+                    Err(Error::InvalidPrivateKey)
+                }
+            }
+            Ciphertext::ClassicMcEliece(ct) => {
+                if let PrivateKey::ClassicMcEliece(sk) = sk {
+                    let ss = decapsulate_boxed(ct, sk);
+                    Ok(SharedSecret::ClassicMcEliece(ss))
+                } else {
+                    Err(Error::InvalidPrivateKey)
+                }
+            }
+        }
+    }
+}
+
+impl Into<libcrux_kem::Algorithm> for Algorithm {
+    fn into(self) -> libcrux_kem::Algorithm {
+        match self {
+            Algorithm::X25519 => libcrux_kem::Algorithm::X25519,
+            Algorithm::MlKem768 => libcrux_kem::Algorithm::MlKem768,
+            Algorithm::ClassicMcEliece => {
+                unimplemented!("libcrux does not support Classic McEliece")
+            }
+        }
+    }
+}
+
+pub fn generate_key_pair(
+    alg: Algorithm,
+    rng: &mut (impl CryptoRng + Rng),
+) -> Result<(PrivateKey<'static>, PublicKey<'static>), Error> {
+    match alg {
+        Algorithm::X25519 => {
+            let (sk, pk) = libcrux_kem::key_gen(alg.into(), rng).unwrap();
+            Ok((PrivateKey::X25519(sk), PublicKey::X25519(pk)))
+        }
+        Algorithm::MlKem768 => {
+            let (sk, pk) = libcrux_kem::key_gen(alg.into(), rng).unwrap();
+            Ok((PrivateKey::MlKem768(sk), PublicKey::MlKem768(pk)))
+        }
+        Algorithm::ClassicMcEliece => {
+            let (pk, sk) = classic_mceliece_rust::keypair_boxed(rng);
+            Ok((
+                PrivateKey::ClassicMcEliece(sk),
+                PublicKey::ClassicMcEliece(pk),
+            ))
+        }
+    }
+}
+
+impl PublicKey<'_> {
+    pub(crate) fn encode(&self) -> Vec<u8> {
+        match self {
+            PublicKey::X25519(k) | PublicKey::MlKem768(k) => k.encode(),
+            PublicKey::ClassicMcEliece(k) => k.as_ref().to_vec(),
+        }
+    }
+
+    pub(crate) fn encapsulate(
+        &self,
+        rng: &mut (impl CryptoRng + Rng),
+    ) -> Result<(SharedSecret, Ciphertext), Error> {
+        match self {
+            PublicKey::X25519(pk) => {
+                let (ss, enc) = pk.encapsulate(rng).unwrap();
+                Ok((SharedSecret::X25519(ss), Ciphertext::X25519(enc)))
+            }
+            PublicKey::MlKem768(pk) => {
+                let (ss, enc) = pk.encapsulate(rng).unwrap();
+                Ok((SharedSecret::MlKem768(ss), Ciphertext::MlKem768(enc)))
+            }
+            PublicKey::ClassicMcEliece(pk) => {
+                let (enc, ss) = encapsulate_boxed(pk, rng);
+                Ok((
+                    SharedSecret::ClassicMcEliece(ss),
+                    Ciphertext::ClassicMcEliece(enc),
+                ))
+            }
+        }
+    }
+}
+
 pub struct PskMessage {
-    enc: libcrux_kem::Ct,
+    enc: Ciphertext,
     ts: i64,
     mac: Mac,
 }
 
 pub fn generate_psk(
-    pk_b: &[u8],
+    pk: &PublicKey,
     sctx: &[u8],
     rng: &mut (impl CryptoRng + Rng),
 ) -> Result<(Psk, PskMessage), Error> {
-    let pk = libcrux_kem::PublicKey::decode(libcrux_kem::Algorithm::X25519, pk_b)
-        .map_err(|_| Error::GenerationError)?;
-
     let (ik, enc) = pk.encapsulate(rng).map_err(|_| Error::GenerationError)?;
-    let mut info = pk_b.to_vec();
+    let mut info = pk.encode();
     info.extend_from_slice(&enc.encode());
     info.extend_from_slice(sctx);
 
@@ -77,8 +223,8 @@ pub fn generate_psk(
 }
 
 pub fn derive_psk(
-    pk_b: &[u8],
-    sk: &libcrux_kem::PrivateKey,
+    pk: &PublicKey,
+    sk: &PrivateKey,
     message: &PskMessage,
     sctx: &[u8],
 ) -> Result<Psk, Error> {
@@ -97,7 +243,7 @@ pub fn derive_psk(
 
     let ik = enc.decapsulate(sk).map_err(|_| Error::DerivationError)?;
 
-    let mut info = pk_b.to_vec();
+    let mut info = pk.encode();
     info.extend_from_slice(&enc.encode());
     info.extend_from_slice(sctx);
 
@@ -136,4 +282,42 @@ pub fn derive_psk(
         .expect("should receive the correct number of bytes from HKDF");
 
     Ok(psk)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_x25519() {
+        let mut rng = rand::thread_rng();
+        let (sk, pk) = generate_key_pair(Algorithm::X25519, &mut rng).unwrap();
+        let sctx = b"test context";
+        let (psk_initiator, message) = generate_psk(&pk, sctx, &mut rng).unwrap();
+
+        let psk_responder = derive_psk(&pk, &sk, &message, sctx).unwrap();
+        assert_eq!(psk_initiator, psk_responder);
+    }
+
+    #[test]
+    fn simple_mlkem768() {
+        let mut rng = rand::thread_rng();
+        let (sk, pk) = generate_key_pair(Algorithm::MlKem768, &mut rng).unwrap();
+        let sctx = b"test context";
+        let (psk_initiator, message) = generate_psk(&pk, sctx, &mut rng).unwrap();
+
+        let psk_responder = derive_psk(&pk, &sk, &message, sctx).unwrap();
+        assert_eq!(psk_initiator, psk_responder);
+    }
+
+    #[test]
+    fn simple_classic_mceliece() {
+        let mut rng = rand::thread_rng();
+        let (sk, pk) = generate_key_pair(Algorithm::ClassicMcEliece, &mut rng).unwrap();
+        let sctx = b"test context";
+        let (psk_initiator, message) = generate_psk(&pk, sctx, &mut rng).unwrap();
+
+        let psk_responder = derive_psk(&pk, &sk, &message, sctx).unwrap();
+        assert_eq!(psk_initiator, psk_responder);
+    }
 }
