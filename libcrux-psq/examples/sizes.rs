7d40ffcd1d21eff567965fa80b93d51d8c28dedb
Jonas Schneider-Bensch <124457079+jschneider-bensch@users.noreply.github.com>
2024-06-27 08:41:17 +0200

    PQ-PSK prototype (#310)
    * Make ML-KEM-768 constants public
    
    So they can be used by hybrid KEMs in the `libcrux-kem` crate.
    
    * Change `libcrux::ecdh` submodule and item visibility to public
    
    So they can be used in the `libcrux-kem` crate
    
    * Change visibility of `libcrux-ml-kem::MlKemKeyPair` fields to `pub`
    
    So they can be accessed in the `libcrux-kem` crate.
    
    * Extract `libcrux::kem` module to its own crate
    
    * Update KEM crate documentation
    
    * Format
    
    * Provide `.len()` on ML-KEM structs and use that instead of constants
    
    * Use `.into_parts()` on `MlKemKeyPair` instead of direct access
    
    * Add CI run for `libcrux-kem`
    
    * Fix ML-KEM tests
    
    * CI: Use Rust stable and exclude Win32 (linker issue)
    
    * Revert CI change to now install Rust nightly again
    
    * Pull out `ecdh` module into its own crate
    
    * Make `libcrux-kem` use `libcrux-ecdh` instead of `libcrux`
    
    * Make `libcrux` use standalone `libcrux-ecdh` crate
    
    * WIP: PQ-PSK prototype
    
    * Copied CI workflow for ECDH crate
    
    * Merge imports
    
    * Option to use Curve25519, ML-KEM768 or Classic McEliece
    
    * Move P256 ECDSA signature API back to libcrux
    
    * Move ECDH tests to the `libcrux-ecdh` crate
    
    * Make PSK lifetime a parameter of PSK generation
    
    * Update Cargo.lock
    
    * Reduce public API
    
    * Make `libcrux` depend on `libcrux-kem`
    
    * Make spec libcrux interop tests use standalone crate
    
    * Update benchmarks to use `libcrux-ml-kem` (resp. `libcrux-kem`)
    
    * Remove dead code
    
    * Simple benchmarks for PQ-PSK
    
    * Basic PSK message size measurement
    
    * Include raw KEM operations in benchmark
    
    * Include public key sizes in size comparison
    
    * Include XWing in size comparison
    
    * Include XWing in benchmarks
    
    * wip; no chrono and encaps example
    
    * std::time timestamp
    
    * Simple timestamp tests
    
    * Minor rewrites for PV extraction
    
    * Format
    
    * Add documentation
    
    * Add `README.md`
    
    ---------
    
    Co-authored-by: Franziskus Kiefer <franziskuskiefer@gmail.com>

---
+++ libcrux-psq/examples/sizes.rs
@@ -0,0 +1,67 @@
+use std::time::Duration;
+
+use libcrux_psq::*;
+use rand::{self, thread_rng};
+
+fn main() {
+    let mut rng = thread_rng();
+    let mlkem_keypair = generate_key_pair(Algorithm::MlKem768, &mut rng).unwrap();
+    let x25519_keypair = generate_key_pair(Algorithm::X25519, &mut rng).unwrap();
+    let xwing_keypair = generate_key_pair(Algorithm::XWingKemDraft02, &mut rng).unwrap();
+    let classic_mceliece_keypair = generate_key_pair(Algorithm::ClassicMcEliece, &mut rng).unwrap();
+
+    let mlkem_message = mlkem_keypair
+        .1
+        .send_psk(b"size context", Duration::from_secs(3600), &mut rng)
+        .unwrap();
+    let x25519_message = x25519_keypair
+        .1
+        .send_psk(b"size context", Duration::from_secs(3600), &mut rng)
+        .unwrap();
+    let xwing_message = xwing_keypair
+        .1
+        .send_psk(b"size context", Duration::from_secs(3600), &mut rng)
+        .unwrap();
+    let classic_mceliece_message = classic_mceliece_keypair
+        .1
+        .send_psk(b"size context", Duration::from_secs(3600), &mut rng)
+        .unwrap();
+
+    println!("ML-KEM-768:");
+    println!("  Public key size (bytes): {}", mlkem_keypair.1.size());
+    println!("  Message size (bytes): {}", mlkem_message.1.size());
+    println!(
+        "  including ciphertext size (bytes): {}",
+        mlkem_message.1.ct_size()
+    );
+
+    println!("X25519:");
+    println!("  Public key size (bytes): {}", x25519_keypair.1.size());
+    println!("  Message size (bytes): {}", x25519_message.1.size());
+    println!(
+        "  including ciphertext size (bytes): {}",
+        x25519_message.1.ct_size()
+    );
+
+    println!("XWingKemDraft02:");
+    println!("  Public key size (bytes): {}", xwing_keypair.1.size());
+    println!("  Message size (bytes): {}", xwing_message.1.size());
+    println!(
+        "  including ciphertext size (bytes): {}",
+        xwing_message.1.ct_size()
+    );
+
+    println!("Classic McEliece:");
+    println!(
+        "  Public key size (bytes): {}",
+        classic_mceliece_keypair.1.size()
+    );
+    println!(
+        "  Message size (bytes): {}",
+        classic_mceliece_message.1.size()
+    );
+    println!(
+        "  including ciphertext size (bytes): {}",
+        classic_mceliece_message.1.ct_size()
+    );
+}
