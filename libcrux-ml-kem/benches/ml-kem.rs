8de8b7af03e3d0575267826251491bb98b733e60
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-06-28 10:55:34 +0200

    more ct and more benchmarks

--- libcrux-ml-kem/benches/ml-kem.rs
+++ libcrux-ml-kem/benches/ml-kem.rs
@@ -5,160 +5,223 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use rand_core::OsRng;
 use rand_core::RngCore;
 
-use libcrux_ml_kem::mlkem768;
+use libcrux_ml_kem::{mlkem1024, mlkem512, mlkem768};
 
-pub fn comparisons_key_generation(c: &mut Criterion) {
+pub fn key_generation(c: &mut Criterion) {
     let mut rng = OsRng;
-    let mut group = c.benchmark_group("Kyber768 Key Generation");
+
+    macro_rules! fun {
+        ($name:expr, $p:path, $group:expr) => {
+            $group.bench_function(format!("libcrux {} (external random)", $name), |b| {
+                use $p as p;
+
+                let mut seed = [0; 64];
+                rng.fill_bytes(&mut seed);
+                b.iter(|| {
+                    let _kp = core::hint::black_box(p::generate_key_pair(seed));
+                })
+            });
+        };
+    }
+
+    let mut group = c.benchmark_group("ML-KEM 512 Key Generation");
     group.measurement_time(Duration::from_secs(10));
 
-    group.bench_function("libcrux portable (external random)", |b| {
-        let mut seed = [0; 64];
-        rng.fill_bytes(&mut seed);
-        b.iter(|| {
-            let _kp = mlkem768::generate_key_pair(seed);
-        })
-    });
+    fun!("portable", mlkem512::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem512::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem512::avx2, group);
+    drop(group);
 
-    // group.bench_function("libcrux portable (HACL-DRBG)", |b| {
-    //     b.iter(|| {
-    //         let (_secret_key, _public_key) =
-    //             libcrux::kem::key_gen(Algorithm::MlKem768, &mut drbg).unwrap();
-    //     })
-    // });
+    let mut group = c.benchmark_group("ML-KEM 768 Key Generation");
+    group.measurement_time(Duration::from_secs(10));
 
-    // group.bench_function("libcrux portable (OsRng)", |b| {
-    //     b.iter(|| {
-    //         let (_secret_key, _public_key) =
-    //             libcrux::kem::key_gen(Algorithm::MlKem768, &mut rng).unwrap();
-    //     })
-    // });
+    fun!("portable", mlkem768::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem768::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem768::avx2, group);
+    drop(group);
 
-    // group.bench_function("pqclean reference implementation", |b| {
-    //     b.iter(|| {
-    //         let (_public_key, _secret_key) = pqcrypto_kyber::kyber768::keypair();
-    //     })
-    // });
+    let mut group = c.benchmark_group("ML-KEM 1024 Key Generation");
+    group.measurement_time(Duration::from_secs(10));
+
+    fun!("portable", mlkem1024::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem1024::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem1024::avx2, group);
 }
 
-pub fn comparisons_pk_validation(c: &mut Criterion) {
+pub fn pk_validation(c: &mut Criterion) {
     let mut rng = OsRng;
-    let mut group = c.benchmark_group("Kyber768 PK Validation");
+
+    macro_rules! fun {
+        ($name:expr, $p:path, $group:expr) => {
+            $group.bench_function(format!("libcrux {}", $name), |b| {
+                use $p as p;
+
+                let mut seed = [0; 64];
+                rng.fill_bytes(&mut seed);
+                b.iter_batched(
+                    || {
+                        let keypair = p::generate_key_pair(seed);
+                        keypair.public_key().as_slice().into()
+                    },
+                    |public_key| {
+                        let _valid = black_box(p::validate_public_key(public_key));
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        };
+    }
+
+    let mut group = c.benchmark_group("ML-KEM 512 PK Validation");
     group.measurement_time(Duration::from_secs(10));
 
-    group.bench_function("libcrux portable", |b| {
-        let mut seed = [0; 64];
-        rng.fill_bytes(&mut seed);
-        b.iter_batched(
-            || {
-                let keypair = mlkem768::generate_key_pair(seed);
-                keypair.public_key().as_slice().into()
-            },
-            |public_key| {
-                let _valid = black_box(mlkem768::validate_public_key(public_key));
-            },
-            BatchSize::SmallInput,
-        )
-    });
+    fun!("portable", mlkem512::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem512::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem512::avx2, group);
+    drop(group);
+
+    let mut group = c.benchmark_group("ML-KEM 768 PK Validation");
+    group.measurement_time(Duration::from_secs(10));
+
+    fun!("portable", mlkem768::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem768::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem768::avx2, group);
+    drop(group);
+
+    let mut group = c.benchmark_group("ML-KEM 1024 PK Validation");
+    group.measurement_time(Duration::from_secs(10));
+
+    fun!("portable", mlkem1024::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem1024::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem1024::avx2, group);
 }
 
-pub fn comparisons_encapsulation(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Kyber768 Encapsulation");
+pub fn encapsulation(c: &mut Criterion) {
+    macro_rules! fun {
+        ($name:expr, $p:path, $group:expr) => {
+            $group.bench_function(format!("libcrux {} (external random)", $name), |b| {
+                use $p as p;
+
+                let mut seed1 = [0; 64];
+                OsRng.fill_bytes(&mut seed1);
+                let mut seed2 = [0; 32];
+                OsRng.fill_bytes(&mut seed2);
+                b.iter_batched(
+                    || p::generate_key_pair(seed1),
+                    |keypair| {
+                        let (_shared_secret, _ciphertext) =
+                            black_box(p::encapsulate(keypair.public_key(), seed2));
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        };
+    }
+
+    let mut group = c.benchmark_group("ML-KEM 512 Encapsulation");
     group.measurement_time(Duration::from_secs(10));
 
-    group.bench_function("libcrux portable (external random)", |b| {
-        let mut seed1 = [0; 64];
-        OsRng.fill_bytes(&mut seed1);
-        let mut seed2 = [0; 32];
-        OsRng.fill_bytes(&mut seed2);
-        b.iter_batched(
-            || mlkem768::generate_key_pair(seed1),
-            |keypair| {
-                let (_shared_secret, _ciphertext) =
-                    mlkem768::encapsulate(keypair.public_key(), seed2);
-            },
-            BatchSize::SmallInput,
-        )
-    });
+    fun!("portable", mlkem512::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem512::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem512::avx2, group);
+    drop(group);
 
-    // group.bench_function("libcrux portable", |b| {
-    //     b.iter_batched(
-    //         || {
-    //             let mut drbg = Drbg::new(digest::Algorithm::Sha256).unwrap();
-    //             let (_secret_key, public_key) =
-    //                 libcrux::kem::key_gen(Algorithm::MlKem768, &mut drbg).unwrap();
+    let mut group = c.benchmark_group("ML-KEM 768 Encapsulation");
+    group.measurement_time(Duration::from_secs(10));
 
-    //             (drbg, public_key)
-    //         },
-    //         |(mut rng, public_key)| {
-    //             let (_shared_secret, _ciphertext) = public_key.encapsulate(&mut rng).unwrap();
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    fun!("portable", mlkem768::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem768::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem768::avx2, group);
+    drop(group);
 
-    // group.bench_function("pqclean reference implementation", |b| {
-    //     b.iter_batched(
-    //         || {
-    //             let (public_key, _secret_key) = pqcrypto_kyber::kyber768::keypair();
+    let mut group = c.benchmark_group("ML-KEM 1024 Encapsulation");
+    group.measurement_time(Duration::from_secs(10));
 
-    //             public_key
-    //         },
-    //         |public_key| {
-    //             let (_shared_secret, _ciphertext) =
-    //                 pqcrypto_kyber::kyber768::encapsulate(&public_key);
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    fun!("portable", mlkem1024::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem1024::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem1024::avx2, group);
 }
 
-pub fn comparisons_decapsulation(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Kyber768 Decapsulation");
+pub fn decapsulation(c: &mut Criterion) {
+    macro_rules! fun {
+        ($name:expr, $p:path, $group:expr) => {
+            $group.bench_function(format!("libcrux {}", $name), |b| {
+                use $p as p;
+
+                let mut seed1 = [0; 64];
+                OsRng.fill_bytes(&mut seed1);
+                let mut seed2 = [0; 32];
+                OsRng.fill_bytes(&mut seed2);
+                b.iter_batched(
+                    || {
+                        let keypair = p::generate_key_pair(seed1);
+                        let (ciphertext, _shared_secret) =
+                            p::encapsulate(keypair.public_key(), seed2);
+                        (keypair, ciphertext)
+                    },
+                    |(keypair, ciphertext)| {
+                        let _shared_secret =
+                            black_box(p::decapsulate(keypair.private_key(), &ciphertext));
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        };
+    }
+
+    let mut group = c.benchmark_group("ML-KEM 512 Decapsulation");
     group.measurement_time(Duration::from_secs(10));
 
-    group.bench_function("libcrux portable", |b| {
-        let mut seed1 = [0; 64];
-        OsRng.fill_bytes(&mut seed1);
-        let mut seed2 = [0; 32];
-        OsRng.fill_bytes(&mut seed2);
-        b.iter_batched(
-            || {
-                let keypair = mlkem768::generate_key_pair(seed1);
-                let (ciphertext, _shared_secret) =
-                    mlkem768::encapsulate(keypair.public_key(), seed2);
-                (keypair, ciphertext)
-            },
-            |(keypair, ciphertext)| {
-                let _shared_secret = mlkem768::decapsulate(keypair.private_key(), &ciphertext);
-            },
-            BatchSize::SmallInput,
-        )
-    });
+    fun!("portable", mlkem512::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem512::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem512::avx2, group);
+    drop(group);
 
-    // group.bench_function("pqclean reference implementation", |b| {
-    //     b.iter_batched(
-    //         || {
-    //             let (public_key, secret_key) = pqcrypto_kyber::kyber768::keypair();
-    //             let (_shared_secret, ciphertext) =
-    //                 pqcrypto_kyber::kyber768::encapsulate(&public_key);
+    let mut group = c.benchmark_group("ML-KEM 768 Decapsulation");
+    group.measurement_time(Duration::from_secs(10));
 
-    //             (ciphertext, secret_key)
-    //         },
-    //         |(ciphertext, secret_key)| {
-    //             let _shared_secret =
-    //                 pqcrypto_kyber::kyber768::decapsulate(&ciphertext, &secret_key);
-    //         },
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    fun!("portable", mlkem768::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem768::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem768::avx2, group);
+    drop(group);
+
+    let mut group = c.benchmark_group("ML-KEM 1024 Decapsulation");
+    group.measurement_time(Duration::from_secs(10));
+
+    fun!("portable", mlkem1024::portable, group);
+    #[cfg(feature = "simd128")]
+    fun!("neon", mlkem1024::neon, group);
+    #[cfg(feature = "simd256")]
+    fun!("neon", mlkem1024::avx2, group);
 }
 
 pub fn comparisons(c: &mut Criterion) {
-    comparisons_pk_validation(c);
-    comparisons_key_generation(c);
-    comparisons_encapsulation(c);
-    comparisons_decapsulation(c);
+    pk_validation(c);
+    key_generation(c);
+    encapsulation(c);
+    decapsulation(c);
 }
 
 criterion_group!(benches, comparisons);
--- libcrux-ml-kem/src/constant_time_ops.rs
+++ libcrux-ml-kem/src/constant_time_ops.rs
@@ -1,6 +1,11 @@
 use crate::constants::SHARED_SECRET_SIZE;
 use crate::hax_utils::hax_debug_assert;
 
+// These are crude attempts to prevent LLVM from optimizing away the code in this
+// module. This is not guaranteed to work but at the time of writing, achieved
+// its goals.
+// `read_volatile` could be used as well but seems unnecessary at this point in
+// time.
 // Examine the output that LLVM produces for this code from time to time to ensure
 // operations are not being optimized away/constant-timedness is not being broken.
 
@@ -9,13 +14,15 @@ use crate::hax_utils::hax_debug_assert;
     hax_lib::implies(value == 0, || result == 0) &&
     hax_lib::implies(value != 0, || result == 1)
 ))]
+fn inz(value: u8) -> u8 {
+    let value = value as u16;
+    let result = ((value | (!value).wrapping_add(1)) >> 8) & 1;
+    result as u8
+}
+
 #[inline(never)] // Don't inline this to avoid that the compiler optimizes this out.
 fn is_non_zero(value: u8) -> u8 {
-    let value = value as u16;
-
-    let result = ((value | (!value).wrapping_add(1)) >> 8) & 1;
-
-    result as u8
+    core::hint::black_box(inz(value))
 }
 
 /// Return 1 if the bytes of `lhs` and `rhs` do not exactly
@@ -24,16 +31,13 @@ fn is_non_zero(value: u8) -> u8 {
     hax_lib::implies(lhs == rhs, || result == 0) &&
     hax_lib::implies(lhs != rhs, || result == 1)
 ))]
-pub(crate) fn compare_ciphertexts_in_constant_time<const CIPHERTEXT_SIZE: usize>(
-    lhs: &[u8],
-    rhs: &[u8],
-) -> u8 {
+fn compare<const SIZE: usize>(lhs: &[u8], rhs: &[u8]) -> u8 {
     hax_debug_assert!(lhs.len() == rhs.len());
     hax_debug_assert!(lhs.len() == CIPHERTEXT_SIZE);
 
     let mut r: u8 = 0;
-    for i in 0..CIPHERTEXT_SIZE {
-        r |= lhs[i] ^ rhs[i];
+    for i in 0..SIZE {
+        r = r | (lhs[i] ^ rhs[i]);
     }
 
     is_non_zero(r)
@@ -45,11 +49,7 @@ pub(crate) fn compare_ciphertexts_in_constant_time<const CIPHERTEXT_SIZE: usize>
     hax_lib::implies(selector == 0, || result == lhs) &&
     hax_lib::implies(selector != 0, || result == rhs)
 ))]
-pub(crate) fn select_shared_secret_in_constant_time(
-    lhs: &[u8],
-    rhs: &[u8],
-    selector: u8,
-) -> [u8; SHARED_SECRET_SIZE] {
+pub(crate) fn select(lhs: &[u8], rhs: &[u8], selector: u8) -> [u8; SHARED_SECRET_SIZE] {
     hax_debug_assert!(lhs.len() == rhs.len());
     hax_debug_assert!(lhs.len() == SHARED_SECRET_SIZE);
 
@@ -61,4 +61,21 @@ pub(crate) fn select_shared_secret_in_constant_time(
     }
 
     out
+}
+
+#[inline(never)] // Don't inline this to avoid that the compiler optimizes this out.
+pub(crate) fn compare_ciphertexts_in_constant_time<const CIPHERTEXT_SIZE: usize>(
+    lhs: &[u8],
+    rhs: &[u8],
+) -> u8 {
+    core::hint::black_box(compare::<CIPHERTEXT_SIZE>(lhs, rhs))
+}
+
+#[inline(never)] // Don't inline this to avoid that the compiler optimizes this out.
+pub(crate) fn select_shared_secret_in_constant_time(
+    lhs: &[u8],
+    rhs: &[u8],
+    selector: u8,
+) -> [u8; SHARED_SECRET_SIZE] {
+    core::hint::black_box(select(lhs, rhs, selector))
 }
