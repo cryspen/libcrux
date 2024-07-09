b31719bda62168a163496ec08d1c8efc1283fafe
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-07-09 10:45:46 +0200

    re-add pqcp udpate script

---
+++ libcrux-ml-kem/pqcp/update-pqcp.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# This script copies files to be included in the `mlkem-rust-libcrux`
+# crate for the PQ code package. Files and directories to be copied
+# are listed, one per line, in a file at `ALLOWLIST` and copied to
+# `TARGET_DIR`, which should be the `mlkem-rust-libcrux` git
+# repository. The copied files include custom Cargo.toml and
+# .gitignore files for mlkem-rust-libcrux. On successful completion of
+# `cargo test`, a signed-off commit is created in the target
+# repository.
+
+set -e -x
+set -o pipefail
+
+cwd=$(cd $(dirname $0); pwd -P)
+ALLOWLIST="$cwd/allowlist.txt"
+LIBCRUX_HOME="$cwd/../.."
+
+if [[ -z "$TARGET_DIRECTORY" ]]; then
+    echo "Please set TARGET_DIRECTORY to the libcrux-ml-kem PQ code packages repository"
+    exit 1
+fi
+
+while read -r item; do
+    cp -r "$item" "$TARGET_DIRECTORY"
+done < "$ALLOWLIST"
+
+cd "$TARGET_DIRECTORY"
+cargo test
+cargo clean
+
+git switch -c $1
+git add .
+git commit -sm "mlkem-rust-libcrux PQ code packages update ($(date)) - libcrux revision $(git -C $LIBCRUX_HOME rev-parse HEAD)"
