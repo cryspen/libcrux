5d4f200f7586cdfe6459e68f0300ebcb39cd4334
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-07-12 15:44:26 +0200

    update extraction

--- libcrux-ml-kem/boring.sh
+++ libcrux-ml-kem/boring.sh
@@ -10,16 +10,11 @@ cargo clean
 
 # Fixup code
 # TODO: This should go away as soon as the code generation is fixed.
-sed -i -E 's/static inline/__attribute__((target("avx2")))\nstatic inline/g' cg/libcrux_sha3_avx2.h
-sed -i -E 's/static inline/__attribute__((target("avx2")))\nstatic inline/g' cg/libcrux_mlkem768_avx2.h
-
 sed -i -E 's/.*libcrux_ml_kem_types_MlKemCiphertext_s.*//g' cg/libcrux_core.h
 sed -i -E 's/.*Eurydice_error_t_cg_array.*//g' cg/libcrux_core.h
 sed -i -E 's/.*libcrux_ml_kem_types_MlKemCiphertext;//g' cg/libcrux_core.h
 sed -i -E 's/.*libcrux_ml_kem_ind_cca_MlKem_s.*//g' cg/libcrux_mlkem768_portable.h
 sed -i -E 's/.*libcrux_ml_kem_ind_cca_MlKem;//g' cg/libcrux_mlkem768_portable.h
-
-sed -i -E 's/static inline/__attribute__((noinline,unused)) static/g' cg/libcrux_ct_ops.h
 
 clang-format --style=Google -i cg/*.h
 
