663bbc1c5a54cc1f2e69be2fc9faa2f9420670ef
xvzcf <xvzcf@users.noreply.github.com>
2024-07-15 10:48:39 -0400

    Vectorized arithmetic.rs.

--- libcrux-ml-dsa/src/arithmetic.rs
+++ libcrux-ml-dsa/src/arithmetic.rs
@@ -1,5 +1,5 @@
 use crate::{
-    constants::{COEFFICIENTS_IN_RING_ELEMENT, FIELD_MODULUS},
+    constants::COEFFICIENTS_IN_RING_ELEMENT,
     polynomial::{PolynomialRingElement, SIMDPolynomialRingElement, SIMD_UNITS_IN_RING_ELEMENT},
     simd::{portable::PortableSIMDUnit, traits::Operations},
 };
@@ -79,69 +79,6 @@ pub(crate) fn power2round_vector<const DIMENSION: usize>(
     (vector_t0, vector_t1)
 }
 
-// Take a representative -q < r < q and convert it
-// to the standard unsigned one in the interval [0, q).
-//
-// Splits this representative r into r₀ and r₁ such that:
-//
-// - r = r₁*α + r₀
-// - -α/2 < r₀ ≤ α/2
-//
-// except when r₁ = (q-1)/α; in this case:
-//
-// - r₁ is set to 0 is taken
-// - α/2 ≤ r₀ < 0.
-//
-// Note that 0 ≤ r₁ < (q-1)/α.
-#[allow(non_snake_case)]
-#[inline(always)]
-fn decompose<const GAMMA2: i32>(r: i32) -> (i32, i32) {
-    debug_assert!(
-        r > -FIELD_MODULUS && r < FIELD_MODULUS,
-        "the representative is {}",
-        r
-    );
-
-    // Convert the signed representative to the standard unsigned one.
-    let r = r + ((r >> 31) & FIELD_MODULUS);
-
-    let ALPHA = GAMMA2 * 2;
-
-    let r1 = {
-        // Compute ⌈r / 128⌉
-        let ceil_of_r_by_128 = (r + 127) >> 7;
-
-        match ALPHA {
-            190_464 => {
-                // We approximate 1 / 1488 as:
-                // ⌊2²⁴ / 1488⌋ / 2²⁴ = 11,275 / 2²⁴
-                let result = ((ceil_of_r_by_128 * 11_275) + (1 << 23)) >> 24;
-
-                // For the corner-case a₁ = (q-1)/α = 44, we have to set a₁=0.
-                (result ^ (43 - result) >> 31) & result
-            }
-            523_776 => {
-                // We approximate 1 / 4092 as:
-                // ⌊2²² / 4092⌋ / 2²² = 1025 / 2²²
-                let result = (ceil_of_r_by_128 * 1025 + (1 << 21)) >> 22;
-
-                // For the corner-case a₁ = (q-1)/α = 16, we have to set a₁=0.
-                result & 15
-            }
-            _ => unreachable!(),
-        }
-    };
-
-    let mut r0 = r - (r1 * ALPHA);
-
-    // In the corner-case, when we set a₁=0, we will incorrectly
-    // have a₀ > (q-1)/2 and we'll need to subtract q.  As we
-    // return a₀ + q, that comes down to adding q if a₀ < (q-1)/2.
-    r0 -= (((FIELD_MODULUS - 1) / 2 - r0) >> 31) & FIELD_MODULUS;
-
-    (r0, r1)
-}
-
 #[inline(always)]
 pub(crate) fn decompose_vector<const DIMENSION: usize, const GAMMA2: i32>(
     t: [PolynomialRingElement; DIMENSION],
@@ -153,12 +90,20 @@ pub(crate) fn decompose_vector<const DIMENSION: usize, const GAMMA2: i32>(
     let mut vector_high = [PolynomialRingElement::ZERO; DIMENSION];
 
     for i in 0..DIMENSION {
-        for (j, coefficient) in t[i].coefficients.into_iter().enumerate() {
-            let (low, high) = decompose::<GAMMA2>(coefficient);
+        let v_t = SIMDPolynomialRingElement::<PortableSIMDUnit>::from_polynomial_ring_element(t[i]);
 
-            vector_low[i].coefficients[j] = low;
-            vector_high[i].coefficients[j] = high;
+        let mut v_low = SIMDPolynomialRingElement::ZERO();
+        let mut v_high = SIMDPolynomialRingElement::ZERO();
+
+        for j in 0..v_low.simd_units.len() {
+            let (low, high) = PortableSIMDUnit::decompose::<GAMMA2>(v_t.simd_units[j]);
+
+            v_low.simd_units[j] = low;
+            v_high.simd_units[j] = high;
         }
+
+        vector_low[i] = v_low.to_polynomial_ring_element();
+        vector_high[i] = v_high.to_polynomial_ring_element();
     }
 
     (vector_low, vector_high)
@@ -180,7 +125,7 @@ pub(crate) fn make_hint<const DIMENSION: usize, const GAMMA2: i32>(
 
         let mut v_hint = SIMDPolynomialRingElement::ZERO();
 
-        for j in 0..SIMD_UNITS_IN_RING_ELEMENT {
+        for j in 0..v_hint.simd_units.len() {
             let (one_hints_count, current_hint) =
                 PortableSIMDUnit::compute_hint::<GAMMA2>(v_low.simd_units[j], v_high.simd_units[j]);
             v_hint.simd_units[j] = current_hint;
@@ -195,41 +140,6 @@ pub(crate) fn make_hint<const DIMENSION: usize, const GAMMA2: i32>(
 }
 
 #[inline(always)]
-pub(crate) fn use_hint_value<const GAMMA2: i32>(r: i32, hint: i32) -> i32 {
-    let (r0, r1) = decompose::<GAMMA2>(r);
-
-    if hint == 0 {
-        return r1;
-    }
-
-    match GAMMA2 {
-        95_232 => {
-            if r0 > 0 {
-                if r1 == 43 {
-                    0
-                } else {
-                    r1 + 1
-                }
-            } else if r1 == 0 {
-                43
-            } else {
-                r1 - 1
-            }
-        }
-
-        261_888 => {
-            if r0 > 0 {
-                (r1 + 1) & 15
-            } else {
-                (r1 - 1) & 15
-            }
-        }
-
-        _ => unreachable!(),
-    }
-}
-
-#[inline(always)]
 pub(crate) fn use_hint<const DIMENSION: usize, const GAMMA2: i32>(
     hint: [[i32; COEFFICIENTS_IN_RING_ELEMENT]; DIMENSION],
     re_vector: [PolynomialRingElement; DIMENSION],
@@ -237,36 +147,23 @@ pub(crate) fn use_hint<const DIMENSION: usize, const GAMMA2: i32>(
     let mut result = [PolynomialRingElement::ZERO; DIMENSION];
 
     for i in 0..DIMENSION {
-        for j in 0..COEFFICIENTS_IN_RING_ELEMENT {
-            result[i].coefficients[j] =
-                use_hint_value::<GAMMA2>(re_vector[i].coefficients[j], hint[i][j]);
+        let v_re = SIMDPolynomialRingElement::<PortableSIMDUnit>::from_polynomial_ring_element(
+            re_vector[i],
+        );
+        let v_hint = SIMDPolynomialRingElement::<PortableSIMDUnit>::from_polynomial_ring_element(
+            PolynomialRingElement {
+                coefficients: hint[i],
+            },
+        );
+
+        let mut res = SIMDPolynomialRingElement::ZERO();
+        for j in 0..SIMD_UNITS_IN_RING_ELEMENT {
+            res.simd_units[j] =
+                PortableSIMDUnit::use_hint::<GAMMA2>(v_re.simd_units[j], v_hint.simd_units[j]);
         }
+
+        result[i] = res.to_polynomial_ring_element();
     }
 
     result
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_decompose() {
-        assert_eq!(decompose::<95_232>(3574899), (-43917, 19));
-        assert_eq!(decompose::<95_232>(7368323), (-59773, 39));
-        assert_eq!(decompose::<95_232>(3640854), (22038, 19));
-
-        assert_eq!(decompose::<261_888>(563751), (39975, 1));
-        assert_eq!(decompose::<261_888>(6645076), (-164012, 13));
-        assert_eq!(decompose::<261_888>(7806985), (-49655, 15));
-    }
-
-    #[test]
-    fn test_use_hint_value() {
-        assert_eq!(use_hint_value::<95_232>(7622170, 0), 40);
-        assert_eq!(use_hint_value::<95_232>(2332762, 1), 13);
-
-        assert_eq!(use_hint_value::<261_888>(7691572, 0), 15);
-        assert_eq!(use_hint_value::<261_888>(6635697, 1), 12);
-    }
 }
