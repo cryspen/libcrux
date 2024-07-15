bc1cac03781f4e09083787665db3ee1c97a8fb5c
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-07-15 09:14:25 +0000

    drop another unused sed from boring.sh

--- libcrux-ml-kem/boring.sh
+++ libcrux-ml-kem/boring.sh
@@ -13,8 +13,6 @@ cargo clean
 sed -i -E 's/.*libcrux_ml_kem_types_MlKemCiphertext_s.*//g' cg/libcrux_core.h
 sed -i -E 's/.*Eurydice_error_t_cg_array.*//g' cg/libcrux_core.h
 sed -i -E 's/.*libcrux_ml_kem_types_MlKemCiphertext;//g' cg/libcrux_core.h
-sed -i -E 's/.*libcrux_ml_kem_ind_cca_MlKem_s.*//g' cg/libcrux_mlkem768_portable.h
-sed -i -E 's/.*libcrux_ml_kem_ind_cca_MlKem;//g' cg/libcrux_mlkem768_portable.h
 
 clang-format --style=Google -i cg/*.h
 
