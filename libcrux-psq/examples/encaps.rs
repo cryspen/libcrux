84205e2457fa8abb85aa4c3e28174be57baf25c5
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-07-18 14:00:20 +0200

    Remove warnings about unused variables

--- libcrux-psq/examples/encaps.rs
+++ libcrux-psq/examples/encaps.rs
@@ -9,8 +9,8 @@ use rand::thread_rng;
 fn main() {
     let mut rng = thread_rng();
     let mlkem_keypair = generate_key_pair(Algorithm::MlKem768, &mut rng).unwrap();
-    let (receiver_dh_sk, receiver_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
-    let (initiator_dh_sk, initiator_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
+    let (_receiver_dh_sk, receiver_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
+    let (initiator_dh_sk, _initiator_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
 
     for _ in 0..100_000 {
         let _ = core::hint::black_box(send_ecdh_bound_psq(
