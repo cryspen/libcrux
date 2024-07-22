41e7bda2f0fbc5010a33a79e3fbe30bab7bdda91
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-07-22 11:39:52 +0200

    Usage example for PSQ

--- libcrux-psq/src/lib.rs
+++ libcrux-psq/src/lib.rs
@@ -4,6 +4,48 @@
 //! bound to specific outer protocol contexts. The goal is to harden the outer
 //! (potentially classical) protocol against harvest-now-decrypt-later quantum
 //! adversaries.
+//!
+//! Example usage of a PSK bound to an ECDH protocol context:
+//! ```
+//! use std::time::Duration;
+//!
+//! use libcrux_psq::{
+//!     ecdh_binder::{receive_ecdh_bound_psq, send_ecdh_bound_psq},
+//!     psq::{generate_key_pair, Algorithm},
+//! };
+//! use rand::thread_rng;
+//!
+//! fn main() {
+//!     let mut rng = thread_rng();
+//!     let (receiver_pqsk, receiver_pqpk) = generate_key_pair(Algorithm::MlKem768, &mut rng).unwrap();
+//!     let (receiver_dh_sk, receiver_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
+//!     let (initiator_dh_sk, _initiator_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
+//!
+//!     // Generate a PSK and PSK message bound to a specific ECDH key pair.
+//!     let (psk_initiator, ecdh_psk_message) = send_ecdh_bound_psq(
+//!         &receiver_pqpk,
+//!         &receiver_dh_pk,
+//!         &initiator_dh_sk,
+//!         Duration::from_secs(3600),
+//!         b"example context",
+//!         &mut rng,
+//!     )
+//!     .unwrap();
+//!
+//!     // Derive a PSK from the PSK message.
+//!     let psk_receiver = receive_ecdh_bound_psq(
+//!         &receiver_pqsk,
+//!         &receiver_pqpk,
+//!         &receiver_dh_sk,
+//!         &ecdh_psk_message,
+//!         b"example context",
+//!     )
+//!     .unwrap();
+//!
+//!     assert_eq!(psk_initiator, psk_receiver);
+//! }
+//! ```
+//!
 
 #![deny(missing_docs)]
 
