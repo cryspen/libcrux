4ce31766baf071d07fc26a4e0c4e6145595082c6
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-07-22 09:17:44 +0200

    Fix sed script path

--- libcrux-ml-kem/headers_kill_revs.sh
+++ libcrux-ml-kem/headers_kill_revs.sh
@@ -2,5 +2,7 @@
 # This script removes specific revision number lines from files that
 # have them. Its purpose is to allow diffing of extraction results
 # independent of concrete tool revs used for extraction.
+#
+# Run this from the repository root.
 
-grep -rl -e "This code was generated with the following revisions:" "$1" | xargs -d '\n' sed -i -f header_kill_revs.sed
+grep -rl -e "This code was generated with the following revisions:" "$1" | xargs -d '\n' sed -i -f libcrux-ml-kem/header_kill_revs.sed
