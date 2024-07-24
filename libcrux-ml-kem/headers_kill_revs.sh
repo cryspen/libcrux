daee7aee60fb903005d5217239106ee1609742e2
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-07-24 08:49:18 +0200

    Remove `code_gen.txt` before diffing

--- libcrux-ml-kem/headers_kill_revs.sh
+++ libcrux-ml-kem/headers_kill_revs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # This script removes specific revision number lines from files that
-# have them. Its purpose is to allow diffing of extraction results
-# independent of concrete tool revs used for extraction.
+# have them, as well as removing the `code_gen.txt` file that also
+# stores only this information. Its purpose is to prepare two
+# extractions for diffing of extraction results independent of
+# concrete tool revs used for extraction.
 #
 # Run this from the repository root.
 
 grep -rl -e "This code was generated with the following revisions:" "$1" | xargs -d '\n' sed -i -f libcrux-ml-kem/header_kill_revs.sed
+rm "$1/code_gen.txt"
