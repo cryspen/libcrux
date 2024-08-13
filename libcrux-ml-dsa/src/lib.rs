66c395b33cd44f5d328eb52a5beee79cc47ce7c7
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-08-13 15:01:24 +0200

    Expose key generation and signing randomness requirement constants

--- libcrux-ml-dsa/src/lib.rs
+++ libcrux-ml-dsa/src/lib.rs
@@ -17,6 +17,9 @@ mod utils;
 
 pub use {ml_dsa_generic::VerificationError, types::*};
 
+pub use crate::constants::KEY_GENERATION_RANDOMNESS_SIZE;
+pub use crate::constants::SIGNING_RANDOMNESS_SIZE;
+
 pub mod ml_dsa_44;
 pub mod ml_dsa_65;
 pub mod ml_dsa_87;
