5b5b44c0d2000609e5ada7fcba3ef47a3486259d
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-08-13 11:15:57 +0200

    Remove feature gated import

--- libcrux-sha3/src/lib.rs
+++ libcrux-sha3/src/lib.rs
@@ -936,7 +936,6 @@ pub mod avx2 {
                 absorb_final, squeeze_first_three_blocks, squeeze_next_block,
                 KeccakState as GenericState,
             };
-            #[cfg(not(feature = "simd128"))]
             use crate::generic_keccak::{squeeze_first_block, squeeze_first_five_blocks};
             #[cfg(feature = "simd256")]
             use libcrux_intrinsics::avx2::*;
