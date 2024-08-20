66bd4d4b12caf8b96582a7b76b5a1e2daf132927
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-08-20 15:09:54 +0200

    PSQ registration protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,7 @@
-//! # Modular PQ-PSK Protocol
+//! # PQ-PSK
 //!
-//! This crate implements a method to generate post-quantum pre-shared keys
-//! bound to specific outer protocol contexts. The goal is to harden the outer
-//! (potentially classical) protocol against harvest-now-decrypt-later quantum
-//! adversaries.
-//!
-//! Example usage of a PSK bound to an ECDH protocol context:
-//! ```
-//! use std::time::Duration;
-//!
-//! use libcrux_psq::{
-//!     ecdh_binder::{receive_ecdh_bound_psq, send_ecdh_bound_psq},
-//!     psq::{generate_key_pair, Algorithm},
-//! };
-//! use rand::thread_rng;
-//!
-//! fn main() {
-//!     let mut rng = thread_rng();
-//!     let (receiver_pqsk, receiver_pqpk) = generate_key_pair(Algorithm::MlKem768, &mut rng).unwrap();
-//!     let (receiver_dh_sk, receiver_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
-//!     let (initiator_dh_sk, _initiator_dh_pk) = libcrux_ecdh::x25519_key_gen(&mut rng).unwrap();
-//!
-//!     // Generate a PSK and PSK message bound to a specific ECDH key pair.
-//!     let (psk_initiator, ecdh_psk_message) = send_ecdh_bound_psq(
-//!         &receiver_pqpk,
-//!         &receiver_dh_pk,
-//!         &initiator_dh_sk,
-//!         Duration::from_secs(3600),
-//!         b"example context",
-//!         &mut rng,
-//!     )
-//!     .unwrap();
-//!
-//!     // Derive a PSK from the PSK message.
-//!     let psk_receiver = receive_ecdh_bound_psq(
-//!         &receiver_pqsk,
-//!         &receiver_pqpk,
-//!         &receiver_dh_sk,
-//!         &ecdh_psk_message,
-//!         b"example context",
-//!     )
-//!     .unwrap();
-//!
-//!     assert_eq!(psk_initiator, psk_receiver);
-//! }
-//! ```
-//!
+//! This crate implements a protocol for establishing and mutually
+//! registering a PQ-PSK between an initiator and a responder.
 
 #![deny(missing_docs)]
 
@@ -60,8 +16,10 @@ pub enum Error {
     PSQGenerationError,
     /// An error during PSK decapsulation
     PSQDerivationError,
+    /// An error during credential processing
+    CredError,
     /// An error during binder computation
-    BinderError,
+    RegistrationError,
     /// An error in the underlying cryptographic algorithms
     CryptoError,
     /// Error relating to operating system environment
@@ -77,5 +35,6 @@ impl From<libcrux_kem::Error> for Error {
 const PSK_LENGTH: usize = 32;
 type Psk = [u8; PSK_LENGTH];
 
-pub mod ecdh_binder;
-pub mod psq;
+pub mod cred;
+pub mod psk_registration;
+mod psq;
