4ad532b206174114dd4140b718e7794a28fc59ee
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-08-28 18:34:16 +0000

    add a single kat for dk validation

--- libcrux-ml-kem/tests/ml-kem.rs
+++ libcrux-ml-kem/tests/ml-kem.rs
@@ -14,6 +14,7 @@ fn test_invalid_modulus(p: &str) {
     for line in reader.lines() {
         let line = line.unwrap();
         let pk = hex::decode(line).unwrap();
+        #[allow(dead_code)]
         let pk = pk.as_slice();
         match p {
             #[cfg(all(feature = "mlkem512", feature = "pre-verification"))]
@@ -56,4 +57,66 @@ fn file_name(p: &str) -> std::path::PathBuf {
         .join("kats")
         .join("invalid_modulus")
         .join(format!("ML-KEM-{}.txt", p))
+}
+
+#[allow(dead_code)]
+fn test_invalid_dk(p: &str) {
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+    };
+
+    fn dk_file(p: &str) -> std::path::PathBuf {
+        std::path::Path::new("tests")
+            .join("kats")
+            .join("invalid_dk")
+            .join(format!("ML-KEM-{}.txt", p))
+    }
+
+    let kat_file_path = dk_file(p);
+    let kat_file = File::open(kat_file_path).unwrap();
+    let reader = BufReader::new(kat_file);
+    let mut lines = reader.lines();
+    while let (Some(dk_string), Some(ct_string)) = (lines.next(), lines.next()) {
+        let dk = hex::decode(dk_string.unwrap()).unwrap();
+        let dk = dk.as_slice();
+        let ct = hex::decode(ct_string.unwrap()).unwrap();
+        let ct = ct.as_slice();
+        match p {
+            #[cfg(all(feature = "mlkem512", feature = "pre-verification"))]
+            "512" => assert!(!libcrux_ml_kem::mlkem512::validate_private_key(
+                &dk.try_into().unwrap(),
+                &ct.try_into().unwrap(),
+            )),
+            #[cfg(all(feature = "mlkem768", feature = "pre-verification"))]
+            "768" => assert!(!libcrux_ml_kem::mlkem768::validate_private_key(
+                &dk.try_into().unwrap(),
+                &ct.try_into().unwrap(),
+            )),
+            #[cfg(all(feature = "mlkem1024", feature = "pre-verification"))]
+            "1024" => assert!(!libcrux_ml_kem::mlkem1024::validate_private_key(
+                &dk.try_into().unwrap(),
+                &ct.try_into().unwrap(),
+            )),
+            _ => unreachable!(),
+        };
+    }
+}
+
+#[test]
+#[cfg(all(feature = "mlkem512", feature = "pre-verification"))]
+fn invalid_dk_512() {
+    test_invalid_dk("512");
+}
+
+#[test]
+#[cfg(all(feature = "mlkem768", feature = "pre-verification"))]
+fn invalid_dk_768() {
+    test_invalid_dk("768");
+}
+
+#[test]
+#[cfg(all(feature = "mlkem1024", feature = "pre-verification"))]
+fn invalid_dk_1024() {
+    test_invalid_dk("1024");
 }
