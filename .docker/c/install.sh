a39bda0ca1b44ecc1c88156a375e760db9c37aaf
Franziskus Kiefer <franziskuskiefer@gmail.com>
2024-09-02 10:03:30 +0000

    update docker

--- .docker/c/install.sh
+++ .docker/c/install.sh
@@ -25,30 +25,8 @@ unzip hacl-star.zip
 rm -rf hacl-star.zip
 mv hacl-star-2a8b61343a1a7232611cb763b0dc3e4dff84d656/ hacl-star
 
-curl -L https://github.com/AeneasVerif/charon/archive/b351338f6a84c7a1afc27433eb0ffdc668b3581d.zip \
-    --output charon.zip
-unzip charon.zip
-rm -rf charon.zip
-mv charon-b351338f6a84c7a1afc27433eb0ffdc668b3581d/ charon
-
-
-curl -L https://github.com/FStarLang/karamel/archive/c96fb69d15693284644d6aecaa90afa37e4de8f0.zip \
-    --output karamel.zip
-unzip karamel.zip
-rm -rf karamel.zip
-mv karamel-c96fb69d15693284644d6aecaa90afa37e4de8f0/ karamel
-
-curl -L https://github.com/AeneasVerif/eurydice/archive/7efec1624422fd5e94388ef06b9c76dfe7a48d46.zip \
-    --output eurydice.zip
-unzip eurydice.zip
-rm -rf eurydice.zip
-mv eurydice-7efec1624422fd5e94388ef06b9c76dfe7a48d46/ eurydice
-
 echo "export FSTAR_HOME=$HOME/fstar" >>$HOME/.profile
 echo "export HACL_HOME=$HOME/hacl-star" >>$HOME/.profile
-echo "export KRML_HOME=$HOME/karamel" >>$HOME/.profile
-echo "export EURYDICE_HOME=$HOME/eurydice" >>$HOME/.profile
-echo "export CHARON_HOME=$HOME/charon" >>$HOME/.profile
 echo "export HAX_HOME=$HOME/hax" >>$HOME/.profile
 echo "export PATH=\"${PATH}:$HOME/fstar/bin:$HOME/z3/bin\"" >>$HOME/.profile
 echo "[[ ! -r /home/$USER/.opam/opam-init/init.sh ]] || source /home/$USER/.opam/opam-init/init.sh  > /dev/null 2> /dev/null" >>$HOME/.profile
@@ -56,21 +34,3 @@ echo "[[ ! -r /home/$USER/.opam/opam-init/init.sh ]] || source /home/$USER/.opam
 source $HOME/.profile
 opam install --yes ocamlfind visitors menhir ppx_deriving_yojson core_unix sedlex wasm fix process pprint zarith yaml easy_logging terminal
 eval $(opam env)
-
-# Build everything
-cd karamel
-make -j
-cd -
-
-cd charon
-make -j
-cd -
-
-cd eurydice/lib
-rm -f charon
-ln -s $CHARON_HOME/charon-ml charon
-rm -f krml
-ln -s $KRML_HOME/lib krml
-cd ../
-make -j
-cd ../
