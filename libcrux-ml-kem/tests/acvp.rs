e16df242e8b3f33656f381988b58968fb8d5e88c
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-07 20:20:35 +0200

    Only run ACVP when `pre-verification && (mlkem512|768|1024)` are set

--- libcrux-ml-kem/tests/acvp.rs
+++ libcrux-ml-kem/tests/acvp.rs
@@ -1,4 +1,7 @@
-#![cfg(feature = "pre-verification")]
+#![cfg(all(
+    feature = "pre-verification",
+    any(feature = "mlkem512", feature = "mlkem768", feature = "mlkem1024",)
+))]
 
 use serde::{de::DeserializeOwned, Deserialize};
 use serde_json::Value;
