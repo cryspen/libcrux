ed01b801161e599932bedc2d9427f9dbafb9dbe2
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-10-07 16:48:04 +0200

    ML-KEM: ACVP vectors for encapsulation / decapsulation

--- libcrux-ml-kem/tests/acvp.rs
+++ libcrux-ml-kem/tests/acvp.rs
@@ -101,7 +101,7 @@ fn keygen() {
                 assert_eq!(result.dk, keys.sk());
             }
 
-            let expectred_result = results
+            let expected_result = results
                 .testGroups
                 .iter()
                 .find(|tg| tg.tgId == kat.tgId)
@@ -113,11 +113,11 @@ fn keygen() {
 
             match parameter_set.as_str() {
                 #[cfg(feature = "mlkem512")]
-                "ML-KEM-512" => check(mlkem512::generate_key_pair(seed), expectred_result),
+                "ML-KEM-512" => check(mlkem512::generate_key_pair(seed), expected_result),
                 #[cfg(feature = "mlkem768")]
-                "ML-KEM-768" => check(mlkem768::generate_key_pair(seed), expectred_result),
+                "ML-KEM-768" => check(mlkem768::generate_key_pair(seed), expected_result),
                 #[cfg(feature = "mlkem1024")]
-                "ML-KEM-1024" => check(mlkem1024::generate_key_pair(seed), expectred_result),
+                "ML-KEM-1024" => check(mlkem1024::generate_key_pair(seed), expected_result),
                 _ => unimplemented!(),
             }
         }
@@ -157,93 +157,186 @@ struct DecapPrompt {
     c: Vec<u8>,
 }
 
-// #[derive(Deserialize)]
-// #[allow(non_snake_case, dead_code)]
-// struct EncapDecapPromptTestGroup {
-//     tgId: usize,
-//     testType: String,
-//     parameterSet: String,
-//     function: String,
-//     tests: Vec<EncapDecapPrompt>,
-// }
+#[derive(Deserialize)]
+#[allow(non_snake_case, dead_code)]
+struct EncapDecapPromptTestGroup {
+    tgId: usize,
+    testType: String,
+    parameterSet: String,
+    #[serde(flatten)]
+    tests: EncapDecapTests,
+}
 
-// #[derive(Deserialize)]
-// #[allow(non_snake_case, dead_code)]
-// struct EncapDecapResult {
-//     tcId: usize,
+#[allow(non_snake_case, dead_code)]
+#[derive(Deserialize)]
+#[serde(tag = "function")]
+enum EncapDecapTests {
+    #[serde(rename(deserialize = "encapsulation"))]
+    EncapTests { tests: Vec<EncapPrompt> },
+    #[serde(rename(deserialize = "decapsulation"))]
+    DecapTests {
+        #[serde(with = "hex::serde")]
+        dk: Vec<u8>,
+        tests: Vec<DecapPrompt>,
+    },
+}
 
-//     #[serde(with = "hex::serde")]
-//     ek: Vec<u8>,
+#[derive(Deserialize)]
+#[serde(untagged)]
+#[allow(non_snake_case, dead_code)]
+enum EncapDecapResult {
+    EncapResult {
+        tcId: usize,
+        #[serde(with = "hex::serde")]
+        c: Vec<u8>,
+        #[serde(with = "hex::serde")]
+        k: Vec<u8>,
+    },
+    DecapResult {
+        tcId: usize,
+        #[serde(with = "hex::serde")]
+        k: Vec<u8>,
+    },
+}
 
-//     #[serde(with = "hex::serde")]
-//     dk: Vec<u8>,
-// }
-
-// #[derive(Deserialize)]
-// #[allow(non_snake_case, dead_code)]
-// struct ResultEncapDedapTestGroup {
-//     tgId: usize,
-//     tests: Vec<KeyGenResult>,
-// }
+#[derive(Deserialize)]
+#[allow(non_snake_case, dead_code)]
+struct ResultEncapDecapTestGroup {
+    tgId: usize,
+    tests: Vec<EncapDecapResult>,
+}
 
 #[test]
 fn encap_decap() {
     use libcrux_ml_kem::*;
 
-    let prompts: Prompts<Value> = read("encap-decap", "prompt.json");
+    let prompts: Prompts<EncapDecapPromptTestGroup> = read("encap-decap", "prompt.json");
     assert!(prompts.algorithm == "ML-KEM");
     assert!(prompts.revision == "FIPS203");
 
-    let results: Results<Value> = read("encap-decap", "expectedResults.json");
+    let results: Results<ResultEncapDecapTestGroup> = read("encap-decap", "expectedResults.json");
     assert!(results.algorithm == "ML-KEM");
     assert!(results.revision == "FIPS203");
 
     for kat in prompts.testGroups {
-        let parameter_set = kat.get("parameterSet").unwrap().as_str().unwrap();
+        let parameter_set = kat.parameterSet;
         assert!(
             parameter_set == "ML-KEM-512"
                 || parameter_set == "ML-KEM-768"
                 || parameter_set == "ML-KEM-1024"
         );
-        eprintln!(
-            "{parameter_set} {}",
-            kat.get("function").unwrap().as_str().unwrap()
-        );
+        eprintln!("{parameter_set}");
 
-        let mut tests = kat.get("tests").unwrap().as_array().unwrap();
-        for test in tests {
-            eprintln!("  {}", test.get("tcId").unwrap());
-            //             let mut seed = [0u8; 64];
-            //             seed[..32].copy_from_slice(&test.d);
-            //             seed[32..].copy_from_slice(&test.z);
+        match kat.tests {
+            EncapDecapTests::EncapTests { tests } => {
+                for test in tests {
+                    let expected_result = results
+                        .testGroups
+                        .iter()
+                        .find(|tg| tg.tgId == kat.tgId)
+                        .unwrap()
+                        .tests
+                        .iter()
+                        .find(|t| {
+                            if let EncapDecapResult::EncapResult { tcId, .. } = t {
+                                *tcId == test.tcId
+                            } else {
+                                false
+                            }
+                        })
+                        .unwrap();
 
-            //             fn check<const EK_LEN: usize, const DK_LEN: usize>(
-            //                 keys: MlKemKeyPair<DK_LEN, EK_LEN>,
-            //                 result: &KeyGenResult,
-            //             ) {
-            //                 assert_eq!(result.ek, keys.pk());
-            //                 assert_eq!(result.dk, keys.sk());
-            //             }
+                    if let EncapDecapResult::EncapResult { c, k, .. } = expected_result {
+                        let ek = test.ek;
+                        let randomness = test.m;
+                        match parameter_set.as_str() {
+                            #[cfg(feature = "mlkem512")]
+                            "ML-KEM-512" => {
+                                let (actual_ct, actual_k) = mlkem512::encapsulate(
+                                    &mlkem512::MlKem512PublicKey::try_from(ek.as_slice()).unwrap(),
+                                    randomness,
+                                );
+                                assert_eq!(actual_ct.as_ref(), c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
+                            #[cfg(feature = "mlkem768")]
+                            "ML-KEM-768" => {
+                                let (actual_ct, actual_k) = mlkem768::encapsulate(
+                                    &mlkem768::MlKem768PublicKey::try_from(ek.as_slice()).unwrap(),
+                                    randomness,
+                                );
+                                assert_eq!(actual_ct.as_ref(), c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
+                            #[cfg(feature = "mlkem1024")]
+                            "ML-KEM-1024" => {
+                                let (actual_ct, actual_k) = mlkem1024::encapsulate(
+                                    &mlkem1024::MlKem1024PublicKey::try_from(ek.as_slice())
+                                        .unwrap(),
+                                    randomness,
+                                );
+                                assert_eq!(actual_ct.as_ref(), c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
+                            _ => unimplemented!(),
+                        }
+                    }
+                }
+            }
+            EncapDecapTests::DecapTests { dk, tests } => {
+                for test in tests {
+                    let expected_result = results
+                        .testGroups
+                        .iter()
+                        .find(|tg| tg.tgId == kat.tgId)
+                        .unwrap()
+                        .tests
+                        .iter()
+                        .find(|t| {
+                            if let EncapDecapResult::DecapResult { tcId, .. } = t {
+                                *tcId == test.tcId
+                            } else {
+                                false
+                            }
+                        })
+                        .unwrap();
 
-            //             let expectred_result = results
-            //                 .testGroups
-            //                 .iter()
-            //                 .find(|tg| tg.tgId == kat.tgId)
-            //                 .unwrap()
-            //                 .tests
-            //                 .iter()
-            //                 .find(|t| t.tcId == test.tcId)
-            //                 .unwrap();
+                    if let EncapDecapResult::DecapResult { k, .. } = expected_result {
+                        let c = test.c;
+                        match parameter_set.as_str() {
+                            #[cfg(feature = "mlkem512")]
+                            "ML-KEM-512" => {
+                                let dk =
+                                    mlkem512::MlKem512PrivateKey::try_from(dk.as_slice()).unwrap();
+                                let c =
+                                    mlkem512::MlKem512Ciphertext::try_from(c.as_slice()).unwrap();
+                                let actual_k = mlkem512::decapsulate(&dk, &c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
+                            #[cfg(feature = "mlkem768")]
+                            "ML-KEM-768" => {
+                                let dk =
+                                    mlkem768::MlKem768PrivateKey::try_from(dk.as_slice()).unwrap();
+                                let c =
+                                    mlkem768::MlKem768Ciphertext::try_from(c.as_slice()).unwrap();
+                                let actual_k = mlkem768::decapsulate(&dk, &c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
+                            #[cfg(feature = "mlkem1024")]
+                            "ML-KEM-1024" => {
+                                let dk = mlkem1024::MlKem1024PrivateKey::try_from(dk.as_slice())
+                                    .unwrap();
+                                let c =
+                                    mlkem1024::MlKem1024Ciphertext::try_from(c.as_slice()).unwrap();
+                                let actual_k = mlkem1024::decapsulate(&dk, &c);
+                                assert_eq!(actual_k.as_ref(), k);
+                            }
 
-            //             match parameter_set.as_str() {
-            //                 #[cfg(feature = "mlkem512")]
-            //                 "ML-KEM-512" => check(mlkem512::generate_key_pair(seed), expectred_result),
-            //                 #[cfg(feature = "mlkem768")]
-            //                 "ML-KEM-768" => check(mlkem768::generate_key_pair(seed), expectred_result),
-            //                 #[cfg(feature = "mlkem1024")]
-            //                 "ML-KEM-1024" => check(mlkem1024::generate_key_pair(seed), expectred_result),
-            //                 _ => unimplemented!(),
-            //             }
+                            _ => unimplemented!(),
+                        }
+                    }
+                }
+            }
         }
     }
 }
