ed655dadcfb9b2e20f07da752c069632c9235aa4
Jan Winkelmann (keks) <jan@cryspen.com>
2024-11-05 18:23:32 +0100

    use ed25519 from hacl-rs

--- libcrux-hacl-rs/src/lib.rs
+++ libcrux-hacl-rs/src/lib.rs
@@ -1,9 +1,19 @@
 // Utility modules. In the generated hacl-rs, these are individual crates.
+mod bignum;
 mod fstar;
 mod lowstar;
+mod util;
 
 pub mod hash_sha1;
 pub mod hash_sha2;
-//pub(crate) mod hkdf;
 pub mod hmac;
 pub mod streaming_types;
+
+pub mod bignum25519_51;
+pub mod curve25519_51;
+pub mod ed25519;
+pub mod ed25519_precomptable;
+
+// things that still are broken:
+//
+//pub(crate) mod hkdf;
