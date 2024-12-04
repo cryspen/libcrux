52178f67f153cfcdbf0440e8f5fadc0ba0872152
Karthikeyan Bhargavan <karthik.bhargavan@gmail.com>
2024-12-04 11:05:08 +0100

    F* update

--- libcrux-ml-kem/src/polynomial.rs
+++ libcrux-ml-kem/src/polynomial.rs
@@ -53,13 +53,203 @@ pub(crate) const VECTORS_IN_RING_ELEMENT: usize =
         "let to_spec_poly_t (#v_Vector: Type0)
     {| i2: Libcrux_ml_kem.Vector.Traits.t_Operations v_Vector |}
     (p: t_PolynomialRingElement v_Vector) : Spec.MLKEM.polynomial =
-    admit()"
+    createi (sz 256) (fun i -> Spec.MLKEM.Math.to_spec_fe 
+                                (Seq.index (i2._super_8706949974463268012.f_repr 
+                                    (Seq.index p.f_coefficients (v i / 16))) (v i % 16)))"
     )
 )]
 // XXX: We don't want to copy this. But for eurydice we have to have this.
 #[derive(Clone, Copy)]
 pub(crate) struct PolynomialRingElement<Vector: Operations> {
     pub(crate) coefficients: [Vector; VECTORS_IN_RING_ELEMENT],
+}
+
+
+#[allow(non_snake_case)]
+pub(crate) fn ZERO<Vector: Operations>() -> PolynomialRingElement<Vector> {
+    PolynomialRingElement {
+        // FIXME:  The THIR body of item DefId(0:415 ~ libcrux_ml_kem[9000]::polynomial::{impl#0}::ZERO::{constant#0}) was stolen.
+        coefficients: [Vector::ZERO(); 16],
+    }
+}
+
+#[inline(always)]
+#[hax_lib::requires(VECTORS_IN_RING_ELEMENT * 16 <= a.len())]
+pub(crate) fn from_i16_array<Vector: Operations>(a: &[i16]) -> PolynomialRingElement<Vector> {
+    let mut result = ZERO();
+    for i in 0..VECTORS_IN_RING_ELEMENT {
+        result.coefficients[i] = Vector::from_i16_array(&a[i * 16..(i + 1) * 16]);
+    }
+    result
+}
+
+/// Given two polynomial ring elements `lhs` and `rhs`, compute the pointwise
+/// sum of their constituent coefficients.
+#[inline(always)]
+#[hax_lib::fstar::verification_status(panic_free)]
+pub(crate) fn add_to_ring_element<Vector: Operations, const K: usize>(myself: &mut PolynomialRingElement<Vector>, rhs: &PolynomialRingElement<Vector>) {
+    // The semicolon and parentheses at the end of loop are a workaround
+    // for the following bug https://github.com/hacspec/hax/issues/720
+    for i in 0..myself.coefficients.len() {
+        myself.coefficients[i] = Vector::add(myself.coefficients[i], &rhs.coefficients[i]);
+    }
+    ()
+}
+
+#[inline(always)]
+pub fn poly_barrett_reduce<Vector: Operations>(myself: &mut PolynomialRingElement<Vector>) {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    // The semicolon and parentheses at the end of loop are a workaround
+    // for the following bug https://github.com/hacspec/hax/issues/720
+    for i in 0..VECTORS_IN_RING_ELEMENT {
+        myself.coefficients[i] = Vector::barrett_reduce(myself.coefficients[i]);
+    }
+    ()
+}
+
+#[inline(always)]
+pub(crate) fn subtract_reduce<Vector: Operations>(myself: &PolynomialRingElement<Vector>, mut b: PolynomialRingElement<Vector>) -> PolynomialRingElement<Vector> {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    for i in 0..VECTORS_IN_RING_ELEMENT {
+        let coefficient_normal_form =
+            Vector::montgomery_multiply_by_constant(b.coefficients[i], 1441);
+        b.coefficients[i] =
+            Vector::barrett_reduce(Vector::sub(myself.coefficients[i], &coefficient_normal_form));
+    }
+    b
+}
+
+#[inline(always)]
+pub(crate) fn add_message_error_reduce<Vector: Operations>(myself: &PolynomialRingElement<Vector>, message: &PolynomialRingElement<Vector>, mut result: PolynomialRingElement<Vector>) -> PolynomialRingElement<Vector> {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    for i in 0..VECTORS_IN_RING_ELEMENT {
+        let coefficient_normal_form =
+            Vector::montgomery_multiply_by_constant(result.coefficients[i], 1441);
+
+        // FIXME: Eurydice crashes with:
+        //
+        // Warning 11: in top-level declaration libcrux_ml_kem.polynomial.{libcrux_ml_kem::polynomial::PolynomialRingElement<Vector>[TraitClause@0]}.add_message_error_reduce__libcrux_ml_kem_libcrux_polynomials_PortableVector: this expression is not Low*; the enclosing function cannot be translated into C*: let mutable ret(Mark.Present,(Mark.AtMost 2), ): int16_t[16size_t] = $any in
+        // libcrux_ml_kem.libcrux_polynomials.{(libcrux_ml_kem::libcrux_polynomials::libcrux_traits::Operations␣for␣libcrux_ml_kem::libcrux_polynomials::PortableVector)}.add ((@9: libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t]*)[0uint32_t]:int16_t[16size_t][16size_t])[@4] &(((@8: libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t]*)[0uint32_t]:libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t])[@4]) @0;
+        // @0
+        // Warning 11 is fatal, exiting.
+        //
+        // On the following code:
+
+        // ```rust
+        // result.coefficients[i] = Vector::barrett_reduce(Vector::add(
+        //     coefficient_normal_form,
+        //     &Vector::add(myself.coefficients[i], &message.coefficients[i]),
+        // ));
+        // ```
+
+        let tmp = Vector::add(myself.coefficients[i], &message.coefficients[i]);
+        let tmp = Vector::add(coefficient_normal_form, &tmp);
+        result.coefficients[i] = Vector::barrett_reduce(tmp);
+    }
+    result
+}
+
+#[inline(always)]
+pub(crate) fn add_error_reduce<Vector: Operations>(myself: &mut PolynomialRingElement<Vector>, error: &PolynomialRingElement<Vector>) {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    // The semicolon and parentheses at the end of loop are a workaround
+    // for the following bug https://github.com/hacspec/hax/issues/720
+    for j in 0..VECTORS_IN_RING_ELEMENT {
+        let coefficient_normal_form =
+            Vector::montgomery_multiply_by_constant(myself.coefficients[j], 1441);
+
+        myself.coefficients[j] = Vector::barrett_reduce(Vector::add(
+            coefficient_normal_form,
+            &error.coefficients[j],
+        ));
+    }
+    ()
+}
+
+#[inline(always)]
+pub(crate) fn add_standard_error_reduce<Vector: Operations>(myself: &mut PolynomialRingElement<Vector>, error: &PolynomialRingElement<Vector>) {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    // The semicolon and parentheses at the end of loop are a workaround
+    // for the following bug https://github.com/hacspec/hax/issues/720
+    for j in 0..VECTORS_IN_RING_ELEMENT {
+        // The coefficients are of the form aR^{-1} mod q, which means
+        // calling to_montgomery_domain() on them should return a mod q.
+        let coefficient_normal_form = to_standard_domain::<Vector>(myself.coefficients[j]);
+
+        myself.coefficients[j] = Vector::barrett_reduce(Vector::add(
+            coefficient_normal_form,
+            &error.coefficients[j],
+        ));
+    }
+    ()
+}
+
+/// Given two `KyberPolynomialRingElement`s in their NTT representations,
+/// compute their product. Given two polynomials in the NTT domain `f^` and `ĵ`,
+/// the `iᵗʰ` coefficient of the product `k̂` is determined by the calculation:
+///
+/// ```plaintext
+/// ĥ[2·i] + ĥ[2·i + 1]X = (f^[2·i] + f^[2·i + 1]X)·(ĝ[2·i] + ĝ[2·i + 1]X) mod (X² - ζ^(2·BitRev₇(i) + 1))
+/// ```
+///
+/// This function almost implements <strong>Algorithm 10</strong> of the
+/// NIST FIPS 203 standard, which is reproduced below:
+///
+/// ```plaintext
+/// Input: Two arrays fˆ ∈ ℤ₂₅₆ and ĝ ∈ ℤ₂₅₆.
+/// Output: An array ĥ ∈ ℤq.
+///
+/// for(i ← 0; i < 128; i++)
+///     (ĥ[2i], ĥ[2i+1]) ← BaseCaseMultiply(fˆ[2i], fˆ[2i+1], ĝ[2i], ĝ[2i+1], ζ^(2·BitRev₇(i) + 1))
+/// end for
+/// return ĥ
+/// ```
+/// We say "almost" because the coefficients of the ring element output by
+/// this function are in the Montgomery domain.
+///
+/// The NIST FIPS 203 standard can be found at
+/// <https://csrc.nist.gov/pubs/fips/203/ipd>.
+// TODO: Remove or replace with something that works and is useful for the proof.
+// #[cfg_attr(hax, hax_lib::requires(
+//     hax_lib::forall(|i:usize|
+//         hax_lib::implies(i < COEFFICIENTS_IN_RING_ELEMENT, ||
+//             (lhs.coefficients[i] >= 0 && lhs.coefficients[i] < 4096) &&
+//             (rhs.coefficients[i].abs() <= FIELD_MODULUS)
+
+// ))))]
+// #[cfg_attr(hax, hax_lib::ensures(|result|
+//     hax_lib::forall(|i:usize|
+//         hax_lib::implies(i < result.coefficients.len(), ||
+//                 result.coefficients[i].abs() <= FIELD_MODULUS
+// ))))]
+#[inline(always)]
+pub(crate) fn ntt_multiply<Vector: Operations>(myself: &PolynomialRingElement<Vector>, rhs: &PolynomialRingElement<Vector>) -> PolynomialRingElement<Vector> {
+    // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
+    hax_lib::fstar!("admit ()");
+    // hax_debug_debug_assert!(lhs
+    //     .coefficients
+    //     .into_iter()
+    //     .all(|coefficient| coefficient >= 0 && coefficient < 4096));
+
+    let mut out = ZERO();
+
+    for i in 0..VECTORS_IN_RING_ELEMENT {
+        out.coefficients[i] = Vector::ntt_multiply(
+            &myself.coefficients[i],
+            &rhs.coefficients[i],
+            get_zeta(64 + 4 * i),
+            get_zeta(64 + 4 * i + 1),
+            get_zeta(64 + 4 * i + 2),
+            get_zeta(64 + 4 * i + 3),
+        );
+    }
+
+    out
 }
 
 #[hax_lib::attributes]
@@ -75,179 +265,43 @@ impl<Vector: Operations> PolynomialRingElement<Vector> {
     #[inline(always)]
     #[requires(VECTORS_IN_RING_ELEMENT * 16 <= a.len())]
     pub(crate) fn from_i16_array(a: &[i16]) -> Self {
-        let mut result = PolynomialRingElement::ZERO();
-        for i in 0..VECTORS_IN_RING_ELEMENT {
-            result.coefficients[i] = Vector::from_i16_array(&a[i * 16..(i + 1) * 16]);
-        }
-        result
+        from_i16_array(a)
     }
 
     /// Given two polynomial ring elements `lhs` and `rhs`, compute the pointwise
     /// sum of their constituent coefficients.
     #[inline(always)]
     pub(crate) fn add_to_ring_element<const K: usize>(&mut self, rhs: &Self) {
-        hax_lib::fstar!("admit ()");
-        // The semicolon and parentheses at the end of loop are a workaround
-        // for the following bug https://github.com/hacspec/hax/issues/720
-        for i in 0..self.coefficients.len() {
-            self.coefficients[i] = Vector::add(self.coefficients[i], &rhs.coefficients[i]);
-        }
-        ()
+        add_to_ring_element::<Vector,K>(self, rhs);
     }
 
     #[inline(always)]
     pub fn poly_barrett_reduce(&mut self) {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        // The semicolon and parentheses at the end of loop are a workaround
-        // for the following bug https://github.com/hacspec/hax/issues/720
-        for i in 0..VECTORS_IN_RING_ELEMENT {
-            self.coefficients[i] = Vector::barrett_reduce(self.coefficients[i]);
-        }
-        ()
+        poly_barrett_reduce(self);
     }
 
     #[inline(always)]
-    pub(crate) fn subtract_reduce(&self, mut b: Self) -> Self {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        for i in 0..VECTORS_IN_RING_ELEMENT {
-            let coefficient_normal_form =
-                Vector::montgomery_multiply_by_constant(b.coefficients[i], 1441);
-            b.coefficients[i] =
-                Vector::barrett_reduce(Vector::sub(self.coefficients[i], &coefficient_normal_form));
-        }
-        b
+    pub(crate) fn subtract_reduce(&self, b: Self) -> Self {
+        subtract_reduce(self, b)
     }
 
     #[inline(always)]
-    pub(crate) fn add_message_error_reduce(&self, message: &Self, mut result: Self) -> Self {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        for i in 0..VECTORS_IN_RING_ELEMENT {
-            let coefficient_normal_form =
-                Vector::montgomery_multiply_by_constant(result.coefficients[i], 1441);
-
-            // FIXME: Eurydice crashes with:
-            //
-            // Warning 11: in top-level declaration libcrux_ml_kem.polynomial.{libcrux_ml_kem::polynomial::PolynomialRingElement<Vector>[TraitClause@0]}.add_message_error_reduce__libcrux_ml_kem_libcrux_polynomials_PortableVector: this expression is not Low*; the enclosing function cannot be translated into C*: let mutable ret(Mark.Present,(Mark.AtMost 2), ): int16_t[16size_t] = $any in
-            // libcrux_ml_kem.libcrux_polynomials.{(libcrux_ml_kem::libcrux_polynomials::libcrux_traits::Operations␣for␣libcrux_ml_kem::libcrux_polynomials::PortableVector)}.add ((@9: libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t]*)[0uint32_t]:int16_t[16size_t][16size_t])[@4] &(((@8: libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t]*)[0uint32_t]:libcrux_ml_kem_libcrux_polynomials_PortableVector[16size_t])[@4]) @0;
-            // @0
-            // Warning 11 is fatal, exiting.
-            //
-            // On the following code:
-
-            // ```rust
-            // result.coefficients[i] = Vector::barrett_reduce(Vector::add(
-            //     coefficient_normal_form,
-            //     &Vector::add(self.coefficients[i], &message.coefficients[i]),
-            // ));
-            // ```
-
-            let tmp = Vector::add(self.coefficients[i], &message.coefficients[i]);
-            let tmp = Vector::add(coefficient_normal_form, &tmp);
-            result.coefficients[i] = Vector::barrett_reduce(tmp);
-        }
-        result
+    pub(crate) fn add_message_error_reduce(&self, message: &Self, result: Self) -> Self {
+        add_message_error_reduce(self, message, result)
     }
 
     #[inline(always)]
     pub(crate) fn add_error_reduce(&mut self, error: &Self) {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        // The semicolon and parentheses at the end of loop are a workaround
-        // for the following bug https://github.com/hacspec/hax/issues/720
-        for j in 0..VECTORS_IN_RING_ELEMENT {
-            let coefficient_normal_form =
-                Vector::montgomery_multiply_by_constant(self.coefficients[j], 1441);
-
-            self.coefficients[j] = Vector::barrett_reduce(Vector::add(
-                coefficient_normal_form,
-                &error.coefficients[j],
-            ));
-        }
-        ()
+        add_error_reduce(self, error);
     }
 
     #[inline(always)]
     pub(crate) fn add_standard_error_reduce(&mut self, error: &Self) {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        // The semicolon and parentheses at the end of loop are a workaround
-        // for the following bug https://github.com/hacspec/hax/issues/720
-        for j in 0..VECTORS_IN_RING_ELEMENT {
-            // The coefficients are of the form aR^{-1} mod q, which means
-            // calling to_montgomery_domain() on them should return a mod q.
-            let coefficient_normal_form = to_standard_domain::<Vector>(self.coefficients[j]);
-
-            self.coefficients[j] = Vector::barrett_reduce(Vector::add(
-                coefficient_normal_form,
-                &error.coefficients[j],
-            ));
-        }
-        ()
+        add_standard_error_reduce(self, error);
     }
 
-    /// Given two `KyberPolynomialRingElement`s in their NTT representations,
-    /// compute their product. Given two polynomials in the NTT domain `f^` and `ĵ`,
-    /// the `iᵗʰ` coefficient of the product `k̂` is determined by the calculation:
-    ///
-    /// ```plaintext
-    /// ĥ[2·i] + ĥ[2·i + 1]X = (f^[2·i] + f^[2·i + 1]X)·(ĝ[2·i] + ĝ[2·i + 1]X) mod (X² - ζ^(2·BitRev₇(i) + 1))
-    /// ```
-    ///
-    /// This function almost implements <strong>Algorithm 10</strong> of the
-    /// NIST FIPS 203 standard, which is reproduced below:
-    ///
-    /// ```plaintext
-    /// Input: Two arrays fˆ ∈ ℤ₂₅₆ and ĝ ∈ ℤ₂₅₆.
-    /// Output: An array ĥ ∈ ℤq.
-    ///
-    /// for(i ← 0; i < 128; i++)
-    ///     (ĥ[2i], ĥ[2i+1]) ← BaseCaseMultiply(fˆ[2i], fˆ[2i+1], ĝ[2i], ĝ[2i+1], ζ^(2·BitRev₇(i) + 1))
-    /// end for
-    /// return ĥ
-    /// ```
-    /// We say "almost" because the coefficients of the ring element output by
-    /// this function are in the Montgomery domain.
-    ///
-    /// The NIST FIPS 203 standard can be found at
-    /// <https://csrc.nist.gov/pubs/fips/203/ipd>.
-    // TODO: Remove or replace with something that works and is useful for the proof.
-    // #[cfg_attr(hax, hax_lib::requires(
-    //     hax_lib::forall(|i:usize|
-    //         hax_lib::implies(i < COEFFICIENTS_IN_RING_ELEMENT, ||
-    //             (lhs.coefficients[i] >= 0 && lhs.coefficients[i] < 4096) &&
-    //             (rhs.coefficients[i].abs() <= FIELD_MODULUS)
-
-    // ))))]
-    // #[cfg_attr(hax, hax_lib::ensures(|result|
-    //     hax_lib::forall(|i:usize|
-    //         hax_lib::implies(i < result.coefficients.len(), ||
-    //                 result.coefficients[i].abs() <= FIELD_MODULUS
-    // ))))]
     #[inline(always)]
     pub(crate) fn ntt_multiply(&self, rhs: &Self) -> Self {
-        // Using `hax_lib::fstar::verification_status(lax)` works but produces an error while extracting
-        hax_lib::fstar!("admit ()");
-        // hax_debug_debug_assert!(lhs
-        //     .coefficients
-        //     .into_iter()
-        //     .all(|coefficient| coefficient >= 0 && coefficient < 4096));
-
-        let mut out = PolynomialRingElement::ZERO();
-
-        for i in 0..VECTORS_IN_RING_ELEMENT {
-            out.coefficients[i] = Vector::ntt_multiply(
-                &self.coefficients[i],
-                &rhs.coefficients[i],
-                get_zeta(64 + 4 * i),
-                get_zeta(64 + 4 * i + 1),
-                get_zeta(64 + 4 * i + 2),
-                get_zeta(64 + 4 * i + 3),
-            );
-        }
-
-        out
+        ntt_multiply(self, rhs)
     }
 }
