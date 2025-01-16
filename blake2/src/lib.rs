15ea6805a3a14c5bd04745a76cab89b98a41c0c0
Jan Winkelmann (keks) <jan@cryspen.com>
2025-01-16 16:36:11 +0100

    reduce visibility of hacl modules

--- blake2/src/lib.rs
+++ blake2/src/lib.rs
@@ -1,11 +1,11 @@
 //#![no_std]
 
-pub mod hacl {
+mod hacl {
     //! This module contains generated hacl code.
 
-    pub mod hash_blake2b;
-    pub mod hash_blake2s;
-    pub mod impl_blake2_constants;
+    pub(crate) mod hash_blake2b;
+    pub(crate) mod hash_blake2s;
+    pub(crate) mod impl_blake2_constants;
 }
 
 mod impl_hacl;
