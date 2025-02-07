2b737823dbb5ef7e8434560017fd3c68b9ab08f9
Franziskus Kiefer <franziskuskiefer@gmail.com>
2025-02-07 11:23:57 +0100

    Update ext-tools.sh

--- .docker/c/ext-tools.sh
+++ .docker/c/ext-tools.sh
@@ -4,23 +4,23 @@ set -v -e -x
 
 source $HOME/.profile
 
-curl -L https://github.com/AeneasVerif/charon/archive/db4e045d4597d06d854ce7a2c10e8dcfda6ecd25.zip \
+curl -L https://github.com/AeneasVerif/charon/archive/30cab88265206f4fa849736e704983e39a404d96.zip \
     --output charon.zip
 unzip charon.zip
 rm -rf charon.zip
-mv charon-db4e045d4597d06d854ce7a2c10e8dcfda6ecd25/ charon
+mv charon-30cab88265206f4fa849736e704983e39a404d96/ charon
 
-curl -L https://github.com/FStarLang/karamel/archive/3823e3d82fa0b271d799b61c59ffb4742ddc1e65.zip \
+curl -L https://github.com/FStarLang/karamel/archive/97a06e07e7e423df192c40d5a88bf6c85fd4d278.zip \
     --output karamel.zip
 unzip karamel.zip
 rm -rf karamel.zip
-mv karamel-3823e3d82fa0b271d799b61c59ffb4742ddc1e65/ karamel
+mv karamel-97a06e07e7e423df192c40d5a88bf6c85fd4d278/ karamel
 
-curl -L https://github.com/AeneasVerif/eurydice/archive/83ab5654d49df0603797bf510475d52d67ca24d8.zip \
+curl -L https://github.com/AeneasVerif/eurydice/archive/b8ea420ccde8db516ced5db9c097d77fa558fb94.zip \
     --output eurydice.zip
 unzip eurydice.zip
 rm -rf eurydice.zip
-mv eurydice-83ab5654d49df0603797bf510475d52d67ca24d8/ eurydice
+mv eurydice-b8ea420ccde8db516ced5db9c097d77fa558fb94/ eurydice
 
 echo "export KRML_HOME=$HOME/karamel" >>$HOME/.profile
 echo "export EURYDICE_HOME=$HOME/eurydice" >>$HOME/.profile
