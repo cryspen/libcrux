d10060633175732a5946a26bb3781aa8c591e1d8
Franziskus Kiefer <franziskuskiefer@gmail.com>
2025-02-09 16:11:20 +0100

    extract-c: add revisions to docker image

--- .docker/c/ext-tools.sh
+++ .docker/c/ext-tools.sh
@@ -4,27 +4,35 @@ set -v -e -x
 
 source $HOME/.profile
 
-curl -L https://github.com/AeneasVerif/charon/archive/30cab88265206f4fa849736e704983e39a404d96.zip \
+KRML_REV=97a06e07e7e423df192c40d5a88bf6c85fd4d278
+EURYDICE_REV=b8ea420ccde8db516ced5db9c097d77fa558fb94
+CHARON_REV=30cab88265206f4fa849736e704983e39a404d96
+
+curl -L https://github.com/AeneasVerif/charon/archive/$CHARON_REV.zip \
     --output charon.zip
 unzip charon.zip
 rm -rf charon.zip
-mv charon-30cab88265206f4fa849736e704983e39a404d96/ charon
+mv charon-$CHARON_REV/ charon
 
-curl -L https://github.com/FStarLang/karamel/archive/97a06e07e7e423df192c40d5a88bf6c85fd4d278.zip \
+curl -L https://github.com/FStarLang/karamel/archive/$KRML_REV.zip \
     --output karamel.zip
 unzip karamel.zip
 rm -rf karamel.zip
-mv karamel-97a06e07e7e423df192c40d5a88bf6c85fd4d278/ karamel
+mv karamel-$KRML_REV/ karamel
 
-curl -L https://github.com/AeneasVerif/eurydice/archive/b8ea420ccde8db516ced5db9c097d77fa558fb94.zip \
+curl -L https://github.com/AeneasVerif/eurydice/archive/$EURYDICE_REV.zip \
     --output eurydice.zip
 unzip eurydice.zip
 rm -rf eurydice.zip
-mv eurydice-b8ea420ccde8db516ced5db9c097d77fa558fb94/ eurydice
+mv eurydice-$EURYDICE_REV/ eurydice
 
 echo "export KRML_HOME=$HOME/karamel" >>$HOME/.profile
 echo "export EURYDICE_HOME=$HOME/eurydice" >>$HOME/.profile
 echo "export CHARON_HOME=$HOME/charon" >>$HOME/.profile
+
+echo "export KRML_REV=$KRML_REV" >>$HOME/.profile
+echo "export EURYDICE_REV=$EURYDICE_REV" >>$HOME/.profile
+echo "export CHARON_REV=$CHARON_REV" >>$HOME/.profile
 
 source $HOME/.profile
 
