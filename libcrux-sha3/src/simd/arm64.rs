d2ba0bfbce3af7552501b2d99d3ecea5ed3b2773
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-03-12 21:30:53 +0100

    fmt

--- libcrux-sha3/src/simd/arm64.rs
+++ libcrux-sha3/src/simd/arm64.rs
@@ -45,7 +45,7 @@ pub(crate) fn load_block<const RATE: usize>(
 ) {
     debug_assert!(RATE <= blocks[0].len() && RATE % 8 == 0);
     let mut last = [0u64; 2];
-    let mut st0 = [uint64x2_t::zero();16];
+    let mut st0 = [uint64x2_t::zero(); 16];
     for i in 0..RATE / 16 {
         let start = offset + 16 * i;
         st0[i] = _vld1q_bytes_u64(&blocks[0][start..start + 16]);
@@ -53,7 +53,7 @@ pub(crate) fn load_block<const RATE: usize>(
     let last_offset = offset + RATE - 8;
     last[0] = u64::from_le_bytes(blocks[0][last_offset..last_offset + 8].try_into().unwrap());
 
-    let mut st1 = [uint64x2_t::zero();16];
+    let mut st1 = [uint64x2_t::zero(); 16];
     for i in 0..RATE / 16 {
         let start = offset + 16 * i;
         st1[i] = _vld1q_bytes_u64(&blocks[1][start..start + 16]);
@@ -61,9 +61,12 @@ pub(crate) fn load_block<const RATE: usize>(
     last[1] = u64::from_le_bytes(blocks[1][last_offset..last_offset + 8].try_into().unwrap());
 
     for i in 0..RATE / 16 {
-        s[(2 * i) / 5][(2 * i) % 5] = _veorq_u64(s[(2 * i) / 5][(2 * i) % 5], _vtrn1q_u64(st0[i], st1[i]));
-        s[(2 * i + 1) / 5][(2 * i + 1) % 5] =
-            _veorq_u64(s[(2 * i + 1) / 5][(2 * i + 1) % 5], _vtrn2q_u64(st0[i], st1[i]));
+        s[(2 * i) / 5][(2 * i) % 5] =
+            _veorq_u64(s[(2 * i) / 5][(2 * i) % 5], _vtrn1q_u64(st0[i], st1[i]));
+        s[(2 * i + 1) / 5][(2 * i + 1) % 5] = _veorq_u64(
+            s[(2 * i + 1) / 5][(2 * i + 1) % 5],
+            _vtrn2q_u64(st0[i], st1[i]),
+        );
     }
 
     if RATE % 16 != 0 {
