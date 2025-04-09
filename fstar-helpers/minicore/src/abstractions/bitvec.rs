c6b55ae94f85c53cf6eba2da5730bf6f9f3dd6e7
Lucas Franceschino <lucas@cryspen.com>
2025-04-09 13:38:56 +0000

    fix: minicore: get rid of generic pointwise operator, define them manually
    The metaprogram was causing huge perf issues.

--- fstar-helpers/minicore/src/abstractions/bitvec.rs
+++ fstar-helpers/minicore/src/abstractions/bitvec.rs
@@ -88,9 +88,6 @@ fn int_from_bit_slice<T: TryFrom<i128> + MachineInteger + Copy>(bits: &[Bit]) ->
     n
 }
 
-/// An F* attribute that indiquates a rewritting lemma should be applied
-pub const REWRITE_RULE: () = {};
-
 #[hax_lib::fstar::replace(
     r#"
 let ${BitVec::<0>::from_fn::<fn(u64)->Bit>}
@@ -161,29 +158,27 @@ let ${BitVec::<128>::postprocess_rewrite} = postprocess_rewrite_helper (`${BitVe
 )]
 const _: () = ();
 
-#[hax_lib::fstar::replace(
-    r#"
-"#
-)]
-pub fn postprocess_normalize_128() {}
+macro_rules! impl_pointwise {
+    ($n:literal, $($i:literal)*) => {
+        impl BitVec<$n> {
+            pub fn pointwise(self) -> Self {
+                Self::from_fn(|i| match i {
+                    $($i => self[$i],)*
+                    _ => unreachable!(),
+                })
+            }
+        }
+    };
+}
 
-#[hax_lib::exclude]
-impl BitVec<128> {
-    pub fn rewrite_pointwise(self) {}
-    pub fn postprocess_rewrite() {}
-}
-#[hax_lib::exclude]
-impl BitVec<256> {
-    pub fn rewrite_pointwise(self) {}
-    pub fn postprocess_rewrite() {}
-}
+impl_pointwise!(128, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127);
+impl_pointwise!(256, 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 225 226 227 228 229 230 231 232 233 234 235 236 237 238 239 240 241 242 243 244 245 246 247 248 249 250 251 252 253 254 255);
+
+/// An F* attribute that indiquates a rewritting lemma should be applied
+pub const REWRITE_RULE: () = {};
 
 #[hax_lib::exclude]
 impl<const N: u64> BitVec<N> {
-    pub fn pointwise(self) -> Self {
-        self
-    }
-
     /// Constructor for BitVec. `BitVec::<N>::from_fn` constructs a bitvector out of a function that takes usizes smaller than `N` and produces bits.
     pub fn from_fn<F: Fn(u64) -> Bit>(f: F) -> Self {
         Self(FunArray::from_fn(f))
