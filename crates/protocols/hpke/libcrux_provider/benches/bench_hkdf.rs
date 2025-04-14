615a326b935601848c656348a3b07256e174fbed
wysiwys <clara@cryspen.com>
2025-04-14 14:07:01 +0200

    more concise way to get rng from `rand` in benchmarks

--- crates/protocols/hpke/libcrux_provider/benches/bench_hkdf.rs
+++ crates/protocols/hpke/libcrux_provider/benches/bench_hkdf.rs
@@ -1,21 +1,15 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use hpke_rs_crypto::{types::*, HpkeCrypto, RngCore};
 use hpke_rs_libcrux::*;
-use rand::rngs::OsRng;
-
-use rand::TryRngCore;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function(&format!("HKDF SHA256 Extract"), |b| {
         b.iter_batched(
             || {
-                let mut rng = OsRng;
-                let mut rng_mut = rng.unwrap_mut();
-
                 let mut salt = vec![0u8; 77];
                 let mut ikm = vec![0u8; 32];
-                rng_mut.fill_bytes(&mut salt);
-                rng_mut.fill_bytes(&mut ikm);
+                rand::rng().fill_bytes(&mut salt);
+                rand::rng().fill_bytes(&mut ikm);
                 (salt.clone(), ikm.clone())
             },
             |(salt, ikm)| {
@@ -27,13 +21,10 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function(&format!("HKDF SHA256 Expand"), |b| {
         b.iter_batched(
             || {
-                let mut rng = OsRng;
-                let mut rng_mut = rng.unwrap_mut();
-
                 let mut info = vec![0u8; 77];
                 let mut prk = vec![0u8; 32];
-                rng_mut.fill_bytes(&mut info);
-                rng_mut.fill_bytes(&mut prk);
+                rand::rng().fill_bytes(&mut info);
+                rand::rng().fill_bytes(&mut prk);
                 (prk.clone(), info.clone())
             },
             |(prk, info)| {
