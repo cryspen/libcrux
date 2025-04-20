d2217823fe1f25bda907097eeebbf651278108a6
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-04-20 10:34:39 +0200

    gcm portable

--- libcrux-aesgcm/src/platform/portable/gf128_core.rs
+++ libcrux-aesgcm/src/platform/portable/gf128_core.rs
@@ -1,23 +1,25 @@
 pub(crate) type FieldElement = u128;
 
-fn zero() -> FieldElement { 0 }
-fn load_elem(b:& [u8]) -> FieldElement {
+fn zero() -> FieldElement {
+    0
+}
+fn load_elem(b: &[u8]) -> FieldElement {
     debug_assert!(b.len() == 16);
     u128::from_be_bytes(b.try_into().unwrap())
 }
 
-fn store_elem(elem:&FieldElement, b:&mut [u8]) {
+fn store_elem(elem: &FieldElement, b: &mut [u8]) {
     debug_assert!(b.len() == 16);
     b.copy_from_slice(&u128::to_be_bytes(*elem));
 }
-   
-fn add(elem: &FieldElement, other:&FieldElement) -> FieldElement {
+
+fn add(elem: &FieldElement, other: &FieldElement) -> FieldElement {
     elem ^ other
 }
 
-fn ith_bit_mask(elem: &FieldElement, i:usize) -> FieldElement {
+fn ith_bit_mask(elem: &FieldElement, i: usize) -> FieldElement {
     debug_assert!(i < 128);
-    let bit:u16 = ((elem >> (127 - i)) as u16) & 0x1;
+    let bit: u16 = ((elem >> (127 - i)) as u16) & 0x1;
     let bit_mask16 = (!bit).wrapping_add(1);
     let bit_mask32 = (bit_mask16 as u32) ^ ((bit_mask16 as u32) << 16);
     let bit_mask64 = (bit_mask32 as u64) ^ ((bit_mask32 as u64) << 32);
@@ -32,17 +34,17 @@ fn mul_x(elem: &mut FieldElement) {
     *elem = (*elem >> 1) ^ (IRRED & mask)
 }
 
-fn mul_step(x: &FieldElement, y: &mut FieldElement, i:usize, result: &mut FieldElement) {
+fn mul_step(x: &FieldElement, y: &mut FieldElement, i: usize, result: &mut FieldElement) {
     debug_assert!(i < 128);
     let mask = ith_bit_mask(x, i);
     *result ^= (*y & mask);
     mul_x(y);
 }
 
-fn mul(x: &FieldElement, y:&FieldElement) -> FieldElement {
+fn mul(x: &FieldElement, y: &FieldElement) -> FieldElement {
     let mut result = 0;
     let mut multiplicand = *y;
-    for i in 0..128{
+    for i in 0..128 {
         mul_step(x, &mut multiplicand, i, &mut result)
     }
     result
@@ -53,19 +55,19 @@ impl crate::platform::GF128FieldElement for FieldElement {
         zero()
     }
 
-    fn load_elem(b:&[u8]) -> Self {
+    fn load_elem(b: &[u8]) -> Self {
         load_elem(b)
     }
 
-    fn store_elem(&self, b:&mut [u8]) {
+    fn store_elem(&self, b: &mut [u8]) {
         store_elem(self, b);
     }
 
-    fn add(&mut self, other:&Self) {
+    fn add(&mut self, other: &Self) {
         *self = add(self, other);
     }
 
-    fn mul(&mut self, other:&Self) {
+    fn mul(&mut self, other: &Self) {
         *self = mul(self, other)
     }
 }
