9a755706c88b456ba86d4b243076632c0545bae1
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-04-27 17:26:05 +0000

    rcon generic

--- libcrux-aesgcm/src/aes_generic.rs
+++ libcrux-aesgcm/src/aes_generic.rs
@@ -10,11 +10,24 @@ pub(crate) fn aes128_key_expansion<T: AESState>(key: &[u8]) -> ExtendedKey<T, 11
     debug_assert!(key.len() == 16);
     let mut keyex = [T::new(); 11];
     keyex[0].load_block(&key);
-    for i in 1..11 {
-        let prev = keyex[i - 1];
-        keyex[i].aes_keygen_assist0(&prev, RCON[i]);
-        keyex[i].key_expansion_step(&prev);
+
+    macro_rules! expansion_step128 {
+        ($i:expr,$rcon:expr) => {
+            let prev = keyex[$i-1];
+            keyex[$i].aes_keygen_assist0::<$rcon>(&prev);
+            keyex[$i].key_expansion_step(&prev);
+        }
     }
+    expansion_step128!(1,0x01);
+    expansion_step128!(2,0x02);
+    expansion_step128!(3,0x04);
+    expansion_step128!(4,0x08);
+    expansion_step128!(5,0x10);
+    expansion_step128!(6,0x20);
+    expansion_step128!(7,0x40);
+    expansion_step128!(8,0x80);
+    expansion_step128!(9,0x1b);
+    expansion_step128!(10,0x36);
     keyex
 }
 
@@ -23,18 +36,35 @@ pub(crate) fn aes256_key_expansion<T: AESState>(key: &[u8]) -> ExtendedKey<T, 15
     let mut keyex = [T::new(); 15];
     keyex[0].load_block(&key[0..16]);
     keyex[1].load_block(&key[16..32]);
-    for i in 2..14 {
-        let prev0 = keyex[i - 2];
-        let prev1 = keyex[i - 1];
-        keyex[i].aes_keygen_assist0(&prev1, RCON[i / 2]);
-        keyex[i].key_expansion_step(&prev0);
-        let next0 = keyex[i];
-        keyex[i + 1].aes_keygen_assist1(&next0);
-        keyex[i + 1].key_expansion_step(&prev1);
+
+    macro_rules! expansion_step256 {
+        ($i:expr,$rcon:expr) => {
+            let prev0 = keyex[$i-2];
+            let prev1 = keyex[$i-1];
+            keyex[$i].aes_keygen_assist0::<$rcon>(&prev1);
+            keyex[$i].key_expansion_step(&prev0);
+            let next0 = keyex[$i];
+            keyex[$i+1].aes_keygen_assist1(&next0);
+            keyex[$i+1].key_expansion_step(&prev1);
+        }
     }
+
+    expansion_step256!(2,0x01);
+    expansion_step256!(3,0x01);
+    expansion_step256!(4,0x02);
+    expansion_step256!(5,0x02);
+    expansion_step256!(6,0x04);
+    expansion_step256!(7,0x04);
+    expansion_step256!(8,0x08);
+    expansion_step256!(9,0x08);
+    expansion_step256!(10,0x10);
+    expansion_step256!(11,0x10);
+    expansion_step256!(12,0x20);
+    expansion_step256!(13,0x20);
+
     let prev0 = keyex[12];
     let prev1 = keyex[13];
-    keyex[14].aes_keygen_assist0(&prev1, RCON[7]);
+    keyex[14].aes_keygen_assist0::<0x40>(&prev1);
     keyex[14].key_expansion_step(&prev0);
     keyex
 }
