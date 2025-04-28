53e79fee5ff7a84f740e468d035c59bca3141792
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-04-28 11:15:47 +0100

    fmt

--- libcrux-aesgcm/src/lib.rs
+++ libcrux-aesgcm/src/lib.rs
@@ -6,51 +6,116 @@ pub mod platform;
 
 pub use aes_gcm::DecryptError;
 
-pub mod portable{
-    use crate::{aes_gcm::{self, DecryptError}, platform};
+pub mod portable {
+    use crate::{
+        aes_gcm::{self, DecryptError},
+        platform,
+    };
 
-    pub fn aes128_gcm_encrypt(key: &[u8], nonce: &[u8], aad: &[u8], plaintext: &[u8], ciphertext: &mut [u8], tag: &mut [u8]){
-        let mut st = aes_gcm::aes128_gcm_init::<platform::portable::State,platform::portable::FieldElement>(key);
+    pub fn aes128_gcm_encrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        plaintext: &[u8],
+        ciphertext: &mut [u8],
+        tag: &mut [u8],
+    ) {
+        let mut st = aes_gcm::aes128_gcm_init::<
+            platform::portable::State,
+            platform::portable::FieldElement,
+        >(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_encrypt(&mut st, aad, plaintext, ciphertext, tag);
     }
 
-    pub fn aes128_gcm_decrypt(key: &[u8], nonce: &[u8], aad: &[u8], ciphertext: &[u8], tag: &[u8], plaintext: &mut [u8]) -> Result<(), DecryptError>{
-        let mut st = aes_gcm::aes128_gcm_init::<platform::portable::State,platform::portable::FieldElement>(key);
+    pub fn aes128_gcm_decrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        ciphertext: &[u8],
+        tag: &[u8],
+        plaintext: &mut [u8],
+    ) -> Result<(), DecryptError> {
+        let mut st = aes_gcm::aes128_gcm_init::<
+            platform::portable::State,
+            platform::portable::FieldElement,
+        >(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_decrypt(&mut st, aad, ciphertext, tag, plaintext)
     }
 }
 
-#[cfg(all(target_arch = "aarch64", target_feature="aes"))]
-pub mod neon{
-    use crate::{aes_gcm::{self, DecryptError}, platform};
+#[cfg(all(target_arch = "aarch64", target_feature = "aes"))]
+pub mod neon {
+    use crate::{
+        aes_gcm::{self, DecryptError},
+        platform,
+    };
 
-    pub fn aes128_gcm_encrypt(key: &[u8], nonce: &[u8], aad: &[u8], plaintext: &[u8], ciphertext: &mut [u8], tag: &mut [u8]){
-        let mut st = aes_gcm::aes128_gcm_init::<platform::neon::State,platform::neon::FieldElement>(key);
+    pub fn aes128_gcm_encrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        plaintext: &[u8],
+        ciphertext: &mut [u8],
+        tag: &mut [u8],
+    ) {
+        let mut st =
+            aes_gcm::aes128_gcm_init::<platform::neon::State, platform::neon::FieldElement>(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_encrypt(&mut st, aad, plaintext, ciphertext, tag);
     }
 
-    pub fn aes128_gcm_decrypt(key: &[u8], nonce: &[u8], aad: &[u8], ciphertext: &[u8], tag: &[u8], plaintext: &mut [u8]) -> Result<(), DecryptError>{
-        let mut st = aes_gcm::aes128_gcm_init::<platform::neon::State,platform::neon::FieldElement>(key);
+    pub fn aes128_gcm_decrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        ciphertext: &[u8],
+        tag: &[u8],
+        plaintext: &mut [u8],
+    ) -> Result<(), DecryptError> {
+        let mut st =
+            aes_gcm::aes128_gcm_init::<platform::neon::State, platform::neon::FieldElement>(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_decrypt(&mut st, aad, ciphertext, tag, plaintext)
     }
 }
 
 #[cfg(all(target_arch = "x86_64"))] // REENABLE target_feature="aes"
-pub mod intel_ni{
-    use crate::{aes_gcm::{self, DecryptError}, platform};
+pub mod intel_ni {
+    use crate::{
+        aes_gcm::{self, DecryptError},
+        platform,
+    };
 
-    pub fn aes128_gcm_encrypt(key: &[u8], nonce: &[u8], aad: &[u8], plaintext: &[u8], ciphertext: &mut [u8], tag: &mut [u8]){
-        let mut st = aes_gcm::aes128_gcm_init::<platform::intel_ni::State,platform::intel_ni::FieldElement>(key);
+    pub fn aes128_gcm_encrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        plaintext: &[u8],
+        ciphertext: &mut [u8],
+        tag: &mut [u8],
+    ) {
+        let mut st = aes_gcm::aes128_gcm_init::<
+            platform::intel_ni::State,
+            platform::intel_ni::FieldElement,
+        >(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_encrypt(&mut st, aad, plaintext, ciphertext, tag);
     }
 
-    pub fn aes128_gcm_decrypt(key: &[u8], nonce: &[u8], aad: &[u8], ciphertext: &[u8], tag: &[u8], plaintext: &mut [u8]) -> Result<(), DecryptError>{
-        let mut st = aes_gcm::aes128_gcm_init::<platform::intel_ni::State,platform::intel_ni::FieldElement>(key);
+    pub fn aes128_gcm_decrypt(
+        key: &[u8],
+        nonce: &[u8],
+        aad: &[u8],
+        ciphertext: &[u8],
+        tag: &[u8],
+        plaintext: &mut [u8],
+    ) -> Result<(), DecryptError> {
+        let mut st = aes_gcm::aes128_gcm_init::<
+            platform::intel_ni::State,
+            platform::intel_ni::FieldElement,
+        >(key);
         aes_gcm::aes128_gcm_set_nonce(&mut st, nonce);
         aes_gcm::aes128_gcm_decrypt(&mut st, aad, ciphertext, tag, plaintext)
     }
