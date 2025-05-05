39566932b52c479d97e7e5dccd671ca0265f3aba
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-05-05 14:06:09 +0200

    WIP remaining `&mut Operations` changes for Portable
    TODO: AVX2

--- libcrux-ml-kem/src/mlkem768.rs
+++ libcrux-ml-kem/src/mlkem768.rs
@@ -421,8 +421,8 @@ macro_rules! instantiate {
 // Instantiations
 
 instantiate! {portable, ind_cca::instantiations::portable, "Portable ML-KEM 768"}
-#[cfg(feature = "simd256")]
-instantiate! {avx2, ind_cca::instantiations::avx2, "AVX2 Optimised ML-KEM 768"}
+// #[cfg(feature = "simd256")]
+// instantiate! {avx2, ind_cca::instantiations::avx2, "AVX2 Optimised ML-KEM 768"}
 #[cfg(feature = "simd128")]
 instantiate! {neon, ind_cca::instantiations::neon, "Neon Optimised ML-KEM 768"}
 
