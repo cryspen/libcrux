9d6fd26d4cdcc105035986cf23006e0982cf78ce
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-05-23 18:57:31 +0000

    C code refresh

--- libcrux-ml-dsa/cg/libcrux_core.h
+++ libcrux-ml-dsa/cg/libcrux_core.h
@@ -8,7 +8,7 @@
  * Eurydice: d3b14228e2b5fe8710ec7efae31e4de2c96ed20d
  * Karamel: 095cdb73f246711f93f99a159ceca37cd2c227e1
  * F*: 4b3fc11774003a6ff7c09500ecb5f0145ca6d862
- * Libcrux: 75cbe9ea0e459cf8a62d97e8a867411e0dd8529a
+ * Libcrux: 7740a80c5969b8eb18286f0d51b23d27d77fc5d2
  */
 
 #ifndef __libcrux_core_H
