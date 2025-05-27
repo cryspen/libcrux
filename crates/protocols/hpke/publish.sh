075e8652183b93a2c439e9c96cfabbece4a12ce9
wysiwys <clara@cryspen.com>
2025-05-27 12:58:53 +0200

    release script documentation

--- crates/protocols/hpke/publish.sh
+++ crates/protocols/hpke/publish.sh
@@ -3,4 +3,7 @@
 
 set -e
 
+# release all crates in the workspace in order of appearance
+# in the root crate's `workspace.members`.
+# sign tags and only allow the command to be run from the `main` branch. 
 cargo release --workspace --sign-tag --allow-branch main
