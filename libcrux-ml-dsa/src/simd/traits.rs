b7cc0a04932e93e06ba88c77bd06263b4699557d
Lucas Franceschino <lucas@cryspen.com>
2025-07-15 18:38:35 +0200

    wip: add pre/post, make them available

--- libcrux-ml-dsa/src/simd/traits.rs
+++ libcrux-ml-dsa/src/simd/traits.rs
@@ -1,4 +1,4 @@
-use crate::constants::{Eta, Gamma2};
+use crate::constants::{Eta, Gamma2, BITS_IN_LOWER_PART_OF_T};
 
 // Each field element occupies 32 bits and the size of a simd_unit is 256 bits.
 pub(crate) const COEFFICIENTS_IN_SIMD_UNIT: usize = 8;
@@ -24,8 +24,7 @@ pub(crate) trait Repr: Copy + Clone {
     fn repr(&self) -> [i32; COEFFICIENTS_IN_SIMD_UNIT];
 }
 
-#[cfg(hax)]
-pub(crate) mod specs {
+pub mod specs {
     use hax_lib::*;
 
     pub(crate) const PRIME: u32 = 8380417;
@@ -127,6 +126,481 @@ pub(crate) mod specs {
     }
 }
 
+
+mod hey {
+    pub use super::*;
+    pub fn future<T>(x: T) -> T {
+        panic!()
+    }
+    pub mod zero {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>() -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(result: T) -> hax_lib::Prop {
+            hax_lib::Prop::from(result.repr() == [0i32; COEFFICIENTS_IN_SIMD_UNIT])
+        }
+    }
+    pub mod from_coefficient_array {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            array: &[i32],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(array.len() == COEFFICIENTS_IN_SIMD_UNIT)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            array: &[i32],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(future(out).repr() == array)
+        }
+    }
+    pub mod to_coefficient_array {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            value: &T,
+            out: &mut [i32],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(out.len() == COEFFICIENTS_IN_SIMD_UNIT)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            value: &T,
+            out: &mut [i32],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(future(out) == value.repr())
+        }
+    }
+    pub mod add {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(specs::add_pre(&lhs.repr(), &rhs.repr()))
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                specs::add_post(&lhs.repr(), &rhs.repr(), &future(lhs).repr()),
+            )
+        }
+    }
+    pub mod subtract {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(specs::sub_pre(&lhs.repr(), &rhs.repr()))
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                specs::sub_post(&lhs.repr(), &rhs.repr(), &future(lhs).repr()),
+            )
+        }
+    }
+    pub mod infinity_norm_exceeds {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            bound: i32,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"v $bound > 0 /\ 
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (f_repr ${simd_unit})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            bound: i32,
+            _result: bool,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod decompose {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            gamma2: Gamma2,
+            simd_unit: &T,
+            low: &mut T,
+            high: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        (v $gamma2 == v ${crate::constants::GAMMA2_V261_888} \/ 
+         v $gamma2 == v ${crate::constants::GAMMA2_V95_232}) /\
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (f_repr ${simd_unit})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            gamma2: Gamma2,
+            simd_unit: &T,
+            low: &mut T,
+            high: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod compute_hint {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            low: &T,
+            high: &T,
+            gamma2: i32,
+            hint: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        (v $gamma2 == v ${crate::constants::GAMMA2_V261_888} \/ 
+         v $gamma2 == v ${crate::constants::GAMMA2_V95_232})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            low: &T,
+            high: &T,
+            gamma2: i32,
+            hint: &mut T,
+            result: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(result <= 8)
+        }
+    }
+    pub mod use_hint {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            gamma2: Gamma2,
+            simd_unit: &T,
+            hint: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        (v $gamma2 == v ${crate::constants::GAMMA2_V261_888} \/ 
+         v $gamma2 == v ${crate::constants::GAMMA2_V95_232}) /\
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (f_repr ${simd_unit})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            gamma2: Gamma2,
+            simd_unit: &T,
+            hint: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod montgomery_multiply {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (${rhs.repr()})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(lhs: &mut T, rhs: &T) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (f_repr ${lhs}_future) /\
+        Spec.MLDSA.Math.(forall i. i < 8 ==> 
+            mod_q (v (Seq.index (f_repr ${lhs}_future) i)) == 
+            mod_q (v (Seq.index (${lhs.repr()}) i) * v (Seq.index (${rhs.repr()}) i) * 8265825))"#,
+                ),
+            )
+        }
+    }
+    pub mod power2round {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            t0: &mut T,
+            t1: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+        Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) (f_repr ${t0})"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(t0: &mut T, t1: &mut T) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod rejection_sample_less_than_field_modulus {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+            _result: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod rejection_sample_less_than_eta_equals_2 {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+            _result: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod rejection_sample_less_than_eta_equals_4 {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            randomness: &[u8],
+            out: &mut [i32],
+            _result: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod gamma1_serialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            serialized: &mut [u8],
+            gamma1_exponent: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                match gamma1_exponent {
+                    17 => serialized.len() == 18,
+                    19 => serialized.len() == 20,
+                    _ => false,
+                },
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            serialized: &mut [u8],
+            gamma1_exponent: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod gamma1_deserialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+            gamma1_exponent: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+            gamma1_exponent: usize,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod commitment_serialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            serialized: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(serialized.len() == 4 || serialized.len() == 6)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            serialized: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(future(&serialized).len() == serialized.len())
+        }
+    }
+    pub mod error_serialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            eta: Eta,
+            simd_unit: &T,
+            serialized: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                serialized.len()
+                    == match eta {
+                        Eta::Two => 3,
+                        Eta::Four => 4,
+                    },
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            eta: Eta,
+            simd_unit: &T,
+            serialized: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod error_deserialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            eta: Eta,
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                serialized.len()
+                    == match eta {
+                        Eta::Two => 3,
+                        Eta::Four => 4,
+                    },
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            eta: Eta,
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod t0_serialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            out: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"forall i. let x = (pow2 (v $BITS_IN_LOWER_PART_OF_T - 1) - v (Seq.index (${simd_unit.repr()}) i)) in x >= 0 && x < pow2 13"#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            out: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod t0_deserialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(serialized.len() == 13)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod t1_serialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            out: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(out.len() == 10)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_unit: &T,
+            out: &mut [u8],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod t1_deserialize {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(serialized.len() == 10)
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            serialized: &[u8],
+            out: &mut T,
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(true)
+        }
+    }
+    pub mod ntt {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_units: &mut [T; SIMD_UNITS_IN_RING_ELEMENT],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+            (forall (i:nat). i < 32 ==> 
+                Spec.Utils.is_i32b_array_opaque 
+                (v ${specs::NTT_BASE_BOUND})
+                (f_repr (Seq.index ${simd_units} i)))
+        "#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_units: &mut [T; SIMD_UNITS_IN_RING_ELEMENT],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+            (forall (i:nat). i < 32 ==> Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) 
+                (f_repr (Seq.index ${simd_units}_future i)))
+        "#,
+                ),
+            )
+        }
+    }
+    pub mod invert_ntt_montgomery {
+        use super::*;
+        pub fn requires<T: Copy + Clone + Repr>(
+            simd_units: &mut [T; SIMD_UNITS_IN_RING_ELEMENT],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+            (forall (i:nat). i < 32 ==> Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) 
+                (f_repr (Seq.index ${simd_units} i)))
+        "#,
+                ),
+            )
+        }
+        pub fn ensures<T: Copy + Clone + Repr>(
+            simd_units: &mut [T; SIMD_UNITS_IN_RING_ELEMENT],
+        ) -> hax_lib::Prop {
+            hax_lib::Prop::from(
+                hax_lib::fstar::prop!(
+                    r#"
+            (forall (i:nat). i < 32 ==> Spec.Utils.is_i32b_array_opaque (v ${specs::FIELD_MAX}) 
+                (f_repr (Seq.index ${simd_units}_future i)))
+        "#,
+                ),
+            )
+        }
+    }
+}
+
+
 #[cfg(not(eurydice))]
 #[hax_lib::attributes]
 pub(crate) trait Operations: Copy + Clone + Repr {
@@ -215,7 +689,11 @@ pub(crate) trait Operations: Copy + Clone + Repr {
     // Encoding operations
 
     // Gamma1
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(match gamma1_exponent {
+        17 => serialized.len() == 18,
+        19 => serialized.len() == 20,
+        _ => false,
+    })]
     fn gamma1_serialize(simd_unit: &Self, serialized: &mut [u8], gamma1_exponent: usize);
     #[hax_lib::requires(true)]
     fn gamma1_deserialize(serialized: &[u8], out: &mut Self, gamma1_exponent: usize);
@@ -226,21 +704,27 @@ pub(crate) trait Operations: Copy + Clone + Repr {
     fn commitment_serialize(simd_unit: &Self, serialized: &mut [u8]);
 
     // Error
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(serialized.len() == match eta {
+        Eta::Two => 3,
+        Eta::Four => 4,
+    })]
     fn error_serialize(eta: Eta, simd_unit: &Self, serialized: &mut [u8]);
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(serialized.len() == match eta {
+        Eta::Two => 3,
+        Eta::Four => 4,
+    })]
     fn error_deserialize(eta: Eta, serialized: &[u8], out: &mut Self);
 
     // t0
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(fstar!(r#"forall i. let x = (pow2 (v $BITS_IN_LOWER_PART_OF_T - 1) - v (Seq.index (${simd_unit.repr()}) i)) in x >= 0 && x < pow2 13"#))]
     fn t0_serialize(simd_unit: &Self, out: &mut [u8]); // out len 13
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(serialized.len() == 13)]
     fn t0_deserialize(serialized: &[u8], out: &mut Self);
 
     // t1
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(out.len() == 10)]
     fn t1_serialize(simd_unit: &Self, out: &mut [u8]); // out len 10
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(serialized.len() == 10)]
     fn t1_deserialize(serialized: &[u8], out: &mut Self);
 
     // NTT
