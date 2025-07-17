8e34f484d3ad1b2d23fd5c4caa07f919762005cc
wysiwys <clara@cryspen.com>
2025-07-17 17:37:23 +0200

    start implementing signature trait in ed25519

---
+++ ed25519/src/impl_signature_trait.rs
@@ -0,0 +1,48 @@
+use libcrux_traits::signature::arrayref::{SignError, Signature, VerifyError};
+
+pub struct Ed25519;
+
+const PUBLIC_KEY_LEN: usize = 32;
+const PRIVATE_KEY_LEN: usize = 32;
+const SIGNATURE_LEN: usize = 64;
+
+impl Signature<PUBLIC_KEY_LEN, PRIVATE_KEY_LEN, SIGNATURE_LEN> for Ed25519 {
+    fn sign(
+        payload: &[u8],
+        private_key: &[u8; PRIVATE_KEY_LEN],
+        signature: &mut [u8; SIGNATURE_LEN],
+    ) -> Result<(), SignError> {
+        crate::hacl::ed25519::sign(
+            signature,
+            private_key,
+            payload
+                .len()
+                .try_into()
+                .map_err(|_| SignError::InvalidPayloadLength)?,
+            payload,
+        );
+
+        Ok(())
+    }
+    fn verify(
+        payload: &[u8],
+        public_key: &[u8; PUBLIC_KEY_LEN],
+        signature: &[u8; SIGNATURE_LEN],
+    ) -> Result<(), VerifyError> {
+        if crate::hacl::ed25519::verify(
+            public_key,
+            payload
+                .len()
+                .try_into()
+                .map_err(|_| VerifyError::InvalidPayloadLength)?,
+            payload,
+            signature,
+        ) {
+            Ok(())
+        } else {
+            Err(VerifyError::InvalidSignature)
+        }
+    }
+}
+
+libcrux_traits::signature::slice::impl_signature_slice_trait!(Ed25519 => PUBLIC_KEY_LEN, PRIVATE_KEY_LEN, SIGNATURE_LEN);
