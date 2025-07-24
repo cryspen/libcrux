d7c88fff5203bd5828371dae3401a59f99c58126
wysiwys <clara@cryspen.com>
2025-07-24 17:03:42 +0200

    add missing NoAux trait

--- traits/src/signature/secrets.rs
+++ traits/src/signature/secrets.rs
@@ -24,3 +24,25 @@ impl<
         Self::sign(payload, private_key.declassify_ref(), sign_aux)
     }
 }
+
+pub trait SignNoAux<const PRIVATE_KEY_LEN: usize, const SIGNATURE_LEN: usize> {
+    fn sign(
+        payload: &[u8],
+        private_key: &[U8; PRIVATE_KEY_LEN],
+    ) -> Result<[u8; SIGNATURE_LEN], SignError>;
+}
+
+impl<
+        'a,
+        const PRIVATE_KEY_LEN: usize,
+        const SIGNATURE_LEN: usize,
+        T: Sign<&'a (), PRIVATE_KEY_LEN, SIGNATURE_LEN>,
+    > SignNoAux<PRIVATE_KEY_LEN, SIGNATURE_LEN> for T
+{
+    fn sign(
+        payload: &[u8],
+        private_key: &[U8; PRIVATE_KEY_LEN],
+    ) -> Result<[u8; SIGNATURE_LEN], SignError> {
+        <Self as Sign<&'a (), PRIVATE_KEY_LEN, SIGNATURE_LEN>>::sign(payload, private_key, &())
+    }
+}
