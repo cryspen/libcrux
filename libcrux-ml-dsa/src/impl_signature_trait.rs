a0b98ebd8818ff5e56c91a882039032d891d12c6
wysiwys <clara@cryspen.com>
2025-08-11 18:39:34 +0200

    exclude signature trait implementation

--- libcrux-ml-dsa/src/impl_signature_trait.rs
+++ libcrux-ml-dsa/src/impl_signature_trait.rs
@@ -1,112 +1,115 @@
-use libcrux_traits::signature::{arrayref, owned};
-
-impl From<crate::SigningError> for owned::SignError {
-    fn from(e: crate::SigningError) -> Self {
-        match e {
-            crate::SigningError::RejectionSamplingError => owned::SignError::LibraryError,
-            crate::SigningError::ContextTooLongError => owned::SignError::LibraryError,
-        }
-    }
-}
-
-impl From<crate::VerificationError> for arrayref::VerifyError {
-    fn from(e: crate::VerificationError) -> Self {
-        match e {
-            crate::VerificationError::MalformedHintError => arrayref::VerifyError::LibraryError,
-            crate::VerificationError::SignerResponseExceedsBoundError => {
-                arrayref::VerifyError::LibraryError
-            }
-            crate::VerificationError::CommitmentHashesDontMatchError => {
-                arrayref::VerifyError::LibraryError
-            }
-            crate::VerificationError::VerificationContextTooLongError => {
-                arrayref::VerifyError::LibraryError
+#[cfg_attr(hax, hax_lib::exclude)]
+mod trait_impl {
+    use libcrux_traits::signature::{arrayref, owned};
+    impl From<crate::SigningError> for owned::SignError {
+        fn from(e: crate::SigningError) -> Self {
+            match e {
+                crate::SigningError::RejectionSamplingError => owned::SignError::LibraryError,
+                crate::SigningError::ContextTooLongError => owned::SignError::LibraryError,
             }
         }
     }
-}
 
-macro_rules! impl_signature_trait {
-    ($name:ident, $module:ident, $alias:ident, $doc:expr) => {
-        mod $module {
-            use super::*;
-
-            #[doc = $doc]
-            pub struct $name;
-
-            pub type $alias = super::Signer<$name>;
-
-            const VERIFICATION_KEY_LEN: usize =
-                crate::ml_dsa_generic::$module::VERIFICATION_KEY_SIZE;
-            const SIGNING_KEY_LEN: usize = crate::ml_dsa_generic::$module::SIGNING_KEY_SIZE;
-            const SIGNATURE_LEN: usize = crate::ml_dsa_generic::$module::SIGNATURE_SIZE;
-
-            // XXX: implementing owned trait directly because there is no arrayref equivalent
-            // TODO: for docs, these should appear as consts in trait def
-            /// It is the responsibility of the caller to ensure  that the `rand` argument is actually
-            /// random.
-            impl owned::Sign<(&[u8], super::Randomness), SIGNING_KEY_LEN, SIGNATURE_LEN> for $alias {
-                fn sign(
-                    payload: &[u8],
-                    private_key: &[u8; SIGNING_KEY_LEN],
-                    (context, randomness): (&[u8], super::Randomness),
-                ) -> Result<[u8; SIGNATURE_LEN], owned::SignError> {
-                    crate::ml_dsa_generic::multiplexing::$module::sign(
-                        private_key,
-                        payload,
-                        context,
-                        randomness,
-                    )
-                    .map(|sig| sig.value)
-                    .map_err(owned::SignError::from)
+    impl From<crate::VerificationError> for arrayref::VerifyError {
+        fn from(e: crate::VerificationError) -> Self {
+            match e {
+                crate::VerificationError::MalformedHintError => arrayref::VerifyError::LibraryError,
+                crate::VerificationError::SignerResponseExceedsBoundError => {
+                    arrayref::VerifyError::LibraryError
+                }
+                crate::VerificationError::CommitmentHashesDontMatchError => {
+                    arrayref::VerifyError::LibraryError
+                }
+                crate::VerificationError::VerificationContextTooLongError => {
+                    arrayref::VerifyError::LibraryError
                 }
             }
-            impl arrayref::Verify<&[u8], VERIFICATION_KEY_LEN, SIGNATURE_LEN> for $alias {
-                fn verify(
-                    payload: &[u8],
-                    public_key: &[u8; VERIFICATION_KEY_LEN],
-                    signature: &[u8; SIGNATURE_LEN],
-                    context: &[u8],
-                ) -> Result<(), arrayref::VerifyError> {
-                    crate::ml_dsa_generic::multiplexing::$module::verify(
-                        public_key,
-                        payload,
-                        context,
-                        signature,
-                    )
-                    .map_err(arrayref::VerifyError::from)
-                }
-            }
-
-            // TODO: secrets trait not appearing in docs
         }
-        pub use $module::{$alias, $name};
-    };
-}
-pub struct Signer<T> {
-    _phantom_data: core::marker::PhantomData<T>,
+    }
+
+    macro_rules! impl_signature_trait {
+        ($name:ident, $module:ident, $alias:ident, $doc:expr) => {
+            mod $module {
+                use super::*;
+
+                #[doc = $doc]
+                pub struct $name;
+
+                pub type $alias = super::Signer<$name>;
+
+                const VERIFICATION_KEY_LEN: usize =
+                    crate::ml_dsa_generic::$module::VERIFICATION_KEY_SIZE;
+                const SIGNING_KEY_LEN: usize = crate::ml_dsa_generic::$module::SIGNING_KEY_SIZE;
+                const SIGNATURE_LEN: usize = crate::ml_dsa_generic::$module::SIGNATURE_SIZE;
+
+                // XXX: implementing owned trait directly because there is no arrayref equivalent
+                // TODO: for docs, these should appear as consts in trait def
+                /// It is the responsibility of the caller to ensure  that the `rand` argument is actually
+                /// random.
+                impl owned::Sign<(&[u8], super::Randomness), SIGNING_KEY_LEN, SIGNATURE_LEN>
+                    for $alias
+                {
+                    fn sign(
+                        payload: &[u8],
+                        private_key: &[u8; SIGNING_KEY_LEN],
+                        (context, randomness): (&[u8], super::Randomness),
+                    ) -> Result<[u8; SIGNATURE_LEN], owned::SignError> {
+                        crate::ml_dsa_generic::multiplexing::$module::sign(
+                            private_key,
+                            payload,
+                            context,
+                            randomness,
+                        )
+                        .map(|sig| sig.value)
+                        .map_err(owned::SignError::from)
+                    }
+                }
+                impl arrayref::Verify<&[u8], VERIFICATION_KEY_LEN, SIGNATURE_LEN> for $alias {
+                    fn verify(
+                        payload: &[u8],
+                        public_key: &[u8; VERIFICATION_KEY_LEN],
+                        signature: &[u8; SIGNATURE_LEN],
+                        context: &[u8],
+                    ) -> Result<(), arrayref::VerifyError> {
+                        crate::ml_dsa_generic::multiplexing::$module::verify(
+                            public_key, payload, context, signature,
+                        )
+                        .map_err(arrayref::VerifyError::from)
+                    }
+                }
+
+                // TODO: secrets trait not appearing in docs
+            }
+            pub use $module::{$alias, $name};
+        };
+    }
+    pub struct Signer<T> {
+        _phantom_data: core::marker::PhantomData<T>,
+    }
+
+    type Randomness = [u8; 32];
+
+    #[cfg(feature = "mldsa44")]
+    impl_signature_trait!(
+        MlDsa44,
+        ml_dsa_44,
+        MlDsa44Signer,
+        "A struct representing ML-DSA 44"
+    );
+    #[cfg(feature = "mldsa65")]
+    impl_signature_trait!(
+        MlDsa65,
+        ml_dsa_65,
+        MlDsa65Signer,
+        "A struct representing ML-DSA 65"
+    );
+    #[cfg(feature = "mldsa87")]
+    impl_signature_trait!(
+        MlDsa87,
+        ml_dsa_87,
+        MlDsa87Signer,
+        "A struct representing ML-DSA 87"
+    );
 }
 
-type Randomness = [u8; 32];
-
-#[cfg(feature = "mldsa44")]
-impl_signature_trait!(
-    MlDsa44,
-    ml_dsa_44,
-    MlDsa44Signer,
-    "A struct representing ML-DSA 44"
-);
-#[cfg(feature = "mldsa65")]
-impl_signature_trait!(
-    MlDsa65,
-    ml_dsa_65,
-    MlDsa65Signer,
-    "A struct representing ML-DSA 65"
-);
-#[cfg(feature = "mldsa87")]
-impl_signature_trait!(
-    MlDsa87,
-    ml_dsa_87,
-    MlDsa87Signer,
-    "A struct representing ML-DSA 87"
-);
+pub use trait_impl::*;
