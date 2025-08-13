780bb310bd033eebc4ca4e8c884c40643e101fcb
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-08-13 16:20:32 +0200

    Avoid using `rng.next_u32()` to set `selector` in tests
    Doing so will break tests in release mode on `aarch64`.

--- secrets/src/int.rs
+++ secrets/src/int.rs
@@ -535,7 +535,9 @@ mod select {
         [T]: Fill + Select,
     {
         let selector: bool = rng.random();
-        let selector = if selector { 0 } else { rng.next_u32() as u8 };
+        let selector = if selector { 0 } else { 1 };
+        // XXX: Setting `selector` as follows will break the selection in release mode on `aarch64`.
+        // let selector = if selector { 0 } else { rng.next_u32() as u8 };
         let mut lhs = [T::default(); 256];
         rng.fill(&mut lhs);
         let mut rhs = [T::default(); 256];
@@ -575,7 +577,9 @@ mod select {
         macro_rules! secret_test {
             ($ty:ty, $rng:expr) => {{
                 let selector: bool = $rng.random();
-                let selector = if selector { 0 } else { $rng.next_u32() as u8 };
+                // XXX: Setting `selector` as follows will break the selection in release mode on `aarch64`.
+                // let selector = if selector { 0 } else { rng.next_u32() as u8 };
+                let selector = if selector { 0 } else { 1 };
                 let mut lhs = [<$ty>::default(); 256];
                 $rng.fill(&mut lhs);
                 let mut rhs = [<$ty>::default(); 256];
@@ -627,7 +631,9 @@ mod swap {
         [T]: Fill + Swap,
     {
         let selector: bool = rng.random();
-        let selector = if selector { 0 } else { rng.next_u32() as u8 };
+        let selector = if selector { 0 } else { 1 };
+        // XXX: Setting `selector` as follows will break the swap in release mode on `aarch64`.
+        // let selector = if selector { 0 } else { rng.next_u32() as u8 };
         let mut lhs = [T::default(); 256];
         rng.fill(&mut lhs);
         let mut rhs = [T::default(); 256];
@@ -665,7 +671,9 @@ mod swap {
         macro_rules! secret_test {
             ($ty:ty, $rng:expr) => {{
                 let selector: bool = $rng.random();
-                let selector = if selector { 0 } else { $rng.next_u32() as u8 };
+                let selector = if selector { 0 } else { 1 };
+                // XXX: Setting `selector` as follows will break the swap in release mode on `aarch64`.
+                // let selector = if selector { 0 } else { $rng.next_u32() as u8 };
                 let mut lhs = [<$ty>::default(); 256];
                 $rng.fill(&mut lhs);
                 let mut rhs = [<$ty>::default(); 256];
