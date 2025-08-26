6d7ebf693f8569c4fc2e64988ff4d020675eeae2
wysiwys <clara@cryspen.com>
2025-08-26 16:37:17 +0200

    begin implementing keygen

--- traits/src/signature/owned.rs
+++ traits/src/signature/owned.rs
@@ -1,6 +1,6 @@
 //! This module contains the trait and related errors for signers that take array references as arguments and return values as arrays.
 
-pub use super::arrayref::{SignError, VerifyError};
+pub use super::arrayref::{KeyGenError, SignError, VerifyError};
 use libcrux_secrets::U8;
 
 /// A signer that returns values instead of writing results to `&mut` arguments.
@@ -10,6 +10,7 @@ pub trait Sign<
     const SIGNING_KEY_LEN: usize,
     const VERIFICATION_KEY_LEN: usize,
     const SIGNATURE_LEN: usize,
+    const RAND_KEYGEN_LEN: usize,
 >
 {
     /// Auxiliary information needed for signing.
@@ -33,14 +34,23 @@ pub trait Sign<
         signature: &[u8; SIGNATURE_LEN],
         aux: Self::VerifyAux<'_>,
     ) -> Result<(), VerifyError>;
+    fn keygen(
+        randomness: [U8; RAND_KEYGEN_LEN],
+    ) -> Result<([U8; SIGNING_KEY_LEN], [u8; VERIFICATION_KEY_LEN]), KeyGenError>;
 }
 
 impl<
         const SIGNING_KEY_LEN: usize,
         const VERIFICATION_KEY_LEN: usize,
         const SIGNATURE_LEN: usize,
-        T: super::arrayref::Sign<SIGNING_KEY_LEN, VERIFICATION_KEY_LEN, SIGNATURE_LEN>,
-    > Sign<SIGNING_KEY_LEN, VERIFICATION_KEY_LEN, SIGNATURE_LEN> for T
+        const RAND_KEYGEN_LEN: usize,
+        T: super::arrayref::Sign<
+            SIGNING_KEY_LEN,
+            VERIFICATION_KEY_LEN,
+            SIGNATURE_LEN,
+            RAND_KEYGEN_LEN,
+        >,
+    > Sign<SIGNING_KEY_LEN, VERIFICATION_KEY_LEN, SIGNATURE_LEN, RAND_KEYGEN_LEN> for T
 {
     type SignAux<'a> = T::SignAux<'a>;
     fn sign(
@@ -60,5 +70,15 @@ impl<
         aux: Self::VerifyAux<'_>,
     ) -> Result<(), VerifyError> {
         T::verify(payload, verification_key, signature, aux)
+    }
+    fn keygen(
+        randomness: [U8; RAND_KEYGEN_LEN],
+    ) -> Result<([U8; SIGNING_KEY_LEN], [u8; VERIFICATION_KEY_LEN]), KeyGenError> {
+        use libcrux_secrets::Classify;
+
+        let mut signing_key = [0; SIGNING_KEY_LEN].classify();
+        let mut verification_key = [0; VERIFICATION_KEY_LEN];
+        T::keygen(&mut signing_key, &mut verification_key, randomness)
+            .map(|_| (signing_key, verification_key))
     }
 }
