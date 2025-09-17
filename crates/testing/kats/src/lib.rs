0f72779f7c2fb1911b40d74342887b036ff0be9b
wysiwys <clara@cryspen.com>
2025-09-17 14:16:21 +0200

    fix grammar

--- crates/testing/kats/src/lib.rs
+++ crates/testing/kats/src/lib.rs
@@ -1,6 +1,6 @@
 //! Known Answer Tests
 //!
-//! ⚠️ **NOTE**: This crate serves as an internal testing dependency to other `libcrux`
+//! ⚠️ **NOTE**: This crate serves as an internal testing dependency for other `libcrux`
 //! crates, and is not intended to be used directly. Use at your own risk.
 
 #[cfg(feature = "wycheproof")]
