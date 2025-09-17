231d2bed31c43541935c35e92c34bce803b044ce
wysiwys <clara@cryspen.com>
2025-09-17 14:18:52 +0200

    remove `wycheproof` feature flag

--- crates/testing/kats/src/lib.rs
+++ crates/testing/kats/src/lib.rs
@@ -3,5 +3,4 @@
 //! ⚠️ **NOTE**: This crate serves as an internal testing dependency for other `libcrux`
 //! crates, and is not intended to be used directly. Use at your own risk.
 
-#[cfg(feature = "wycheproof")]
 pub mod wycheproof;
