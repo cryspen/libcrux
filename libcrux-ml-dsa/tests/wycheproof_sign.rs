06792a2c3dcdaff0d512dafce7bb6ee3ac607ac3
wysiwys <clara@cryspen.com>
2025-09-22 14:03:14 +0200

    reorganize tests

--- libcrux-ml-dsa/tests/wycheproof_sign.rs
+++ libcrux-ml-dsa/tests/wycheproof_sign.rs
@@ -4,7 +4,7 @@
 // This set of test vectors does not cover the pre-hashed variants of
 // ML-DSA.
 
-use libcrux_kats::wycheproof::mldsa::{sign_noseed_schema::*, MlDsaSignTestsNoSeed, ParameterSet};
+use libcrux_kats::wycheproof::mldsa::{sign_noseed_schema, ParameterSet};
 use libcrux_ml_dsa::{
     ml_dsa_44::{self, MLDSA44SigningKey},
     ml_dsa_65::{self, MLDSA65SigningKey},
@@ -12,48 +12,52 @@ use libcrux_ml_dsa::{
     MLDSASigningKey, SigningError,
 };
 
-macro_rules! wycheproof_sign_noseed_test {
+macro_rules! wycheproof_sign_test {
     ($name:ident, $test_name:expr, $signing_key_type:ty, $sign:expr) => {
-        #[test]
-        fn $name() {
-            let katfile_serialized = MlDsaSignTestsNoSeed::load($test_name);
+        mod $name {
+            use super::*;
+            #[test]
+            fn noseed() {
+                use sign_noseed_schema::*;
+                let katfile_serialized = MlDsaSignTestsNoSeed::load($test_name);
 
-            let signing_randomness = [0u8; 32];
+                let signing_randomness = [0u8; 32];
 
-            for test_group in katfile_serialized.test_groups {
-                let signing_key_bytes = test_group.private_key;
-                if signing_key_bytes.len() != <$signing_key_type>::len() {
-                    // If the signing key size in the KAT does not match the
-                    // signing key size in our implementation, ensure that the KAT
-                    // key has a corresponding flag set staring that its length is incorrect.
-                    //
-                    // This flag is set on the child `tests` object.
-                    assert_eq!(test_group.tests.len(), 1);
-                    assert!(test_group.tests[0]
-                        .flags
-                        .contains(&Flag::IncorrectPrivateKeyLength));
+                for test_group in katfile_serialized.test_groups {
+                    let signing_key_bytes = test_group.private_key;
+                    if signing_key_bytes.len() != <$signing_key_type>::len() {
+                        // If the signing key size in the KAT does not match the
+                        // signing key size in our implementation, ensure that the KAT
+                        // key has a corresponding flag set staring that its length is incorrect.
+                        //
+                        // This flag is set on the child `tests` object.
+                        assert_eq!(test_group.tests.len(), 1);
+                        assert!(test_group.tests[0]
+                            .flags
+                            .contains(&Flag::IncorrectPrivateKeyLength));
 
-                    continue;
-                }
-                let signing_key = MLDSASigningKey::new(signing_key_bytes.try_into().unwrap());
-
-                for test in test_group.tests {
-                    let message = test.msg;
-                    let context = test.ctx;
-
-                    let signature = $sign(&signing_key, &message, &context, signing_randomness);
-
-                    if let Err(SigningError::ContextTooLongError) = signature {
-                        assert!(test.result == SignResult::Invalid)
+                        continue;
                     }
+                    let signing_key = MLDSASigningKey::new(signing_key_bytes.try_into().unwrap());
 
-                    if test.result == SignResult::Valid {
-                        assert_eq!(signature.unwrap().as_slice(), test.sig.as_slice());
+                    for test in test_group.tests {
+                        let message = test.msg;
+                        let context = test.ctx;
+
+                        let signature = $sign(&signing_key, &message, &context, signing_randomness);
+
+                        if let Err(SigningError::ContextTooLongError) = signature {
+                            assert!(test.result == SignResult::Invalid)
+                        }
+
+                        if test.result == SignResult::Valid {
+                            assert_eq!(signature.unwrap().as_slice(), test.sig.as_slice());
+                        }
+                        // TODO: else, the generated signature is invalid; we can
+                        // check that our own implementation agrees with this judgement,
+                        // but in order to do so we'd need the verification key.
+                        // This is being tracked in https://github.com/cryspen/libcrux/issues/340
                     }
-                    // TODO: else, the generated signature is invalid; we can
-                    // check that our own implementation agrees with this judgement,
-                    // but in order to do so we'd need the verification key.
-                    // This is being tracked in https://github.com/cryspen/libcrux/issues/340
                 }
             }
         }
@@ -62,14 +66,14 @@ macro_rules! wycheproof_sign_noseed_test {
 
 // 44
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_44,
     ParameterSet::MlDsa44,
     MLDSA44SigningKey,
     ml_dsa_44::sign
 );
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_44_portable,
     ParameterSet::MlDsa44,
     MLDSA44SigningKey,
@@ -77,7 +81,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd128")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_44_simd128,
     ParameterSet::MlDsa44,
     MLDSA44SigningKey,
@@ -85,7 +89,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd256")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_44_simd256,
     ParameterSet::MlDsa44,
     MLDSA44SigningKey,
@@ -94,14 +98,14 @@ wycheproof_sign_noseed_test!(
 
 // 65
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_65,
     ParameterSet::MlDsa65,
     MLDSA65SigningKey,
     ml_dsa_65::sign
 );
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_65_portable,
     ParameterSet::MlDsa65,
     MLDSA65SigningKey,
@@ -109,7 +113,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd128")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_65_simd128,
     ParameterSet::MlDsa65,
     MLDSA65SigningKey,
@@ -117,7 +121,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd256")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_65_simd256,
     ParameterSet::MlDsa65,
     MLDSA65SigningKey,
@@ -126,14 +130,14 @@ wycheproof_sign_noseed_test!(
 
 // 87
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_87,
     ParameterSet::MlDsa87,
     MLDSA87SigningKey,
     ml_dsa_87::sign
 );
 
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_87_portable,
     ParameterSet::MlDsa87,
     MLDSA87SigningKey,
@@ -141,7 +145,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd128")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_87_simd128,
     ParameterSet::MlDsa87,
     MLDSA87SigningKey,
@@ -149,7 +153,7 @@ wycheproof_sign_noseed_test!(
 );
 
 #[cfg(feature = "simd256")]
-wycheproof_sign_noseed_test!(
+wycheproof_sign_test!(
     wycheproof_sign_87_simd256,
     ParameterSet::MlDsa87,
     MLDSA87SigningKey,
