e0233c4ac96fff55a2a7233bf9265eb2f1a2240b
wysiwys <clara@cryspen.com>
2025-09-23 17:10:06 +0200

    separate method for each test type

--- crates/testing/kats/src/wycheproof/mlkem.rs
+++ crates/testing/kats/src/wycheproof/mlkem.rs
@@ -2,10 +2,41 @@ pub mod schema;
 
 pub use schema::*;
 
-impl schema::MlkemTest {
+impl schema::MlKemTests {
     pub fn load() -> Self {
         let data: &str = include_str!("../../wycheproof/mlkem_test.json");
         serde_json::from_str(data).expect("Could not deserialize KAT file.")
+    }
+
+    pub fn keygen_and_decaps_tests<'a>(
+        &'a self,
+        parameter_set: MlKemParameterSet,
+    ) -> impl Iterator<Item = &'a MlKemTestGroup> {
+        self.test_groups
+            .iter()
+            .filter_map(|g| {
+                if let TestGroup::MlKemTestGroup(g) = g {
+                    Some(g)
+                } else {
+                    None
+                }
+            })
+            .filter(move |g| g.parameter_set == parameter_set)
+    }
+    pub fn encaps_tests<'a>(
+        &'a self,
+        parameter_set: MlKemParameterSet,
+    ) -> impl Iterator<Item = &'a MlKemEncapsTestGroup> {
+        self.test_groups
+            .iter()
+            .filter_map(|g| {
+                if let TestGroup::MlKemEncapsTestGroup(g) = g {
+                    Some(g)
+                } else {
+                    None
+                }
+            })
+            .filter(move |g| g.parameter_set == parameter_set)
     }
 }
 
@@ -16,6 +47,6 @@ mod test {
     // test that data is loaded successfully
     #[test]
     fn test_load() {
-        MlkemTest::load();
+        MlKemTests::load();
     }
 }
