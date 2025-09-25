2720201cfe701e1a8b7efdd30d7e1c9aec6094f2
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-09-25 15:50:02 +0200

    ECDH trait implementation for P256

--- p256/src/lib.rs
+++ p256/src/lib.rs
@@ -4,9 +4,15 @@
 //! By default this crate is empty.
 #![no_std]
 
-// HACL* generated code
+// The two kinds of data that are actually there
+const SCALAR_LEN: usize = 32;
+const POINT_LEN: usize = 64;
+
+pub mod ecdh_api;
 mod impl_kem;
-mod p256;
+
+// HACL* generated code
+pub(crate) mod p256;
 mod p256_precomptable;
 
 pub struct P256;
