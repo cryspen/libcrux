fc013f98ceef3239d94ab3ce7149ad5fc27bbbf5
wysiwys <clara@cryspen.com>
2025-09-25 10:50:07 +0200

    initialize hasher with `new()`

--- traits/src/digest/tests.rs
+++ traits/src/digest/tests.rs
@@ -1,12 +1,7 @@
 pub fn simple<
     const OUTPUT_LEN: usize,
-    IncrementalState,
-    HashImplementation: super::arrayref::DigestIncremental<OUTPUT_LEN, IncrementalState = IncrementalState>
-        + super::arrayref::Hash<OUTPUT_LEN>,
->(
-    // provide the state, since not all states currently implement `Default`
-    state: IncrementalState,
-) {
+    HashImplementation: super::arrayref::DigestIncremental<OUTPUT_LEN> + super::arrayref::Hash<OUTPUT_LEN>,
+>() {
     let payload = &[1, 2, 3, 4, 5];
 
     // oneshot API
@@ -15,7 +10,7 @@ pub fn simple<
 
     // incremental API
     let mut digest_incremental = [0u8; OUTPUT_LEN];
-    let mut hasher = super::Hasher::<OUTPUT_LEN, HashImplementation> { state };
+    let mut hasher = super::Hasher::<OUTPUT_LEN, HashImplementation>::new();
     hasher.update(payload).unwrap();
     hasher.finish(&mut digest_incremental);
 
