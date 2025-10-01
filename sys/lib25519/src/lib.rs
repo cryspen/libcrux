e6055337edc48536555ac02376e4b94793d9db25
Jan Winkelmann (keks) <jan@cryspen.com>
2025-10-01 18:07:53 +0200

    Add lib25519 doc comments (and exception for generated bindings)

--- sys/lib25519/src/lib.rs
+++ sys/lib25519/src/lib.rs
@@ -1,5 +1,11 @@
+//! FFI bindings to lib25519 cryptographic library.
+//!
+//! This crate provides Rust bindings to the lib25519 library, which implements
+//! Curve25519 elliptic curve cryptography operations.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(missing_docs)]
 
 include!("./bindings.rs");
