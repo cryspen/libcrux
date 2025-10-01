8edd064c18a378b91004934bcc29f4d540dc8ba5
wysiwys <clara@cryspen.com>
2025-10-01 16:40:45 +0200

    update generic test

--- traits/src/signature/tests.rs
+++ traits/src/signature/tests.rs
@@ -10,18 +10,15 @@ pub fn simple_arrayref<
     const SIGNATURE_LEN: usize,
     const RAND_KEYGEN_LEN: usize,
     SignAux,
-    VerifyAux,
     Signer: super::arrayref::Sign<
         SIGNING_KEY_LEN,
         VERIFICATION_KEY_LEN,
         SIGNATURE_LEN,
         RAND_KEYGEN_LEN,
         SignAux<'b> = SignAux,
-        VerifyAux<'b> = VerifyAux,
     >,
 >(
     sign_aux: SignAux,
-    verify_aux: VerifyAux,
 ) {
     use libcrux_secrets::Classify;
     let keygen_rand = [1u8; RAND_KEYGEN_LEN].classify();
@@ -34,5 +31,5 @@ pub fn simple_arrayref<
     let mut signature = [0; SIGNATURE_LEN];
 
     Signer::sign(&payload, &signing_key, &mut signature, sign_aux).expect("Error signing");
-    Signer::verify(&payload, &verification_key, &signature, verify_aux).expect("Error verifying");
+    Signer::verify(&payload, &verification_key, &signature).expect("Error verifying");
 }
