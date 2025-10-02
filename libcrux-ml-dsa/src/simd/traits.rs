afe8741183470b57bcb2f377e2ae630c9e3aa77c
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-10-02 17:18:25 +0200

    Move spec for [rejection_sample_less_than_eta_equals_4]

--- libcrux-ml-dsa/src/simd/traits.rs
+++ libcrux-ml-dsa/src/simd/traits.rs
@@ -103,7 +103,8 @@ pub(crate) trait Operations: Copy + Clone + Repr {
     #[hax_lib::ensures(|result| specs::rejection_sample_less_than_eta_equals_2_post(randomness, out, result))]
     fn rejection_sample_less_than_eta_equals_2(randomness: &[u8], out: &mut [i32]) -> usize;
 
-    #[hax_lib::requires(true)]
+    #[hax_lib::requires(specs::rejection_sample_less_than_eta_equals_4_pre(randomness, out))]
+    #[hax_lib::ensures(|result| specs::rejection_sample_less_than_eta_equals_4_post(randomness, out, result))]
     fn rejection_sample_less_than_eta_equals_4(randomness: &[u8], out: &mut [i32]) -> usize;
 
     // Encoding operations
