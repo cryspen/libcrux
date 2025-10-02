8e449bf0b23df6a2bf8069f77c5994842afb743c
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-10-02 15:10:08 +0200

    Move spec for [from_coefficient_array]

--- libcrux-ml-dsa/src/simd/traits.rs
+++ libcrux-ml-dsa/src/simd/traits.rs
@@ -38,8 +38,8 @@ pub(crate) trait Operations: Copy + Clone + Repr {
     #[hax_lib::ensures(|result| specs::zero_post(&result.repr()))]
     fn zero() -> Self;
 
-    #[hax_lib::requires(array.len() == COEFFICIENTS_IN_SIMD_UNIT)]
-    #[hax_lib::ensures(|result| future(out).repr() == array)]
+    #[hax_lib::requires(specs::from_coefficient_array_pre(array, &out.repr()))]
+    #[hax_lib::ensures(|_| specs::from_coefficient_array_post(array, &out.repr(), &future(out).repr()))]
     fn from_coefficient_array(array: &[i32], out: &mut Self);
 
     #[hax_lib::requires(out.len() == COEFFICIENTS_IN_SIMD_UNIT)]
