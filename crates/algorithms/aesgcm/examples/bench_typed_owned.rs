ee6f1587bde0892f795f3b20706470a4de3655a1
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-10-09 16:40:32 +0200

    Rename `PAYLOAD_SIZES`

--- crates/algorithms/aesgcm/examples/bench_typed_owned.rs
+++ crates/algorithms/aesgcm/examples/bench_typed_owned.rs
@@ -5,15 +5,15 @@ use libcrux_aesgcm::{
 };
 
 fn main() {
-    const PAYLOAD_SIZES: usize = 3045;
+    const PAYLOAD_SIZE: usize = 3045;
 
     let key: Key = [0x16; AesGcm128::KEY_LEN].into();
     let nonce: Nonce = [0x12; AesGcm128::NONCE_LEN].into();
 
     let aad = [0xff; 32];
-    let plaintext = [0xab; PAYLOAD_SIZES];
+    let plaintext = [0xab; PAYLOAD_SIZE];
 
-    let mut ciphertext = vec![0; PAYLOAD_SIZES];
+    let mut ciphertext = vec![0; PAYLOAD_SIZE];
     let mut tag: Tag = [0u8; AesGcm128::TAG_LEN].into();
 
     for _ in 0..10000 {
--- crates/algorithms/aesgcm/examples/bench_typed_refs.rs
+++ crates/algorithms/aesgcm/examples/bench_typed_refs.rs
@@ -2,7 +2,7 @@
 use libcrux_aesgcm::{Aead as _, AeadConsts as _, AesGcm128};
 
 fn main() {
-    const PAYLOAD_SIZES: usize = 3045;
+    const PAYLOAD_SIZE: usize = 3045;
 
     let algo = AesGcm128;
 
@@ -12,9 +12,9 @@ fn main() {
     let nonce = algo.new_nonce(&[0x12; AesGcm128::NONCE_LEN]).unwrap();
 
     let aad = [0xff; 32];
-    let plaintext = [0xab; PAYLOAD_SIZES];
+    let plaintext = [0xab; PAYLOAD_SIZE];
 
-    let mut ciphertext = vec![0; PAYLOAD_SIZES];
+    let mut ciphertext = vec![0; PAYLOAD_SIZE];
 
     for _ in 0..10000 {
         let tag = algo.new_tag_mut(&mut tag_bytes).unwrap();
