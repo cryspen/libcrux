5b722e968c8c1584002f1bf4991b7162e97dc31d
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-10-14 11:07:31 +0200

    Fix incorrect `pre_hash_buffer` length

--- libcrux-ml-dsa/src/pre_hash.rs
+++ libcrux-ml-dsa/src/pre_hash.rs
@@ -34,7 +34,7 @@ impl PreHash for SHAKE128_PH {
 
     #[inline(always)]
     fn hash<Shake128: hash_functions::shake128::Xof>(message: &[u8], output: &mut [u8]) {
-        debug_assert_eq!(output.len(), 256);
+        debug_assert_eq!(output.len(), 32);
         Shake128::shake128(message, output);
     }
 }
