d6532e2e9df22edf61bfcbf01964901c16c836b3
wysiwys <clara@cryspen.com>
2025-11-17 15:37:12 +0100

    documentation and doctests

--- crates/testing/kats/src/acvp/mlkem.rs
+++ crates/testing/kats/src/acvp/mlkem.rs
@@ -1,3 +1,51 @@
+//! ACVP ML-KEM Known Answer Tests
+//!
+//! The JSON files for ML-KEM were taken from <https://github.com/usnistgov/ACVP-Server>, as of
+//! commit [112690e8484dba7077709a05b1f3af58ddefdd5d](https://github.com/usnistgov/ACVP-Server/commit/112690e8484dba7077709a05b1f3af58ddefdd5d).
+//!
+//! ### Example usage
+//! ```rust
+//! use libcrux_kats::acvp::mlkem::{
+//!     KeyGenTests, EncapDecapTests, encap_decap_schema::EncapDecapTestPrompts
+//! };
+//!
+//! // keygen tests
+//! let keygen_tests = KeyGenTests::load();
+//!
+//! for test_group in keygen_tests.prompts.testGroups {
+//!     for test in test_group.tests {
+//!        // retrieve the expected result for that test
+//!         let expected_result
+//!             = keygen_tests.results.find_expected_result(test_group.tgId, test.tcId);
+//!    }
+//! }
+//!
+//! // encap-decap tests
+//! let encap_decap_tests = EncapDecapTests::load();
+//!
+//! for test_group in encap_decap_tests.prompts.testGroups {
+//!     match test_group.tests {
+//!         // encapsulation
+//!         EncapDecapTestPrompts::EncapTests { tests } => {
+//!             for test in tests {
+//!                 // retrieve the expected result for that test
+//!                 let expected_result
+//!                     = encap_decap_tests.results
+//!                         .find_expected_result(test_group.tgId, test.tcId);
+//!             }
+//!
+//!         },
+//!         // decapsulation
+//!         EncapDecapTestPrompts::DecapTests { tests } => { /* ... */ },
+//!         // encapsulationKeyCheck
+//!         EncapDecapTestPrompts::EncapKeyCheck { tests } => { /* ... */ },
+//!         // decapsulationKeyCheck
+//!         EncapDecapTestPrompts::DecapKeyCheck { tests } => { /* ... */ },
+//!     }
+//! }
+//!
+//! ```
+
 pub mod encap_decap_schema;
 pub mod keygen_schema;
 
