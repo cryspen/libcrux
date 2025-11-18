a49e2ae13aefcd5afc47926c383464f99d3e2e0a
Jan Winkelmann (keks) <jan@cryspen.com>
2025-11-18 11:00:07 +0100

    Update paths and finish move of secrets

--- libcrux-ml-kem/hax.py
+++ libcrux-ml-kem/hax.py
@@ -46,7 +46,7 @@ class extractAction(argparse.Action):
         hax_env = {}
         shell(
             cargo_hax_into,
-            cwd="../sys/platform",
+            cwd="../crates/sys/platform",
             env=hax_env,
         )
 
@@ -72,7 +72,7 @@ class extractAction(argparse.Action):
         }
         shell(
             cargo_hax_into,
-            cwd="../libcrux-intrinsics",
+            cwd="../crates/utils/intrinsics",
             env=hax_env,
         )
 
@@ -90,7 +90,7 @@ class extractAction(argparse.Action):
         hax_env = {}
         shell(
             cargo_hax_into,
-            cwd="../secrets",
+            cwd="../crates/utils/secrets",
             env=hax_env,
         )
 
