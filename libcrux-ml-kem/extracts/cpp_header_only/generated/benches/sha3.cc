bbe4bd6cc116a9e7380cf9d0c73bd971d4a39891
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-11-18 10:46:29 +0100

    Fix more benchmark code

--- libcrux-ml-kem/extracts/cpp_header_only/generated/benches/sha3.cc
+++ libcrux-ml-kem/extracts/cpp_header_only/generated/benches/sha3.cc
@@ -19,9 +19,16 @@
 #include "libcrux_sha3_avx2.h"
 #endif
 
-Eurydice_dst_ref_87_s mk_dst_ref_uint8_t(uint8_t *x, size_t len) {
-  Eurydice_dst_ref_87_s s;
-  s.ptr = (uint8_t *)x;
+Eurydice_borrow_slice_u8 mk_borrow_slice_u8(const uint8_t *x, size_t len) {
+  Eurydice_borrow_slice_u8 s = {0};
+  s.ptr = x;
+  s.meta = len;
+  return s;
+}
+
+Eurydice_mut_borrow_slice_u8 mk_mut_borrow_slice_u8(uint8_t *x, size_t len) {
+  Eurydice_mut_borrow_slice_u8 s = {0};
+  s.ptr = x;
   s.meta = len;
   return s;
 }
@@ -35,12 +42,12 @@ static void sha3_256_1184(benchmark::State &state) {
   uint8_t input[1184];
   generate_random(input, 1184);
 
-  libcrux_sha3_portable_sha256(mk_dst_ref_uint8_t(input, 32),
-                               mk_dst_ref_uint8_t(digest, 32));
+  libcrux_sha3_portable_sha256(mk_mut_borrow_slice_u8(digest, 32),
+                               mk_borrow_slice_u8(input, 32));
 
   for (auto _ : state) {
-    libcrux_sha3_portable_sha256(mk_dst_ref_uint8_t(input, 32),
-                                 mk_dst_ref_uint8_t(digest, 32));
+    libcrux_sha3_portable_sha256(mk_mut_borrow_slice_u8(digest, 32),
+                                 mk_borrow_slice_u8(input, 32));
   }
 }
 
@@ -49,12 +56,12 @@ static void sha3_512_64(benchmark::State &state) {
   uint8_t input[64];
   generate_random(input, 64);
 
-  libcrux_sha3_portable_sha512(mk_dst_ref_uint8_t(input, 64),
-                               mk_dst_ref_uint8_t(digest, 64));
+  libcrux_sha3_portable_sha512(mk_mut_borrow_slice_u8(digest, 64),
+                               mk_borrow_slice_u8(input, 64));
 
   for (auto _ : state) {
-    libcrux_sha3_portable_sha512(mk_dst_ref_uint8_t(input, 64),
-                                 mk_dst_ref_uint8_t(digest, 64));
+    libcrux_sha3_portable_sha512(mk_mut_borrow_slice_u8(digest, 64),
+                                 mk_borrow_slice_u8(input, 64));
   }
 }
 
@@ -97,20 +104,20 @@ __attribute__((target("avx2"))) static void shake256_1120_32(
   uint8_t digest1[32];
   uint8_t digest2[32];
   uint8_t digest3[32];
-  Eurydice_dst_ref_87_s out0 = mk_dst_ref_uint8_t(digest0, 32);
-  Eurydice_dst_ref_87_s out1 = mk_dst_ref_uint8_t(digest1, 32);
-  Eurydice_dst_ref_87_s out2 = mk_dst_ref_uint8_t(digest2, 32);
-  Eurydice_dst_ref_87_s out3 = mk_dst_ref_uint8_t(digest3, 32);
+  Eurydice_mut_borrow_slice_u8 out0 = mk_mut_borrow_slice_u8(digest0, 32);
+  Eurydice_mut_borrow_slice_u8 out1 = mk_mut_borrow_slice_u8(digest1, 32);
+  Eurydice_mut_borrow_slice_u8 out2 = mk_mut_borrow_slice_u8(digest2, 32);
+  Eurydice_mut_borrow_slice_u8 out3 = mk_mut_borrow_slice_u8(digest3, 32);
 
   libcrux_sha3_avx2_x4_shake256(
-      mk_dst_ref_uint8_t(input, 1120), mk_dst_ref_uint8_t(input, 1120),
-      mk_dst_ref_uint8_t(input, 1120), mk_dst_ref_uint8_t(input, 1120), out0,
+      mk_borrow_slice_u8(input, 1120), mk_borrow_slice_u8(input, 1120),
+      mk_borrow_slice_u8(input, 1120), mk_borrow_slice_u8(input, 1120), out0,
       out1, out2, out3);
 
   for (auto _ : state) {
     libcrux_sha3_avx2_x4_shake256(
-        mk_dst_ref_uint8_t(input, 1120), mk_dst_ref_uint8_t(input, 1120),
-        mk_dst_ref_uint8_t(input, 1120), mk_dst_ref_uint8_t(input, 1120), out0,
+        mk_borrow_slice_u8(input, 1120), mk_borrow_slice_u8(input, 1120),
+        mk_borrow_slice_u8(input, 1120), mk_borrow_slice_u8(input, 1120), out0,
         out1, out2, out3);
   }
 }
@@ -124,20 +131,20 @@ __attribute__((target("avx2"))) static void shake256_33_128(
   uint8_t digest1[128];
   uint8_t digest2[128];
   uint8_t digest3[128];
-  Eurydice_dst_ref_87_s out0 = mk_dst_ref_uint8_t(digest0, 128);
-  Eurydice_dst_ref_87_s out1 = mk_dst_ref_uint8_t(digest1, 128);
-  Eurydice_dst_ref_87_s out2 = mk_dst_ref_uint8_t(digest2, 128);
-  Eurydice_dst_ref_87_s out3 = mk_dst_ref_uint8_t(digest3, 128);
+  Eurydice_mut_borrow_slice_u8 out0 = mk_mut_borrow_slice_u8(digest0, 128);
+  Eurydice_mut_borrow_slice_u8 out1 = mk_mut_borrow_slice_u8(digest1, 128);
+  Eurydice_mut_borrow_slice_u8 out2 = mk_mut_borrow_slice_u8(digest2, 128);
+  Eurydice_mut_borrow_slice_u8 out3 = mk_mut_borrow_slice_u8(digest3, 128);
 
   libcrux_sha3_avx2_x4_shake256(
-      mk_dst_ref_uint8_t(input, 128), mk_dst_ref_uint8_t(input, 128),
-      mk_dst_ref_uint8_t(input, 128), mk_dst_ref_uint8_t(input, 128), out0,
+      mk_borrow_slice_u8(input, 128), mk_borrow_slice_u8(input, 128),
+      mk_borrow_slice_u8(input, 128), mk_borrow_slice_u8(input, 128), out0,
       out1, out2, out3);
 
   for (auto _ : state) {
     libcrux_sha3_avx2_x4_shake256(
-        mk_dst_ref_uint8_t(input, 128), mk_dst_ref_uint8_t(input, 128),
-        mk_dst_ref_uint8_t(input, 128), mk_dst_ref_uint8_t(input, 128), out0,
+        mk_borrow_slice_u8(input, 128), mk_borrow_slice_u8(input, 128),
+        mk_borrow_slice_u8(input, 128), mk_borrow_slice_u8(input, 128), out0,
         out1, out2, out3);
   }
 }
