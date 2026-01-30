a875e08e7f807ecea8a6a2e85d2b2d322a303702
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-01-30 08:41:07 +0100

    [kmac] Reduce code duplication

--- crates/algorithms/kmac/src/kmac.rs
+++ crates/algorithms/kmac/src/kmac.rs
@@ -7,9 +7,11 @@ mod internals {
     pub(super) mod kmac;
 }
 use internals::kmac::*;
+use libcrux_sha3::portable::incremental::{CShake128It, CShake256It};
 
 pub fn kmac_128<'a>(tag: &'a mut [u8], key: &[u8], data: &[u8], customization: &[u8]) -> &'a [u8] {
-    compute_kmac_128(
+    // Assert that key is long enough, i.e. at least 128 bits
+    compute_kmac::<168, CShake128It>(
         tag,
         tag.len(),
         key,
@@ -21,7 +23,8 @@ pub fn kmac_128<'a>(tag: &'a mut [u8], key: &[u8], data: &[u8], customization: &
 }
 
 pub fn kmac_256<'a>(tag: &'a mut [u8], key: &[u8], data: &[u8], customization: &[u8]) -> &'a [u8] {
-    compute_kmac_256(
+    // Assert that key is long enough, i.e. at least 256 bits
+    compute_kmac::<136, CShake256It>(
         tag,
         tag.len(),
         key,
