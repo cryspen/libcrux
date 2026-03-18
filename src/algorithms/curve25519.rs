ab3fcac48333f4cc5b8484bc0fcbd8843011fbc5
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-03-18 10:08:42 +0100

    [libcrux] Remove further exports of hacl APIs

--- src/algorithms/curve25519.rs
+++ src/algorithms/curve25519.rs
@@ -1,1 +1,3 @@
-pub use libcrux_curve25519::*;
+pub use libcrux_curve25519::{
+    clamp, ecdh, ecdh_api, is_clamped, secret_to_public, Error, DK_LEN, EK_LEN, SS_LEN, X25519,
+};
--- src/algorithms/hmac.rs
+++ src/algorithms/hmac.rs
@@ -1,1 +1,3 @@
-pub use libcrux_hmac::*;
+pub use libcrux_hmac::{
+    hmac, hmac_sha1, hmac_sha2_256, hmac_sha2_384, hmac_sha2_512, tag_size, Algorithm,
+};
