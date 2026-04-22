53b884fd762c7d72f943e2e6453fdde3f12431aa
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-04-22 11:56:00 +0200

    [kmac] Add rudimentary documentation

--- crates/algorithms/kmac/Readme.md
+++ crates/algorithms/kmac/Readme.md
@@ -1,9 +1,9 @@
 # KMAC
 
-![pre-verification](../../../.assets/pre_verification-orange.svg)
+![verified](../../../.assets/verified-brightgreen.svg)
 
-This crate implements a keyed MAC (KMAC) with SHA-3. It provides KMAC_128 and KMAC_256.
+This crate implements a keyed MAC (KMAC) with SHA-3 as defined in [NIST SP 800-185](https://csrc.nist.gov/pubs/sp/800/185/final). It provides KMAC_128 and KMAC_256.
 
 ## Verification
 
-The code in this crate is not yet verified.
+The code in this crate is verified for runtime safety using F*.
--- crates/algorithms/kmac/src/kmac.rs
+++ crates/algorithms/kmac/src/kmac.rs
@@ -1,6 +1,6 @@
-//! KMAC
+//! # KMAC
 //!
-//! This crate implements KMAC.
+//! This crate implements KMAC as defined in [NIST SP 800-185](https://csrc.nist.gov/pubs/sp/800/185/final).
 #![no_std]
 
 mod internals {
@@ -9,11 +9,21 @@ mod internals {
 use internals::kmac::*;
 use libcrux_sha3::portable::incremental::{CShake128, CShake256};
 
+/// Write the KMAC128 MAC of `data` under `key` into `tag`, using
+/// `customization` if provided.
+///
+/// C.f. [NIST SP 800-185](https://csrc.nist.gov/pubs/sp/800/185/final),
+/// section 4.
 pub fn kmac_128<'a>(tag: &'a mut [u8], key: &[u8], data: &[u8], customization: &[u8]) -> &'a [u8] {
     // Assert that key is long enough, i.e. at least 128 bits
     compute_kmac::<168, CShake128>(tag, tag.len(), key, key.len(), data, customization)
 }
 
+/// Write the KMAC256 MAC of `data` under `key` into `tag`, using
+/// `customization` if provided.
+///
+/// C.f. [NIST SP 800-185](https://csrc.nist.gov/pubs/sp/800/185/final),
+/// section 4.
 pub fn kmac_256<'a>(tag: &'a mut [u8], key: &[u8], data: &[u8], customization: &[u8]) -> &'a [u8] {
     // Assert that key is long enough, i.e. at least 256 bits
     compute_kmac::<136, CShake256>(tag, tag.len(), key, key.len(), data, customization)
