946953e52a5040b5fa2656126c620aba168a9f6c
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-04-23 14:07:10 +0200

    [libcrux] Remove `hmac_sha1` re-export

--- src/algorithms/hmac.rs
+++ src/algorithms/hmac.rs
@@ -1,4 +1,2 @@
 //! # HMAC using SHA1 or SHA2
-pub use libcrux_hmac::{
-    hmac, hmac_sha1, hmac_sha2_256, hmac_sha2_384, hmac_sha2_512, tag_size, Algorithm,
-};
+pub use libcrux_hmac::{hmac, hmac_sha2_256, hmac_sha2_384, hmac_sha2_512, tag_size, Algorithm};
