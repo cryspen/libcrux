b3a7768f1bea228f454af5cb861c762513deaf3d
Jonas Schneider-Bensch <jonas@celabs.eu>
2026-07-06 10:27:04 +0200

    [blake2] Update `libcrux-traits` impls

--- crates/algorithms/blake2/tests/blake2.rs
+++ crates/algorithms/blake2/tests/blake2.rs
@@ -208,17 +208,23 @@ fn test_blake2s() {
 
 #[test]
 fn test_digest_traits_2s() {
+    fn updated_hasher() -> Blake2sHasher<32> {
+        let mut hasher = Blake2sHasher::<32>::new();
+        hasher.update(b"this is a test").unwrap();
+        hasher
+    }
+
     let mut got_hash = [0; 32];
 
     // test unkeyed, with const key and digest len
     let expected_hash = b"\xf2\x01\x46\xc0\x54\xf9\xdd\x6b\x67\x64\xb6\xc0\x93\x57\xf7\xcd\x75\x51\xdf\xbc\xba\x54\x59\x72\xa4\xc8\x16\x6d\xf8\xaf\xde\x60";
-    let mut hasher = Blake2sHasher::<32>::new();
-    hasher.update(b"this is a test").unwrap();
+    let hasher = updated_hasher();
     hasher.finish(&mut got_hash);
 
     assert_eq!(&got_hash, expected_hash);
 
     let mut too_short = vec![0; 31];
+    let hasher = updated_hasher();
     let err = hasher.finish_slice(&mut too_short).unwrap_err();
     assert_eq!(
         err,
@@ -226,6 +232,7 @@ fn test_digest_traits_2s() {
     );
 
     let mut too_long = vec![0; 33];
+    let hasher = updated_hasher();
     let err = hasher.finish_slice(&mut too_long).unwrap_err();
     assert_eq!(
         err,
@@ -234,17 +241,23 @@ fn test_digest_traits_2s() {
 }
 #[test]
 fn test_digest_traits_2b() {
+    fn updated_hasher() -> Blake2bHasher<32> {
+        let mut hasher = Blake2bHasher::<32>::new();
+        hasher.update(b"this is a test").unwrap();
+        hasher
+    }
+
     let mut got_hash = [0; 32];
 
     // test unkeyed, with const key and digest len
     let expected_hash = b"\xe9\xed\x14\x1d\xf1\xce\xbf\xc8\x9e\x46\x6c\xe0\x89\xee\xdd\x4f\x12\x5a\xa7\x57\x15\x01\xa0\xaf\x87\x1f\xab\x60\x59\x71\x17\xb7";
-    let mut hasher = Blake2bHasher::<32>::new();
-    hasher.update(b"this is a test").unwrap();
+    let hasher = updated_hasher();
     hasher.finish(&mut got_hash);
 
     assert_eq!(&got_hash, expected_hash);
 
     let mut too_short = vec![0; 31];
+    let hasher = updated_hasher();
     let err = hasher.finish_slice(&mut too_short).unwrap_err();
     assert_eq!(
         err,
@@ -252,6 +265,7 @@ fn test_digest_traits_2b() {
     );
 
     let mut too_long = vec![0; 33];
+    let hasher = updated_hasher();
     let err = hasher.finish_slice(&mut too_long).unwrap_err();
     assert_eq!(
         err,
