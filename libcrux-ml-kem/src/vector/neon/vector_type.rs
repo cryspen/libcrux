d68a2e37460a8afa3330c12a84430e7cf64908af
Jonas Schneider-Bensch <jonas@celabs.eu>
2026-09-08 09:50:47 +0200

    [mlkem] Format

--- libcrux-ml-kem/src/vector/neon/vector_type.rs
+++ libcrux-ml-kem/src/vector/neon/vector_type.rs
@@ -1,6 +1,9 @@
 use libcrux_intrinsics::arm64::*;
 #[derive(Clone, Copy)]
-#[cfg_attr(hax, hax_lib::fstar::after(interface, "val repr (x:t_SIMD128Vector) : t_Array i16 (sz 16)"))]
+#[cfg_attr(
+    hax,
+    hax_lib::fstar::after(interface, "val repr (x:t_SIMD128Vector) : t_Array i16 (sz 16)")
+)]
 #[cfg_attr(hax, hax_lib::fstar::after("let repr (x:t_SIMD128Vector) = admit()"))]
 pub struct SIMD128Vector {
     pub low: _int16x8_t,
